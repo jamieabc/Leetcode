6844ce242d8b8db6fccbe7279f26ab733c93f12e
Aaron Chang <xesuschang@gmail.com>
2019-10-28 23:17:40 +0800

    Squashed commit of 28:
    commit eab7f1e319d603b92d8268a37b8ba6d62b3790f1
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon Oct 28 23:15:59 2019 +0800
    
        refactor

--- 28.go
+++ 28.go
@@ -21,38 +21,24 @@ import "fmt"
 //For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
 
 func strStr(haystack string, needle string) int {
-	length := len(needle)
+	needleLength := len(needle)
 
-	if 0 == length {
+	if 0 == needleLength {
 		return 0
 	}
+	haystackLength := len(haystack)
 
-	if len(haystack) < len(needle) {
+	if haystackLength < needleLength {
 		return -1
 	}
 
-	haystackLength := len(haystack)
-
 	for i := range haystack {
-		if haystack[i] == needle[0] && haystackLength-i >= length && same(haystack[i+1:i+length], needle[1:]) {
+		if haystack[i] == needle[0] && haystackLength-i >= needleLength && haystack[i+1:i+needleLength] == needle[1:] {
 			return i
 		}
 	}
 
 	return -1
-}
-
-func same(source, target string) bool {
-	if len(source) != len(target) {
-		return false
-	}
-
-	for i := range target {
-		if target[i] != source[i] {
-			return false
-		}
-	}
-	return true
 }
 
 func main() {
