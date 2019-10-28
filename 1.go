ba38621e89676b5579a0192ad821f40f568d2d61
Aaron Chang <xesuschang@gmail.com>
2019-10-28 19:53:34 +0800

    add another problem 1

--- 1.go
+++ 1.go
@@ -1,5 +1,7 @@
 package main
 
+import "sort"
+
 //Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 //
 //You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -43,4 +45,39 @@ func generateMap(nums []int) map[int][]int {
 		}
 	}
 	return result
+}
+
+func twoSum2(nums []int, target int) []int {
+	length := len(nums)
+	sorted := make([]int, length)
+	used := make([]bool, length)
+
+	copy(sorted, nums)
+	sort.Ints(sorted)
+
+	i := 0
+	j := length - 1
+
+	for i < j {
+		sum := sorted[i] + sorted[j]
+
+		if sum == target {
+			return []int{findIndex(nums, sorted[i], used), findIndex(nums, sorted[j], used)}
+		} else if sum > target {
+			j--
+		} else {
+			i++
+		}
+	}
+	return []int{}
+}
+
+func findIndex(nums []int, num int, used []bool) int {
+	for i, v := range nums {
+		if v == num && !used[i] {
+			used[i] = true
+			return i
+		}
+	}
+	return -1
 }
