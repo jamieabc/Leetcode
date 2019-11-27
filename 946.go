45524beafa8c5eb74ca54d27dad2ca433fd0d645
Aaron Chang <xesuschang@gmail.com>
2019-11-27 11:27:10 +0800

    Squashed commit of 946:
    commit 16206c3bf702d0ef239f4fff9f8215bad8c3b80e
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Wed Nov 27 11:26:40 2019 +0800
    
        add faster operation
    
    commit 7d59e33cca8b35c66dbd6c09fe973d2e119824f7
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Wed Nov 27 10:30:34 2019 +0800
    
        add problem 946

---
+++ 946.go
@@ -0,0 +1,91 @@
+package main
+
+//Given two sequences pushed and popped with distinct values, return true if and only if this could have been the result of a sequence of push and pop operations on an initially empty stack.
+//
+//
+//
+//Example 1:
+//
+//Input: pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
+//Output: true
+//Explanation: We might do the following sequence:
+//push(1), push(2), push(3), push(4), pop() -> 4,
+//push(5), pop() -> 5, pop() -> 3, pop() -> 2, pop() -> 1
+//Example 2:
+//
+//Input: pushed = [1,2,3,4,5], popped = [4,3,5,1,2]
+//Output: false
+//Explanation: 1 cannot be popped before 2.
+//
+//
+//Note:
+//
+//0 <= pushed.length == popped.length <= 1000
+//0 <= pushed[i], popped[i] < 1000
+//pushed is a permutation of popped.
+//pushed and popped have distinct values.
+
+// since every item should appear once
+// the easier way is to follow operation of push/pop
+// a pointer to popped item list, if this popped item is same as top of stack
+// pop it and go to next popped item, otherwise push next pushed item
+// operation is invalid when find stack is not empty
+func validateStackSequences(pushed []int, popped []int) bool {
+	if len(pushed) != len(popped) {
+		return false
+	}
+
+	if len(popped) <= 1 {
+		return true
+	}
+
+	s := make([]int, len(pushed))
+	loc := 0
+	j := 0
+	for i := 0; i < len(pushed); i++ {
+		s[loc] = pushed[i]
+		loc++
+		for loc-1 >= 0 && s[loc-1] == popped[j] {
+			loc--
+			j++
+		}
+	}
+
+	return loc == 0
+}
+
+// use a map to store item <-> index
+// use another array to store if item pushed status
+// a pointer to denote current location, if pointer moves left, mark path 1 as popped
+// move pointer right, mark only the target item as popped
+// if any pointer target location on left is marked as popped, then it's illegal
+func validateStackSequences1(pushed []int, popped []int) bool {
+	if len(popped) <= 1 {
+		return true
+	}
+
+	m := make(map[int]int)
+	for idx, item := range pushed {
+		m[item] = idx
+	}
+
+	status := make([]bool, len(pushed))
+	status[m[popped[0]]] = true
+
+	for j := 1; j < len(popped); j++ {
+		currentIndex := m[popped[j]]
+		previousIndex := m[popped[j-1]]
+		if status[currentIndex] == true {
+			return false
+		}
+
+		if previousIndex > currentIndex {
+			for tmp := currentIndex; tmp <= previousIndex; tmp++ {
+				status[tmp] = true
+			}
+		} else {
+			status[currentIndex] = true
+		}
+	}
+	return true
+}
--- go.mod
+++ go.mod
@@ -3,27 +3,7 @@ module github.com/jamieabc/leetcode
 go 1.12
 
 require (
-	github.com/cosiner/argv v0.0.1 // indirect
-	github.com/cpuguy83/go-md2man v1.0.10 // indirect
-	github.com/go-delve/delve v1.2.0 // indirect
-	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
-	github.com/mattn/go-colorable v0.1.1 // indirect
-	github.com/mattn/go-isatty v0.0.7 // indirect
-	github.com/mattn/go-runewidth v0.0.4 // indirect
-	github.com/peterh/liner v1.1.0 // indirect
-	github.com/pkg/profile v1.3.0 // indirect
-	github.com/russross/blackfriday v2.0.0+incompatible // indirect
-	github.com/sirupsen/logrus v1.4.1 // indirect
-	github.com/spf13/cobra v0.0.3 // indirect
-	github.com/spf13/pflag v1.0.3 // indirect
-	github.com/stretchr/objx v0.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/golang-collections/collections v0.0.0-20130729185459-604e922904d3
 	github.com/stretchr/testify v1.3.0
-	golang.org/x/arch v0.0.0-20190312162104-788fe5ffcd8c // indirect
-	golang.org/x/crypto v0.0.0-20190510104115-cbcb75029529 // indirect
-	golang.org/x/net v0.0.0-20190509222800-a4d6f7feada5 // indirect
-	golang.org/x/sys v0.0.0-20190509141414-a5b02f93d862 // indirect
-	golang.org/x/text v0.3.2 // indirect
-	golang.org/x/tools v0.0.0-20190511041617-99f201b6807e // indirect
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
-	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
