e8926cb14de4ac7de58c5737123d09e4f3ee51e2
Aaron Chang <xesuschang@gmail.com>
2019-12-20 19:22:41 +0800

    Squashed commit of 20:
    commit 412914c1cd2bf50d4b677a8f1bcb599a0aab647d
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Dec 20 19:22:18 2019 +0800
    
        add problem 20

---
+++ 20.go
@@ -0,0 +1,87 @@
+package main
+
+//Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+//
+//An input string is valid if:
+//
+//    Open brackets must be closed by the same type of brackets.
+//    Open brackets must be closed in the correct order.
+//
+//Note that an empty string is also considered valid.
+//
+//Example 1:
+//
+//Input: "()"
+//Output: true
+//
+//Example 2:
+//
+//Input: "()[]{}"
+//Output: true
+//
+//Example 3:
+//
+//Input: "(]"
+//Output: false
+//
+//Example 4:
+//
+//Input: "([)]"
+//Output: false
+//
+//Example 5:
+//
+//Input: "{[]}"
+//Output: true
+
+func isValid(s string) bool {
+	if len(s) == 0 {
+		return true
+	}
+
+	st := &stack{data: make([]rune, 0)}
+
+	for _, c := range s {
+		if c == '(' || c == '[' || c == '{' {
+			st.push(c)
+		} else if c == ')' || c == ']' || c == '}' {
+			if st.pop() != c {
+				return false
+			}
+		} else {
+			return false
+		}
+	}
+	return st.length == 0
+}
+
+type stack struct {
+	data   []rune
+	length int
+}
+
+func (s *stack) push(r rune) {
+	s.data = append(s.data, r)
+	s.length++
+}
+
+func (s *stack) pop() rune {
+	if s.length == 0 {
+		return ' '
+	}
+
+	popped := s.data[s.length-1]
+	s.data = s.data[:s.length-1]
+	s.length--
+
+	switch popped {
+	case '(':
+		return ')'
+	case '[':
+		return ']'
+	case '{':
+		return '}'
+	default:
+		return ' '
+	}
+}
