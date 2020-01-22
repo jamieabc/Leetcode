a77b21f3f14367fbd61899546e97873fd444582e
Aaron Chang <xesuschang@gmail.com>
2020-01-22 19:42:00 +0800

    Squashed commit of 844:
    commit cd8558054c4d9175206d5554454a8d232ff6dfe9
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Wed Jan 22 19:41:05 2020 +0800
    
        attempt 9
    
    commit f099757e1233c2fca316853e897ff7abb0b73116
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Wed Jan 22 19:39:01 2020 +0800
    
        attempt 8
    
    commit 36e8ba4135b3afc117bca6073847c8d03a18db9f
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Wed Jan 22 19:29:01 2020 +0800
    
        fix typo
    
    commit 9c742ad037a673d56ad99abbb91c07c5c8e05819
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Wed Jan 22 19:26:12 2020 +0800
    
        attempt 7
    
    commit db5d57cd9ba9e46e13a95140781d3687cd703d82
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Wed Jan 22 19:07:25 2020 +0800
    
        attempt 6
    
    commit e9ffcd4f2b21261dadb5f6aa4883b36f5b5109d6
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon Jan 20 19:56:21 2020 +0800
    
        attempt 5
    
    commit 61f1e9f98e8db90a2fe2502fec540ef5bb132407
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon Jan 20 19:55:27 2020 +0800
    
        attempt 4
    
    commit 98668cbcf714e621976dbeb115cd6adb68e7239a
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon Jan 20 19:55:08 2020 +0800
    
        attempt 3
    
    commit dac33514a8fc61ed9a942cd7164f410e01e24eff
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon Jan 20 19:49:32 2020 +0800
    
        attempt 2
    
    commit 30c07e43c44892595cd32e0e1ab3c65af8df8252
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon Jan 20 19:47:45 2020 +0800
    
        fix typo
    
    commit 2c710b7b49a063f7909ff8dd311204f28c6e837f
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon Jan 20 19:46:08 2020 +0800
    
        attempt 1

---
+++ 844.go
@@ -0,0 +1,121 @@
+package main
+
+func backspaceCompare(S string, T string) bool {
+	backS, backT := 0, 0
+
+	// traverse backward, since backspace can only affect char before that position
+	var i, j int
+	for i, j = len(S)-1, len(T)-1; i >= 0 || j >= 0; {
+
+		// find next not deleted char
+		for i >= 0 {
+			if S[i] == '#' {
+				backS++
+				i--
+			} else {
+				if backS > 0 {
+					i--
+					backS--
+				} else {
+					break
+				}
+			}
+		}
+
+		for j >= 0 {
+			if T[j] == '#' {
+				backT++
+				j--
+			} else {
+				if backT > 0 {
+					j--
+					backT--
+				} else {
+					break
+				}
+			}
+		}
+
+		if i < 0 && j < 0 {
+			return true
+		}
+
+		if (i < 0 && j >= 0) || (i >= 0 && j < 0) {
+			return false
+		}
+
+		if S[i] != T[j] {
+			return false
+		}
+
+		i--
+		j--
+	}
+
+	return true
+}
+
+func backspaceCompare2(S string, T string) bool {
+	s1 := stack{
+		data: make([]byte, 0),
+	}
+	s2 := stack{
+		data: make([]byte, 0),
+	}
+
+	for i := 0; i < len(S); i++ {
+		if S[i] == '#' {
+			_ = s1.pop()
+		} else {
+			s1.push(S[i])
+		}
+	}
+
+	for i := 0; i < len(T); i++ {
+		if T[i] == '#' {
+			_ = s1.pop()
+		} else {
+			s1.push(T[i])
+		}
+	}
+
+	for !s1.empty() && !s2.empty() {
+		s := s1.pop()
+		t := s2.pop()
+		if s != t {
+			return false
+		}
+	}
+
+	return s1.empty() && s2.empty()
+}
+
+type stack struct {
+	data []byte
+}
+
+func (s *stack) push(i byte) {
+	s.data = append(s.data, i)
+}
+
+func (s *stack) pop() byte {
+	length := len(s.data)
+	if length == 0 {
+		return '0'
+	}
+	popped := s.data[length-1]
+	s.data = s.data[:length-1]
+	return popped
+}
+
+func (s *stack) empty() bool {
+	return len(s.data) == 0
+}
+
+// problems
+// 1. use wrong variable, when processing T, forget to update variable
+// 2. wrong condition of comparing, if should be neither of stack is empty
+// 3. still wrong condition of point 2, because it should be neither, so it's !empty
+// 4. typo
+// 5. wrong situation if all characters are deleted, e.g. i will be 0 & j will be 0 which is not true
+// 6. if i & j both < 0, break loop
