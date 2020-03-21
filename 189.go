694244fbcfc050dc935d84bbafbf3e89b92a235e
Aaron Chang <xesuschang@gmail.com>
2020-03-21 12:32:41 +0800

    Squashed commit of 189:
    commit 25a6768a6114d412ec1b5b09a02567af1d65c925
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sat Mar 21 12:31:05 2020 +0800
    
        attempt 14
    
    commit b3a7f9c03b440db225ac1db87e8339b1477d328c
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Mar 20 20:28:37 2020 +0800
    
        attempt 13
    
    commit 1349957f38777429c5d9ac23876071971b9a96d2
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Mar 20 20:27:52 2020 +0800
    
        attempt 12
    
    commit c89263b148546cb06c1037825b608261e7792d9f
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Mar 20 20:26:23 2020 +0800
    
        attempt 11
    
    commit 7a2b361c004a136dccf02ecb12f5d117ce67cc5d
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Mar 20 19:55:44 2020 +0800
    
        attempt 10
    
    commit 2791c18744a1e6ea93a6ac79a8c35c3d28ae0e6a
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Mar 20 19:54:52 2020 +0800
    
        attempt 9
    
    commit 6b92eba55082f4dece38dd819370a34270d90fed
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Mar 20 19:46:26 2020 +0800
    
        attempt 8
    
    commit 1600bd205b1835ec8e0268787b8ecd40fcdefb04
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Mar 20 19:45:21 2020 +0800
    
        attempt 7
    
    commit 630a271b0aa02e9914f345c34c2942cdeac69b83
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Mar 20 19:42:08 2020 +0800
    
        attempt 6
    
    commit c61d14026b294c2c1ced462624f6eaeba23269cf
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Mar 20 19:31:13 2020 +0800
    
        attempt 5
    
    commit 6d10be2a43b585f1baeb3afcbdd8206ca7eade38
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Mar 20 19:25:27 2020 +0800
    
        attempt 4
    
    commit 7cff2fa3a6c6afec9ebd1241dda05332bf2190ac
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Mar 20 19:23:24 2020 +0800
    
        attempt 3
    
    commit 3cf42a385942f231152c0b285ae6037db5a2141d
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Mar 20 19:22:25 2020 +0800
    
        attempt 2
    
    commit 1a9b09bddc41795cd27949f83e91fd6722422954
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Mar 20 19:21:29 2020 +0800
    
        attempt 1

--- 189.go
+++ 189.go
@@ -24,10 +24,34 @@ package main
 //    Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
 //    Could you do it in-place with O(1) extra space?
 
+func rotate(nums []int, k int) {
+	length := len(nums)
+	if length == 0 {
+		return
+	}
+
+	k = k % length
+	if k == 0 {
+		return
+	}
+
+	var count, start, prev, current, next int
+	for start, count = 0, 0; count < length; start++ {
+		current = start
+		prev = nums[current]
+		for next = -1; next != start; {
+			next = (current + k) % length
+			prev, nums[next] = nums[next], prev
+			current = next
+			count++
+		}
+	}
+}
+
 // [-1, -100, 3, 99] 2
 // [-1, -2, -3, -4, -5, -6] 2 it's gonna cycle
 // [1, 2, 3, 4, 5, 6, 7] 3
-func rotate(nums []int, k int) {
+func rotate3(nums []int, k int) {
 	length := len(nums)
 
 	if length <= 1 {
@@ -92,3 +116,12 @@ func rotate1(nums []int, k int) {
 		nums[0] = tmp
 	}
 }
+
+// problems
+//	1.	length could be 0
+//	2.	when k is 1, it's special case, it's behavior acts like mod == 1
+//	3.	wrong condition, checking should be mod != 0, not mod == 1
+//	4.	original algorithm is totally wrong, it cannot be separated into
+//		fully divided, re-think
+//		stop criteria is that every number should be moved at least once
+//		if start index is repeated, proceed to next one
