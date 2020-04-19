aba81ef5a8e33f2453498ca8502d75043704040d
Aaron Chang <xesuschang@gmail.com>
2020-04-19 13:42:42 +0800

    Squashed commit of 921:
    commit 8ce8644157ec0f3471351743060edae94973219f
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sun Apr 19 13:42:26 2020 +0800
    
        attempt 1

---
+++ 921.go
@@ -0,0 +1,59 @@
+package main
+
+// Given a string S of '(' and ')' parentheses, we add the minimum number of parentheses ( '(' or ')', and in any positions ) so that the resulting parentheses string is valid.
+//
+// Formally, a parentheses string is valid if and only if:
+//
+//     It is the empty string, or
+//     It can be written as AB (A concatenated with B), where A and B are valid strings, or
+//     It can be written as (A), where A is a valid string.
+//
+// Given a parentheses string, return the minimum number of parentheses we must add to make the resulting string valid.
+//
+//
+//
+// Example 1:
+//
+// Input: "())"
+// Output: 1
+//
+// Example 2:
+//
+// Input: "((("
+// Output: 3
+//
+// Example 3:
+//
+// Input: "()"
+// Output: 0
+//
+// Example 4:
+//
+// Input: "()))(("
+// Output: 4
+//
+//
+//
+// Note:
+//
+//     S.length <= 1000
+//     S only consists of '(' and ')' characters.
+
+func minAddToMakeValid(S string) int {
+	stack := make([]rune, 0)
+	var add int
+
+	for _, s := range S {
+		if s == '(' {
+			stack = append(stack, s)
+		} else {
+			if len(stack) == 0 {
+				add++
+			} else {
+				stack = stack[:len(stack)-1]
+			}
+		}
+	}
+
+	return add + len(stack)
+}
