0a7723993f5846741a8c81561182c667ea9d525e
Aaron Chang <xesuschang@gmail.com>
2020-04-24 18:48:23 +0800

    Squashed commit of 442:
    commit c3f2f969431c88e977e12adfc095b92da2aab704
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Apr 24 18:48:03 2020 +0800
    
        modify comment
    
    commit d41804e11e1f4f618a187468d40ccec5f3a44d4e
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Apr 24 18:45:30 2020 +0800
    
        attempt 2
    
    commit fdaa10cfc761d82ecf0f0e6f49345bf15be8f8dc
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Apr 24 18:37:24 2020 +0800
    
        attempt 1

---
+++ 442.go
@@ -0,0 +1,40 @@
+package main
+
+// Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+//
+// Find all the elements that appear twice in this array.
+//
+// Could you do it without extra space and in O(n) runtime?
+//
+// Example:
+//
+// Input:
+// [4,3,2,7,8,2,3,1]
+//
+// Output:
+// [2,3]
+
+func findDuplicates(nums []int) []int {
+	result := make([]int, 0)
+
+	for _, n := range nums {
+		// index start from 0, but number starts from 1, so decrease by 1 for indirect accessing
+		if n < 0 {
+			n = -n - 1
+		} else {
+			n -= 1
+		}
+
+		// if the number appears twice, the other number referenced by the number becomes negative
+		if nums[n] < 0 {
+			result = append(result, n+1)
+		} else {
+			nums[n] *= -1
+		}
+	}
+
+	return result
+}
+
+//	problems
+//	1.	I store wrong number, it should be n not nums[n]
