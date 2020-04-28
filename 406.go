598d37ca2e897e4bd968db2d42166672116ecf7c
Aaron Chang <xesuschang@gmail.com>
2020-04-28 19:52:36 +0800

    Squashed commit of 406:
    commit 87f80e8d44502f86d50212802bdd7d56e1de3db8
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Tue Apr 28 19:49:32 2020 +0800
    
        attempt 8
    
    commit 0b8def30286eae8c7bd54cbefca208b929d9d091
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Tue Apr 28 19:16:33 2020 +0800
    
        attempt 7
    
    commit 4c45757bbfce41fdb13fce451328ebc877d234ff
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Tue Apr 28 18:51:33 2020 +0800
    
        attempt 6
    
    commit 5e3f3dc4987d1e047cbfe478509e553dbb6e9df3
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Tue Apr 28 18:45:16 2020 +0800
    
        attempt 5
    
    commit bba3aae54042923a04f6fa35bafc400a3bb006e5
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Tue Apr 28 18:32:40 2020 +0800
    
        attempt 4
    
    commit bc0e9dbb865da065cf13433977a74b92243c2026
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sun Apr 26 16:33:26 2020 +0800
    
        attempt 3
    
    commit 47fe146045c9e08a9cbf76cf0fadaab026a0c694
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sun Apr 26 16:28:23 2020 +0800
    
        attempt 2
    
    commit d95ce9cb751c6e6426421524c081382389752f4b
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sun Apr 26 16:01:59 2020 +0800
    
        attempt 1

---
+++ 406.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"sort"
+)
+
+// Suppose you have a random list of people standing in a queue. Each person is described by a pair of integers (h, k), where h is the height of the person and k is the number of people in front of this person who have a height greater than or equal to h. Write an algorithm to reconstruct the queue.
+//
+// Note:
+// The number of people is less than 1,100.
+//
+//
+// Example
+//
+// Input:
+// [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+//
+// Output:
+// [[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
+
+func reconstructQueue(people [][]int) [][]int {
+	sort.Slice(people, func(i, j int) bool {
+		if people[i][0] == people[j][0] {
+			return people[i][1] < people[j][1]
+		}
+
+		return people[i][0] > people[j][0]
+	})
+
+	result := make([][]int, len(people))
+
+	for i := 0; i < len(people); i++ {
+		copy(result[people[i][1]+1:], result[people[i][1]:])
+		result[people[i][1]] = people[i]
+	}
+
+	return result
+}
+
+func reconstructQueue2(people [][]int) [][]int {
+	sort.Slice(people, func(i, j int) bool {
+		if people[i][0] == people[j][0] {
+			return people[i][1] < people[j][1]
+		}
+
+		return people[i][0] < people[j][0]
+	})
+
+	result := make([][]int, len(people))
+	var j, count int
+	for i := range people {
+		for j, count = 0, 0; count < people[i][1] || len(result[j]) != 0; j++ {
+			if len(result[j]) == 0 || result[j][0] >= people[i][0] {
+				count++
+			}
+		}
+		result[j] = people[i]
+	}
+
+	return result
+}
+
+//	problem
+//	1.	when moving by difference, it should be finding # of people higher/lower
+//		than me, and put myself there
+
+//		e.g. if higher than me is 4, but now is 2, means I need to move myself
+//			 after 2 people higher than me
+
+//		e.g. if higher than me is 4, but now is 6, means too many people higher
+//			 than me, I have to move myself before 2 people higher me
+
+//	2.	when deciding next position, it could be possible that to end of array,
+//		which means dst could beyond range
+
+//	3.	can't think of any way to improve, so I check reference
+//		https://leetcode.com/problems/queue-reconstruction-by-height/discuss/89345/Easy-concept-with-PythonC%2B%2BJava-Solution
+//		this is really clever solution, it founds the rule of sorting, looks like
+//		divide & conquer. The point here is highest people order is always fixed.
+
+//		My thinking goes from minimum to maximum, but the problem is that
+//		minimum position will be changed when larger number moves.
+
+//		What can be better when I think of the problem? I should focus more on
+//		nature of problem: the higher count means lower one can be inserted into
+//		higher ones. Which means higher ones position are still fixed, the change
+//		comes from lower one.
+
+//	4.	the other reference: https://leetcode.com/problems/queue-reconstruction-by-height/discuss/89359/Explanation-of-the-neat-Sort%2BInsert-solution
+//		this one starts from lowest people, since lowest people has nothing to
+//		do with higher people, the higher count of lowest people is actually
+//		the index value of still free spaces.
+
+//	5.	After seeing 2 beautiful solutions, what did I miss during thinking? I
+//		focus on surface and fail to notice actual rule that makes it work.
+//		If I notice that smaller people moving won't affect higher people,
+//		then this could be a clue to solve the problem. It's hard, but not that
+//		hard, I should learn how to think through clues.
+
+//	6.	when start from minimum, count is increased in 2 conditions:
+//		- empty slot
+//		- people higher than or equal to me
+
+//	7.	refactor
+
+//	8.	optimize, too slow when starts from highest...the reason is many memory
+//		allocation/dump, it can be in-place insert
