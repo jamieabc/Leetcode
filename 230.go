ae4ee58993e48b20ee1a6fe9584058eca6340600
Aaron Chang <xesuschang@gmail.com>
2020-05-15 13:37:39 +0800

    Squashed commit of 230:
    commit c0c00094fab29e33218ce7c5bd2df3b21c4cbd7b
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri May 15 13:36:47 2020 +0800
    
        add reference
    
    commit 10c3cd77b77912fd4c6879ceb02967f472ba78ef
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri May 15 13:18:26 2020 +0800
    
        attempt 12
    
    commit 0a41b101ab03197903dd4600b1ddbfe8ef228527
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri May 15 12:42:48 2020 +0800
    
        attempt 11
    
    commit 4d745adfdebe2f0a3fb4ba08e7cada24a3490254
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri May 15 12:21:37 2020 +0800
    
        attempt 10
    
    commit c601d79008782f5a0bd7ac0034b1367f8097dd0f
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri May 15 12:03:33 2020 +0800
    
        attempt 9
    
    commit 5590d5e214570425109d9059a33310950c551030
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri May 15 11:52:11 2020 +0800
    
        attempt 8
    
    commit 5b30362819f458326857cd18f52a2027ebdc74ac
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri May 15 11:01:35 2020 +0800
    
        attempt 7
    
    commit 48f3b518c08a7fdab6d73f44afaf4e2a6f7c21f0
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri May 15 10:56:36 2020 +0800
    
        attempt 6
    
    commit bc8439fa18b8fa2ad7265bdc97de7d34bd57897d
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri May 15 10:53:56 2020 +0800
    
        attempt 5
    
    commit c58154bd642bb29bc667ec482228bbe063278b2e
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Thu May 14 20:31:49 2020 +0800
    
        attempt 4
    
    commit 081c3a66f0540625625ebc70a7c53d67655e8f49
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Thu May 14 19:47:28 2020 +0800
    
        attempt 3
    
    commit 6c4fd29f6c6c9e7a358708d6ee71e3c274ee2c9b
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Thu May 14 19:43:54 2020 +0800
    
        attempt 2
    
    commit e8a4cf2a94e48c877c3028a733c17bcac622d46d
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Thu May 14 18:21:28 2020 +0800
    
        attempt 1

--- 230.go
+++ 230.go
@@ -1,34 +1,34 @@
 package main
 
-//Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+// Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
 //
-//Note:
-//You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+// Note:
+// You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
 //
-//Example 1:
+// Example 1:
 //
-//Input: root = [3,1,4,null,2], k = 1
-//   3
-//  / \
-// 1   4
-//  \
-//   2
-//Output: 1
+// Input: root = [3,1,4,null,2], k = 1
+//    3
+//   / \
+//  1   4
+//   \
+//    2
+// Output: 1
 //
-//Example 2:
+// Example 2:
 //
-//Input: root = [5,3,6,2,4,null,null,1], k = 3
-//       5
-//      / \
-//     3   6
-//    / \
-//   2   4
-//  /
-// 1
-//Output: 3
+// Input: root = [5,3,6,2,4,null,null,1], k = 3
+//        5
+//       / \
+//      3   6
+//     / \
+//    2   4
+//   /
+//  1
+// Output: 3
 //
-//Follow up:
-//What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently? How would you optimize the kthSmallest routine?
+// Follow up:
+// What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently? How would you optimize the kthSmallest routine?
 
 /**
  * Definition for a binary tree node.
@@ -38,33 +38,88 @@ package main
  *     Right *TreeNode
  * }
  */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
 func kthSmallest(root *TreeNode, k int) int {
-	if root.Left == nil && root.Right == nil {
-		return root.Val
+	stack := make([]*TreeNode, 0)
+
+	for node := root; len(stack) > 0 || node != nil; {
+		if node != nil {
+			stack = append(stack, node)
+			node = node.Left
+		} else {
+			node = stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			k--
+			if k == 0 {
+				return node.Val
+			}
+
+			node = node.Right
+		}
 	}
 
-	val, _ := traverse(root, k)
-	return val
+	return -1
 }
 
-func traverse(node *TreeNode, k int) (int, bool) {
-	if node == nil {
-		return 0, false
-	}
+//	problems
+//	1.	not the fasted, use iteration instead
 
-	left, found := traverse(node.Left, k)
-	if found {
-		return left, found
-	}
+//	2.	not update node
 
-	if left+1 == k {
-		return node.Val, true
-	}
+//	3.	not add correct node into stack
 
-	right, found := traverse(node.Right, k-left-1)
-	if found {
-		return right, found
-	}
+//	4.	this is really hard for me to figure out how to write iterative code to
+//		traverse tree. I need to practice more about it.
 
-	return left + right + 1, false
-}
+//		I think problem comes from how is stack used. Should I put every node
+//		into it and pop it out later? Or should I only put nodes that left child
+//		is parsed. If a node has right child, does it need to put into stack?
+
+//	5.	reference from https://leetcode.com/problems/kth-smallest-element-in-a-bst/discuss/63660/3-ways-implemented-in-JAVA-(Python)%3A-Binary-Search-in-order-iterative-and-recursive
+
+//		author provides a cleaner way of iterative traversing, compare to my solution, I think most
+//		differences come from how is tree traversed.
+
+//		in-order traversal means if a node has left child, go to left child. If no left child, go
+//		self, then go right. for right child, check if left child exist, keep looping this.
+
+//		I should follow this kind of traversing process, initially put all nodes with left child
+//		into stack. Then for processing, focus on self and right child. If right child has left child,
+//		looping until no left child.
+
+//	6.	from post, there's a more beautiful way to write iterative code. the point here is to use
+//		both stack & pointer. stack saves the node that already process left child. node is the one
+//		that needs to process left child. I need to clarify whole process: always loop left child,
+//		and process self, then right child
+
+//	7.	for followups, I have no idea. reference from https://leetcode.com/problems/kth-smallest-element-in-a-bst/discuss/63659/What-if-you-could-modify-the-BST-node's-structure
+
+//		it stores count of all children below (including self), with this info, every time insertion
+//		or deletion happens, the path than includes this node will be updated, which is O(h), h is
+//		height of BST
+
+//		and search for kth element is also in O(h) time
