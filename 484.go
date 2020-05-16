d22f8f4ece74984fdb669579f5d88a9dcd87db5c
Aaron Chang <xesuschang@gmail.com>
2020-05-16 13:28:27 +0800

    Squashed commit of 484:
    commit 2058b9494cc503911681a96a53a89245330be4aa
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sat May 16 13:28:04 2020 +0800
    
        add reference
    
    commit be58c73f3a09828c1267ec90eac42a1e22c38e9d
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sat May 16 13:19:48 2020 +0800
    
        attempt 4
    
    commit ca979e2439581a302a8e65ef7e8c92b977bbe95c
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sat May 16 12:51:40 2020 +0800
    
        add reference
    
    commit d889fffedc626e1e1d898ad88a6219ff60c10a80
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sat May 16 12:14:57 2020 +0800
    
        attempt 3
    
    commit eaf74f5040a32222d3cd0610542bbf88f8a7259f
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sat May 16 11:30:20 2020 +0800
    
        attempt 2
    
    commit d29df4ba5957a4b5981e4eef07a5ee4a4cde5745
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sat May 16 09:37:20 2020 +0800
    
        attempt 1

---
+++ 484.go
@@ -0,0 +1,75 @@
+package main
+
+//  By now, you are given a secret signature consisting of character 'D' and 'I'. 'D' represents a decreasing relationship between two numbers, 'I' represents an increasing relationship between two numbers. And our secret signature was constructed by a special integer array, which contains uniquely all the different number from 1 to n (n is the length of the secret signature plus 1). For example, the secret signature "DI" can be constructed by array [2,1,3] or [3,1,2], but won't be constructed by array [3,2,4] or [2,1,3,4], which are both illegal constructing special string that can't represent the "DI" secret signature.
+//
+// On the other hand, now your job is to find the lexicographically smallest permutation of [1, 2, ... n] could refer to the given secret signature in the input.
+//
+// Example 1:
+//
+// Input: "I"
+// Output: [1,2]
+// Explanation: [1,2] is the only legal initial spectial string can construct secret signature "I", where the number 1 and 2 construct an increasing relationship.
+//
+// Example 2:
+//
+// Input: "DI"
+// Output: [2,1,3]
+// Explanation: Both [2,1,3] and [3,1,2] can construct the secret signature "DI",
+// but since we want to find the one with the smallest lexicographical permutation, you need to output [2,1,3]
+//
+// Note:
+// The input string will only contain the character 'D' and 'I'.
+// The length of input string is a positive integer and will not exceed 10,000
+
+func findPermutation(s string) []int {
+	result := make([]int, 0)
+
+	for i := 0; i <= len(s); i++ {
+		if i == len(s) || s[i] == 'I' {
+			for j, size := i+1, len(result); j > size; j-- {
+				result = append(result, j)
+			}
+		}
+	}
+
+	return result
+}
+
+//	problems
+//	1.	brute force, should use better way to find solution
+
+//	2.	use ugly way to find solution
+
+//	3.	from reference https://leetcode.com/problems/find-permutation/discuss/96613/Java-O(n)-clean-solution-easy-to-understand
+
+//		During thinking, I have occurred an idea that D is reverse increase,
+//		then I focus on how to do reverse increase.
+
+//		But author does it in another way, reverse increase is normal increase
+//		with swap. This is a clever solution, I have chance to come out but
+//		miss it. The insight to this problem is still not enough.
+
+//		Also, the other amazing part is swapping, 3 operations of XOR.
+
+//	  	 a <-> b:
+//		  a ^= b
+//		  b ^= a
+//		  a ^= b
+
+//	4.	from reference https://leetcode.com/problems/find-permutation/discuss/96663/Greedy-O(n)-JAVA-solution-with-explanation
+
+//		author has a brilliant solution to add '.' to end of s, makes coding
+//		easier.
+
+//		also, he uses != 'D' to do operation, which includes added '.'
+
+//	5.	from reference https://leetcode.com/problems/find-permutation/discuss/96644/C%2B%2B-simple-solution-in-72ms-and-9-lines
+
+//		I view D as limitation, so I try to find D, then I's value is
+//		determined by D.
+
+//		author uses array size to calculate D. difference between I's index &
+//		array size is D amount. very impressive....
+
+//	6.	stack solution from answer, when I is reached, push into stack, then
+//		pop out all numbers in stack, when D is reached, push into stack.
