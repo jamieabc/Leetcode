be01db885c3d7910c737c2e34d488f1da1ca0340
Aaron Chang <xesuschang@gmail.com>
2020-05-18 12:24:06 +0800

    Squashed commit of 889:
    commit 46ecab181f76868e74956f752596c2d7a7190d0f
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon May 18 12:23:49 2020 +0800
    
        add reference
    
    commit 049b60553f542920c5df137a3fcfe285c3d3fb23
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon May 18 12:07:52 2020 +0800
    
        attempt 3
    
    commit 125c0dfacd4fd9e0dc2ccabe4fc749a314f4ad33
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon May 18 11:32:05 2020 +0800
    
        attempt 2
    
    commit 6d947695371e7f925b6c1451be9c171a93faa835
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon May 18 11:20:36 2020 +0800
    
        attempt 1

--- 889.go
+++ 889.go
@@ -1,19 +1,19 @@
 package main
 
-//Return any binary tree that matches the given preorder and postorder traversals.
+// Return any binary tree that matches the given preorder and postorder traversals.
 //
-//Values in the traversals pre and post are distinct positive integers.
-//
-//
-//
-//Example 1:
-//
-//Input: pre = [1,2,4,5,3,6,7], post = [4,5,2,6,7,3,1]
-//Output: [1,2,3,4,5,6,7]
+// Values in the traversals pre and post are distinct positive integers.
 //
 //
 //
-//Note:
+// Example 1:
+//
+// Input: pre = [1,2,4,5,3,6,7], post = [4,5,2,6,7,3,1]
+// Output: [1,2,3,4,5,6,7]
+//
+//
+//
+// Note:
 //
 //    1 <= pre.length == post.length <= 30
 //    pre[] and post[] are both permutations of 1, 2, ..., pre.length.
@@ -28,6 +28,47 @@ package main
  * }
  */
 func constructFromPrePost(pre []int, post []int) *TreeNode {
+	length := len(pre)
+	root := &TreeNode{
+		Val: pre[0],
+	}
+
+	traverse(pre[1:], post[:length-1], root)
+
+	return root
+}
+
+func traverse(pre, post []int, parent *TreeNode) {
+	length := len(pre)
+
+	if length == 0 {
+		return
+	}
+
+	parent.Left = &TreeNode{
+		Val: pre[0],
+	}
+
+	var leftEnd int
+	for i := length - 1; i >= 0; i-- {
+		if post[i] == pre[0] {
+			leftEnd = i
+			break
+		}
+	}
+
+	traverse(pre[1:leftEnd+1], post[:leftEnd], parent.Left)
+
+	if leftEnd < length-1 {
+		// with right child
+		parent.Right = &TreeNode{
+			Val: pre[leftEnd+1],
+		}
+		traverse(pre[leftEnd+2:], post[leftEnd+1:length-1], parent.Right)
+	}
+}
+
+func constructFromPrePost2(pre []int, post []int) *TreeNode {
 	preLength := len(pre)
 	if preLength == 0 {
 		return nil
@@ -69,3 +110,16 @@ func constructFromPrePost(pre []int, post []int) *TreeNode {
 // 5. optimize, length of pre & post are corresponded, so there's no need to calculate right index at post, just get length of left subtree, remaining elements are right subtree
 // 6. optimize, at it comes to situation of one child is empty, no need to distinguish, it will put all nodes into right
 // 7. optimize, no need to check when length is 2, because alogrithm will put all into right side
+
+//	8. 	rewrite, forget about situation when length == 2, still with 2
+//		possibilities
+
+//	9.	reference from https://leetcode.com/problems/construct-binary-tree-from-preorder-and-postorder-traversal/discuss/161268/C%2B%2BJavaPython-One-Pass-Real-O(N)
+
+//		this is really clever solution, I don't know how author comes up.
+//		for pre-order, it travers left most node by lowest height to highest
+//		height. post-order traverse directly to left-most last node
+
+//		with this property, keep build tree using pre-order. when pre-order
+//		number equals post-order means that sub tree is all built, then
+//		starts to build right child tree
