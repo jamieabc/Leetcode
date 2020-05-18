7a969dc12d4e3f6a9a4772df72831dfb64af239c
Aaron Chang <xesuschang@gmail.com>
2020-05-18 09:07:11 +0800

    Squashed commit of 156:
    commit 972f4e98e8bf414c907fc30973e743fe4bd2c40d
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon May 18 09:06:45 2020 +0800
    
        attempt 10
    
    commit 99b2a5f844528e2e6de8012b37be59f69747ba6d
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon May 18 07:43:01 2020 +0800
    
        attempt 9
    
    commit def8bfe2bf42cd36714f74322fdd22815ee4e836
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sun May 17 20:56:10 2020 +0800
    
        attempt 8
    
    commit 0ee7c90c39498aea055e2e3f5a99c557e4dfad7a
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sun May 17 20:55:17 2020 +0800
    
        attempt 7
    
    commit 1996cd96f36969fc7895447bb40d8c47901213d8
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sun May 17 20:54:13 2020 +0800
    
        attempt 6
    
    commit efc27f971d8f2441c203fa9488565b1592f24c5f
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sun May 17 20:48:18 2020 +0800
    
        attempt 5
    
    commit 93cf803edb9d6386c6719ac07eb8855e53608662
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sun May 17 20:46:28 2020 +0800
    
        attempt 4
    
    commit bbd94dacad561ac45bf3f5833ff80f37cbfc0dbf
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sun May 17 14:35:00 2020 +0800
    
        attempt 3
    
    commit 85e28be4bb4218aaa29fd21b92b10dfd500a7416
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sun May 17 14:31:56 2020 +0800
    
        attempt 2
    
    commit 92ab27af9e16d3ebb0d5404727c33a117ca96db0
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sun May 17 14:26:05 2020 +0800
    
        attempt 1

---
+++ 156.go
@@ -0,0 +1,174 @@
+package main
+
+// Given a binary tree where all the right nodes are either leaf nodes with a sibling (a left node that shares the same parent node) or empty, flip it upside down and turn it into a tree where the original right nodes turned into left leaf nodes. Return the new root.
+//
+// Example:
+//
+// Input: [1,2,3,4,5]
+//
+//     1
+//    / \
+//   2   3
+//  / \
+// 4   5
+//
+// Output: return the root of the binary tree [4,5,2,#,#,3,1]
+//
+//    4
+//   / \
+//  5   2
+//     / \
+//    3   1
+//
+// Clarification:
+//
+// Confused what [4,5,2,#,#,3,1] means? Read more below on how binary tree is serialized on OJ.
+//
+// The serialization of a binary tree follows a level order traversal, where '#' signifies a path terminator where no node exists below.
+//
+// Here's an example:
+//
+//    1
+//   / \
+//  2   3
+//     /
+//    4
+//     \
+//      5
+//
+// The above binary tree is serialized as [1,2,3,#,#,4,#,#,5].
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func upsideDownBinaryTree(root *TreeNode) *TreeNode {
+	var next, prev, tmp *TreeNode
+	cur := root
+
+	for cur != nil {
+		// preserve original tree order
+		next = cur.Left
+
+		// flip tree
+		cur.Left = tmp
+
+		// preserve right node for next round
+		tmp = cur.Right
+
+		// flip tree
+		cur.Right = prev
+
+		// next round
+		prev = cur
+		cur = next
+	}
+
+	return prev
+}
+
+func upsideDownBinaryTree2(root *TreeNode) *TreeNode {
+	if root == nil || root.Left == nil {
+		return root
+	}
+
+	var node *TreeNode
+	stack := make([]*TreeNode, 0)
+
+	// find new root (the left most node from original tree)
+	for node = root; node.Left != nil; node = node.Left {
+		stack = append(stack, node)
+	}
+	newRoot := node
+
+	for len(stack) > 0 {
+		node = stack[len(stack)-1]
+
+		if node.Right != nil {
+			node.Left.Left = node.Right
+		}
+		node.Left.Right = node
+		node.Left = nil
+		node.Right = nil
+
+		stack = stack[:len(stack)-1]
+	}
+
+	return newRoot
+}
+
+func upsideDownBinaryTree3(root *TreeNode) *TreeNode {
+	if root == nil || root.Left == nil {
+		return root
+	}
+
+	return traverse(root)
+}
+
+// return traverse(root)
+func traverse(node *TreeNode) *TreeNode {
+	// new root must be left most node
+	if node.Left == nil {
+		return node
+	}
+
+	newRoot := traverse(node.Left)
+
+	if node.Right != nil {
+		node.Left.Left = node.Right
+	}
+
+	node.Left.Right = node
+	node.Left = nil
+	node.Right = nil
+
+	return newRoot
+}
+
+//	problems
+//	1.	beware of corner cases, especially when empty node, single node
+
+//	2.	limitation on right, so left could be empty or single
+
+//	3.	reference from https://www.geeksforgeeks.org/flip-binary-tree/
+//		it's more clear explanation
+
+//	4.	having problem updating function parameter of pointer, then found
+//		explanation from https://github.com/golang/go/issues/25932
+
+//	5.	wrong checking, when right child is nil, still need to flip tree
+
+//	6.	inspired from https://leetcode.com/problems/binary-tree-upside-down/discuss/49412/Clean-Java-solution
+
+//		no need to have additional newRoot as function parameter, can just
+//		make it a return value. And flipping tree can use recursive only
+
+//	7.	the most important information is that left-mode node of original
+//		tree will be new tree's root. Because I didn't know this, my original
+//		method starts from right-most node.
+
+//	8.	reference from https://leetcode.com/problems/binary-tree-upside-down/discuss/49432/Easy-O(n)-iteration-solution-Java
+
+//		visualize flow:
+//			1
+//		  2   3    =>     2 (next)    1 (prev)    3 (tmp)
+//		4   5           4   5
+
+//		=>         2 (prev)  4 (next)   5 (tmp)
+//				 3   1
+
+//		=>		4
+//			  5    2
+//				 3   1
+
+//		I can understand what it wants to do, but cannot know how author
+//		come up idea...
+
+//		But some clue I can think ok:
+//		- when flipping tree, right child be will changed into left node
+//		- since original tree structure is tear apart, it needs to store
+//		  next & prev nodes
