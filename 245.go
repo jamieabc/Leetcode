37160c06241d1d53a3b09d37d8b403e9bf197446
Aaron Chang <xesuschang@gmail.com>
2020-05-18 21:00:36 +0800

    Squashed commit of 245:
    commit 0d32ac35072dac0a071b018432e7da15f144cc20
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon May 18 20:59:21 2020 +0800
    
        attempt 9
    
    commit 14e327266b5c4be02651e2dd628146c2273602a1
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon May 18 20:58:06 2020 +0800
    
        attempt 8
    
    commit efc7278db24efb5d8bcddc30b02459bd26f25794
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon May 18 20:49:48 2020 +0800
    
        attempt 7
    
    commit dda028667a7f14f6527bbea77bed4b845db173bb
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon May 18 20:47:37 2020 +0800
    
        attempt 6
    
    commit a27d53250a6b5c6157b3160a996f665a5edcc294
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon May 18 20:46:52 2020 +0800
    
        attempt 5
    
    commit ef43fba7cd1870f98d5bb5eeae1779f8ac41012e
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon May 18 13:56:25 2020 +0800
    
        attempt 4
    
    commit 90c4de7638511e51498b1bda210e5312f97c6507
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon May 18 13:48:19 2020 +0800
    
        attempt 3
    
    commit 0af378908e66d6b99a63c17c0ac7a4ed8b942734
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon May 18 13:31:38 2020 +0800
    
        attempt 2
    
    commit 0e0e0a2c4a88f84421957bcf8d236c1574a6016e
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon May 18 13:23:19 2020 +0800
    
        attempt 1

---
+++ 245.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"math"
+)
+
+// Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
+//
+// word1 and word2 may be the same and they represent two individual words in the list.
+//
+// Example:
+// Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
+//
+// Input: word1 = “makes”, word2 = “coding”
+// Output: 1
+//
+// Input: word1 = "makes", word2 = "makes"
+// Output: 3
+//
+// Note:
+// You may assume word1 and word2 are both in the list.
+
+func shortestWordDistance(words []string, word1 string, word2 string) int {
+	minDist := math.MaxInt32
+
+	prev := -1
+	same := word1 == word2
+	for i := range words {
+		if words[i] == word1 || words[i] == word2 {
+			if prev != -1 && (same || words[prev] != words[i]) {
+				minDist = min(minDist, i-prev)
+			}
+			prev = i
+		}
+	}
+
+	return minDist
+}
+
+func min(i, j int) int {
+	if i <= j {
+		return i
+	}
+	return j
+}
+
+//	problems
+//	1.	same word should exist different position
+
+//	2.	too slow, use binary search
+
+//	3.	still too slow, iterate through array once to find
+
+//	4. 	wrong condition, as long as larger than 0 are valid
+
+//	5.	forget to check when same word, ith word need to match
+
+//	6.	inspired from https://leetcode.com/problems/shortest-word-distance-iii/discuss/67095/Short-Java-solution-10-lines-O(n)-modified-from-Shortest-Word-Distance-I
+
+//		only one extra variable is needed, not 2, and can combine same /
+//		different cases together
+
+//		also, in discussion, string comparison is expensive, no need to do in
+//		loop, can make it outside
