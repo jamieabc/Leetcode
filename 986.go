95e6bd3ac01f40621bcdfc17c939f7e598b33de6
Aaron Chang <xesuschang@gmail.com>
2020-05-20 18:42:52 +0800

    Squashed commit of 986:
    commit d1fb0172dab729afb4ffd355827fe41ac8f76c49
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Wed May 20 18:42:24 2020 +0800
    
        attempt 4
    
    commit 1bd0b14a85c149ffb5e2b8fd2ce755ac2ebff38f
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Wed May 20 18:39:13 2020 +0800
    
        attempt 3
    
    commit 3ada30f19bed21bc44d83cf1ac92017f50382323
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Wed May 20 18:37:59 2020 +0800
    
        attempt 2
    
    commit 8ba0cdff9ee736773dc787340c66bc727c7f10c5
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Wed May 20 18:30:06 2020 +0800
    
        attempt 1

---
+++ 986.go
@@ -0,0 +1,81 @@
+package main
+
+// Given two lists of closed intervals, each list of intervals is pairwise disjoint and in sorted order.
+//
+// Return the intersection of these two interval lists.
+//
+// (Formally, a closed interval [a, b] (with a <= b) denotes the set of real numbers x with a <= x <= b.  The intersection of two closed intervals is a set of real numbers that is either empty, or can be represented as a closed interval.  For example, the intersection of [1, 3] and [2, 4] is [2, 3].)
+//
+//
+//
+// Example 1:
+//
+// Input: A = [[0,2],[5,10],[13,23],[24,25]], B = [[1,5],[8,12],[15,24],[25,26]]
+// Output: [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
+// Reminder: The inputs and the desired output are lists of Interval objects, and not arrays or lists.
+//
+//
+//
+// Note:
+//
+// 0 <= A.length < 1000
+// 0 <= B.length < 1000
+// 0 <= A[i].start, A[i].end, B[i].start, B[i].end < 10^9
+//
+// NOTE: input types have been changed on April 15, 2019. Please reset to default code definition to get new method signature.
+
+func intervalIntersection(A [][]int, B [][]int) [][]int {
+	result := make([][]int, 0)
+
+	if len(A) == 0 || len(B) == 0 {
+		return result
+	}
+
+	for i, j := 0, 0; i < len(A) && j < len(B); {
+		if A[i][1] < B[j][0] {
+			i++
+		} else if A[i][0] > B[j][1] {
+			j++
+		} else {
+			result = append(result, []int{max(A[i][0], B[j][0]), min(A[i][1], B[j][1])})
+			if A[i][1] < B[j][1] {
+				i++
+			} else if A[i][1] > B[j][1] {
+				j++
+			} else {
+				i++
+				j++
+			}
+		}
+	}
+
+	return result
+}
+
+func max(i, j int) int {
+	if i >= j {
+		return i
+	}
+	return j
+}
+
+func min(i, j int) int {
+	if i <= j {
+		return i
+	}
+	return j
+}
+
+//	problems
+//	1.	inspired by https://leetcode.com/problems/interval-list-intersections/discuss/231108/C%2B%2B-O(n)-%22merge-sort%22
+
+//		when checking intersect, some condition can already be used to
+//		decide i or j should increment
+
+//		also, then checking of start point can use max to decide
+
+//	2.	inspired from https://leetcode.com/problems/interval-list-intersections/discuss/231122/Java-two-pointers-O(m-%2B-n)
+
+//		length invalid doesn't need to check
+
+//	3.	reference from sample code, when both ends are same, increase both
