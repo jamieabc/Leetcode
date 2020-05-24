5dcb344e0413bc4501eabc33d29b775731b13450
Aaron Chang <xesuschang@gmail.com>
2020-05-24 11:51:29 +0800

    Squashed commit of 877:
    commit a4ff0e706c9a0ac0e744793144565cee69bdda5a
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sun May 24 11:51:13 2020 +0800
    
        add reference
    
    commit e25c434135bbf4a86fab74f1c2efe18af411dff7
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sun May 24 11:40:33 2020 +0800
    
        attempt 8
    
    commit 73fb4020017f3ef179f3d880caf2b5cf47f3a656
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sun May 24 11:20:22 2020 +0800
    
        attempt 7
    
    commit f73ceedf577926f184342c622ac8e4be10fbbba5
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sun May 24 11:13:35 2020 +0800
    
        attempt 6
    
    commit 175f61fa7a0c6bf146e91ecf353afd645626e91b
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sun May 24 09:03:39 2020 +0800
    
        attempt 5
    
    commit 8b44c521115aae40a2b9d308c5dab341f73ae329
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sun May 24 08:51:56 2020 +0800
    
        attempt 4
    
    commit 07db7dc2b041f4c73c70768aa714f35d40dd3e81
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sun May 24 08:41:08 2020 +0800
    
        attempt 3
    
    commit 769336152a74c30e37b8de1cef5b96ecf1a78f8d
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sat May 23 12:41:07 2020 +0800
    
        attempt 2
    
    commit 6ba07fdd880bd3e4a579c899f18ee349babd714f
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sat May 23 12:22:17 2020 +0800
    
        attempt 1

---
+++ 877.go
@@ -0,0 +1,131 @@
+package main
+
+// Alex and Lee play a game with piles of stones.  There are an even number of piles arranged in a row, and each pile has a positive integer number of stones piles[i].
+//
+// The objective of the game is to end with the most stones.  The total number of stones is odd, so there are no ties.
+//
+// Alex and Lee take turns, with Alex starting first.  Each turn, a player takes the entire pile of stones from either the beginning or the end of the row.  This continues until there are no more piles left, at which point the person with the most stones wins.
+//
+// Assuming Alex and Lee play optimally, return True if and only if Alex wins the game.
+//
+//
+//
+// Example 1:
+//
+// Input: [5,3,4,5]
+// Output: true
+// Explanation:
+// Alex starts first, and can only take the first 5 or the last 5.
+// Say he takes the first 5, so that the row becomes [3, 4, 5].
+// If Lee takes 3, then the board is [4, 5], and Alex takes 5 to win with 10 points.
+// If Lee takes the last 5, then the board is [3, 4], and Alex takes 4 to win with 9 points.
+// This demonstrated that taking the first 5 was a winning move for Alex, so we return true.
+//
+//
+//
+// Note:
+//
+//     2 <= piles.length <= 500
+//     piles.length is even.
+//     1 <= piles[i] <= 500
+//     sum(piles) is odd.
+
+func stoneGame(piles []int) bool {
+	length := len(piles)
+	dp := make([]int, length)
+	for i := range dp {
+		dp[i] = piles[i]
+	}
+
+	for d := 1; d < length; d++ {
+		for i := 0; i < length-d; i++ {
+			dp[i] = max(piles[i]-dp[i], piles[i+d]-dp[i+d-1])
+		}
+	}
+
+	return dp[length-1] > 0
+}
+
+func stoneGame2(piles []int) bool {
+	length := len(piles)
+	dp := make([][]int, length)
+	for i := range dp {
+		dp[i] = make([]int, length)
+		dp[i][i] = piles[i]
+	}
+
+	for d := 1; d < length; d++ {
+		for i := 0; i < length-d; i++ {
+			dp[i][i+d] = max(piles[i]-dp[i+1][i+d], piles[i+d]-dp[i][i+d-1])
+		}
+	}
+
+	return dp[0][length-1] > 0
+}
+
+func max(i, j int) int {
+	if i >= j {
+		return i
+	}
+	return j
+}
+
+func stoneGame1(piles []int) bool {
+	return play(piles, 0, 0, 0, 0, len(piles)-1)
+}
+
+func play(piles []int, alex, lee, turn, start, end int) bool {
+	if start > end {
+		return alex > lee
+	}
+
+	nextTurn := turn ^ 1
+
+	if turn == 0 {
+		return play(piles, alex+piles[start], lee, start+1, end, nextTurn) || play(piles, alex+piles[end], lee, start, end-1, nextTurn)
+	} else {
+		return play(piles, alex, lee+piles[start], start+1, end, nextTurn) || play(piles, alex, lee+piles[end], start, end-1, nextTurn)
+	}
+}
+
+//	problems
+//	1.	too slow, try to trim some un-necessary branch, time complexity is
+//		O(2^n), cause at every length, people can choose either first or last
+
+//	2.	try not to modify slice but updating index, see if it's faster
+
+//	3.	inspired from https://leetcode.com/problems/stone-game/discuss/154610/DP-or-Just-return-true
+
+//		first thing is that when piles length is even, alex can choose all
+//		even or odd pile, and since total stones are odd which means alex
+//		can always win
+
+//		this is a clever insight, I didn't even take time to observe
+//		problem and find out this simple conclusion. I guess the problem is
+//		about finding rules for alex:
+//		- choose even piles: pick first element, then depending on lee's
+//		  choice, if lee picks first then alex pick first, if lee picks last
+//		  then alex picks last
+//		- choose odd piles: pick last element, then depending on lee's
+//		  choice, if lee picks first then alex picks first, if lee picks
+//		  last then alex picks last
+
+//		this kind of conclusion comes with 2 presumptions: total stones are
+//		odd, and pile length is even
+
+//		when using 2D dp, it's very clever. goal is alex score > lee score.
+//		but dp can pursuit one variable, which become diff = alex - lee.
+//		adn score relationship can be decided by choose first or choose
+//		last, choose whatever max of it:
+//		dp(i, j) = max(p[i] - dp(i+1, j), p[j] - dp(i, j-1))
+
+//		author also uses beautiful dp, starts from dp[i][i] then expands
+//		to dp[i][j]
+
+//		at last also reduces 2D dp to 1D dp, kind of observing dp formula,
+//		dp[i][j] relates to dp(i+1, j) & dp(i, j-1), and dp(i+1, j) is
+//		1D dp(i), dp(i, j-1) is dp(i-1)
+
+//	4.	add another reference https://leetcode.com/problems/stone-game/discuss/154660/Java-This-is-minimax-%2B-dp-(fully-detailed-explanation-%2B-generalization-%2B-easy-understand-code)
+
+//		author explains how dp come out, and the time complexity
