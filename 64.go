394ff6b30e652e703435e5ff7774c2e19c019bba
Aaron Chang <xesuschang@gmail.com>
2020-05-24 12:25:46 +0800

    Squashed commit of 64:
    commit faceb74e2c672aa9e29f298f62112a83fbef21a1
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sun May 24 12:24:13 2020 +0800
    
        attempt 4
    
    commit ad056a744e2348c6ab7afc30fdd81b871563b32d
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sun May 24 12:04:25 2020 +0800
    
        attempt 3
    
    commit ffc5802d6007c72904124cea4c942f3b0e66aa65
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sun May 24 12:01:53 2020 +0800
    
        attempt 2
    
    commit e2fd3ba3d845d8e738804bbb978807d4804fa477
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sun May 24 12:00:01 2020 +0800
    
        attempt 1

---
+++ 64.go
@@ -0,0 +1,83 @@
+package main
+
+// Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+//
+// Note: You can only move either down or right at any point in time.
+//
+// Example:
+//
+// Input:
+// [
+//   [1,3,1],
+//   [1,5,1],
+//   [4,2,1]
+// ]
+// Output: 7
+// Explanation: Because the path 1→3→1→1→1 minimizes the sum.
+
+func minPathSum(grid [][]int) int {
+	y := len(grid)
+	if y == 0 {
+		return 0
+	}
+	x := len(grid[0])
+	dp := make([]int, x)
+	dp[0] = grid[0][0]
+
+	for i := 1; i < x; i++ {
+		dp[i] = dp[i-1] + grid[0][i]
+	}
+
+	for i := 1; i < y; i++ {
+		for j := range grid[0] {
+			if j > 0 {
+				dp[j] = grid[i][j] + min(dp[j-1], dp[j])
+			} else {
+				dp[j] = grid[i][j] + dp[j]
+			}
+		}
+	}
+
+	return dp[x-1]
+}
+
+func min(i, j int) int {
+	if i <= j {
+		return i
+	}
+	return j
+}
+
+func minPathSum1(grid [][]int) int {
+	if len(grid) == 0 {
+		return 0
+	}
+
+	return traverse(grid, len(grid)-1, len(grid[0])-1)
+}
+
+func traverse(grid [][]int, i, j int) int {
+	if i < 0 || j < 0 {
+		return 0
+	}
+
+	if i > 0 && j > 0 {
+		return grid[i][j] + min(traverse(grid, i-1, j), traverse(grid, i, j-1))
+	} else if i != 0 {
+		return grid[i][j] + traverse(grid, i-1, j)
+	} else if j != 0 {
+		return grid[i][j] + traverse(grid, i, j-1)
+	} else {
+		return grid[0][0]
+	}
+}
+
+//  problems
+
+//  1.  wrong index
+
+//	2.	array could not be square, dp size is column size
+
+//	3.	inspired from https://leetcode.com/problems/minimum-path-sum/discuss/344980/Java.-Details-from-Recursion-to-DP.
+
+//		author uses recursion
