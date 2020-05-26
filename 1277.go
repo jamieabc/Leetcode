b472685982dce5e9d18045782fc6df1a9cec0351
Aaron Chang <xesuschang@gmail.com>
2020-05-26 10:55:10 +0800

    Squashed commit of 1277:
    commit 70a304a2763e6a95e9605dddecadec99dc7736bd
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Tue May 26 10:54:53 2020 +0800
    
        add reference
    
    commit 23a574a11e9decfb02eb9a13e141e354bffa9565
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Tue May 26 10:48:20 2020 +0800
    
        attempt 7
    
    commit 4e183fefed9e3a429521b5f9c5e9a0e1d35535a2
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Tue May 26 10:47:06 2020 +0800
    
        attempt 6
    
    commit 360fe9d9fc78f0ed09c49451639bf92dc177741b
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Tue May 26 10:43:35 2020 +0800
    
        attempt 5
    
    commit 964ec1f4e6fa35836ba1b5759817c60db5742274
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Tue May 26 10:40:12 2020 +0800
    
        attempt 4
    
    commit aacaee781ee9a40fc11976088fd16e5ceeb2f80b
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Tue May 26 10:31:00 2020 +0800
    
        attempt 3
    
    commit 018204a133ebf8666c90d59aae0d75c31a589bd5
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Tue May 26 10:29:37 2020 +0800
    
        attempt 2
    
    commit 20e583a39d1a0b05f3d1237d25cc04e693793256
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Tue May 26 10:19:52 2020 +0800
    
        attempt 1

---
+++ 1277.go
@@ -0,0 +1,139 @@
+package main
+
+// Given a m * n matrix of ones and zeros, return how many square submatrices have all ones.
+//
+//
+//
+// Example 1:
+//
+// Input: matrix =
+// [
+//   [0,1,1,1],
+//   [1,1,1,1],
+//   [0,1,1,1]
+// ]
+// Output: 15
+// Explanation:
+// There are 10 squares of side 1.
+// There are 4 squares of side 2.
+// There is  1 square of side 3.
+// Total number of squares = 10 + 4 + 1 = 15.
+//
+// Example 2:
+//
+// Input: matrix =
+// [
+//   [1,0,1],
+//   [1,1,0],
+//   [1,1,0]
+// ]
+// Output: 7
+// Explanation:
+// There are 6 squares of side 1.
+// There is 1 square of side 2.
+// Total number of squares = 6 + 1 = 7.
+//
+//
+//
+// Constraints:
+//
+//     1 <= arr.length <= 300
+//     1 <= arr[0].length <= 300
+//     0 <= arr[i][j] <= 1
+
+func countSquares(matrix [][]int) int {
+	y := len(matrix)
+	if y == 0 {
+		return 0
+	}
+	x := len(matrix[0])
+
+	dp := make([]int, x)
+	var count, prev int
+
+	for i := range matrix {
+		prev = 0
+		for j := range matrix[0] {
+			if i == 0 || j == 0 {
+				prev, dp[j] = dp[j], matrix[i][j]
+			} else {
+				if matrix[i][j] == 1 && prev > 0 && dp[j-1] > 0 && dp[j] > 0 {
+					prev, dp[j] = dp[j], min(min(dp[j-1], dp[j]), prev)+1
+				} else {
+					prev, dp[j] = dp[j], matrix[i][j]
+				}
+			}
+
+			if dp[j] > 0 {
+				count += dp[j]
+			}
+		}
+	}
+
+	return count
+}
+
+func min(i, j int) int {
+	if i <= j {
+		return i
+	}
+	return j
+}
+
+func countSquares1(matrix [][]int) int {
+	y := len(matrix)
+	if y == 0 {
+		return 0
+	}
+	x := len(matrix[0])
+
+	dp := make([][]int, y)
+	for i := range dp {
+		dp[i] = make([]int, x)
+	}
+
+	var count int
+
+	for i := range matrix {
+		for j := range matrix[0] {
+			if i == 0 || j == 0 {
+				dp[i][j] = matrix[i][j]
+			} else {
+				if matrix[i][j] == 1 {
+					if dp[i-1][j] > 0 && dp[i][j-1] > 0 && dp[i-1][j-1] > 0 {
+						dp[i][j] = min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1
+					} else {
+						dp[i][j] = matrix[i][j]
+					}
+				}
+			}
+
+			if dp[i][j] > 0 {
+				count += dp[i][j]
+			}
+		}
+	}
+
+	return count
+}
+
+func min(i, j int) int {
+	if i <= j {
+		return i
+	}
+	return j
+}
+
+//	problems
+//	1.	wrong logic when calculate square
+
+//	2.	i think it can be done in 1D dp, because dp calculation only relates
+//		to dp[i-1][j], dp[i][j-1], dp[i-1][j-1]
+
+//	3.	wrong place to update/reset prev
+
+//	4.	reference from sample code and https://leetcode.com/problems/count-square-submatrices-with-all-ones/discuss/441306/JavaC%2B%2BPython-DP-solution
+
+//		it can use original matrix to store dp, so not extra memory space is needed
+
+//		description of dp is precise, dp[i][j] means the size of biggest square with A[i][j] as bottom-right corner.
