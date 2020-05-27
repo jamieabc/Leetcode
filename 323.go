18778153d535211852764076943d97fbd0e143c9
Aaron Chang <xesuschang@gmail.com>
2020-05-27 08:53:59 +0800

    Squashed commit of 323:
    commit 2ecbe405ed749f67b0a4decc8cb645c92527af35
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Wed May 27 08:53:38 2020 +0800
    
        add reference
    
    commit 996a466a15e3febb0a0d94a576b059244140b06a
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Wed May 27 08:10:34 2020 +0800
    
        attempt 10
    
    commit c444b4828cdc65727212ff77399c5ae1e3e6cc6b
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Wed May 27 07:50:01 2020 +0800
    
        attempt 9
    
    commit a242e261f1402ba6afc667345521202641a2173d
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Wed May 27 07:38:31 2020 +0800
    
        attempt 8
    
    commit 51ffe988a9de0b6338a0ddc3d794c54dfea88df6
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Tue May 26 20:43:22 2020 +0800
    
        attempt 7
    
    commit bad5e4f1e2e6ec56543354f1323fee0277ebf755
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Tue May 26 19:25:38 2020 +0800
    
        attempt 6
    
    commit dd8bb0a9d85e79a6112706de3262257f5dee0a3d
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Tue May 26 18:47:21 2020 +0800
    
        attempt 5
    
    commit af1c722d5fc0d229b836d23676d9d26224122720
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Tue May 26 18:39:42 2020 +0800
    
        attempt 4
    
    commit c0c78abc9f3d1d3357dca7627f4e67b5f371bd8c
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Tue May 26 18:38:53 2020 +0800
    
        attempt 3
    
    commit 3d7acc1c046c5fcec9cc0fb7ee28b3a9c75b9db6
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Tue May 26 18:24:36 2020 +0800
    
        attempt 2
    
    commit efb839e6cc46397137b1c074460280b09864924b
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Tue May 26 18:08:11 2020 +0800
    
        attempt 1

---
+++ 323.go
@@ -0,0 +1,199 @@
+package main
+
+import "fmt"
+
+// Given n nodes labeled from 0 to n - 1 and a list of undirected edges (each edge is a pair of nodes), write a function to find the number of connected components in an undirected graph.
+//
+// Example 1:
+//
+// Input: n = 5 and edges = [[0, 1], [1, 2], [3, 4]]
+//
+//      0          3
+//      |          |
+//      1 --- 2    4
+//
+// Output: 2
+//
+// Example 2:
+//
+// Input: n = 5 and edges = [[0, 1], [1, 2], [2, 3], [3, 4]]
+//
+//      0           4
+//      |           |
+//      1 --- 2 --- 3
+//
+// Output:  1
+//
+// Note:
+// You can assume that no duplicate edges will appear in edges. Since all edges are undirected, [0, 1] is the same as [1, 0] and thus will not appear together in edges.
+
+func countComponents(n int, edges [][]int) int {
+	count := n
+	arr := make([]int, n)
+	for i := range arr {
+		arr[i] = i
+	}
+
+	for _, e := range edges {
+		r1 := parent(arr, e[0])
+		r2 := parent(arr, e[1])
+
+		if r1 != r2 {
+			m := min(r1, r2)
+			arr[r1] = m
+			arr[r2] = m
+			count--
+		}
+	}
+	return count
+}
+
+func parent(arr []int, n int) int {
+	if arr[n] != n {
+		arr[n] = parent(arr, arr[n])
+	}
+	return arr[n]
+}
+
+func min(i, j int) int {
+	if i <= j {
+		return i
+	}
+	return j
+}
+
+func countComponents1(n int, edges [][]int) int {
+	if n == 0 {
+		return 0
+	}
+
+	arr := make([]int, n)
+	for i := range arr {
+		arr[i] = -1
+	}
+	stack := make([]int, 0)
+
+	for _, e := range edges {
+		small, large := smallLarge(e[0], e[1])
+
+		if arr[large] == -1 && arr[small] == -1 {
+			// no data, set one
+			arr[small] = small
+			arr[large] = small
+		} else if arr[large] != -1 && arr[small] != -1 {
+			target := min(parent(arr, large), parent(arr, small))
+			if arr[large] != target {
+				stack = append(stack, arr[large])
+			}
+			if arr[small] != target {
+				stack = append(stack, arr[small])
+			}
+
+			for len(stack) != 0 {
+				s := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+
+				if arr[s] != target {
+					stack = append(stack, arr[s])
+					arr[s] = target
+				}
+			}
+		} else if arr[large] != -1 {
+			arr[small] = arr[large]
+		} else {
+			arr[large] = arr[small]
+		}
+	}
+
+	var count int
+	for i := range arr {
+		if arr[i] == i || arr[i] == -1 {
+			count++
+		}
+	}
+
+	return count
+}
+
+func parent(arr []int, n int) int {
+	if arr[n] != n {
+		arr[n] = parent(arr, arr[n])
+	}
+	return arr[n]
+}
+
+func min(i, j int) int {
+	if i <= j {
+		return i
+	}
+	return j
+}
+
+func countComponents2(n int, edges [][]int) int {
+	mapping := make(map[int][]int)
+
+	for _, e := range edges {
+		mapping[e[0]] = append(mapping[e[0]], e[1])
+		mapping[e[1]] = append(mapping[e[1]], e[0])
+	}
+
+	var count int
+	stack := make([]int, 0)
+	arr := make([]bool, n)
+
+	for i := range arr {
+		if !arr[i] {
+			stack := append(stack, i)
+
+			for len(stack) != 0 {
+				s := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+
+				if !arr[s] {
+					arr[s] = true
+					stack = append(stack, mapping[s]...)
+				}
+			}
+			count++
+		}
+	}
+
+	return count
+}
+
+//	problems
+//	1.	traversing route should not be order dependent
+
+//	2.	from test case, even if node is not connected, still treat it as
+//		one component
+
+//	3.	the algo is still order dependent
+
+//	4.	I think the algo is really bad, need to rethink from beginning. route
+//		could be circular, so need a way to detect circular (already
+//		traveled place). The point is that parsing data implies algo should
+//		be order independent, cause it might exist 0-1-2  3-4-5 0-4, first
+//		and second interval is connected when 0-4 is encountered.
+
+//	5.	inspired from https://leetcode.com/problems/number-of-connected-components-in-an-undirected-graph/discuss/77574/Easiest-2ms-Java-Solution
+
+//		the problem is actually connect number together, so it's quite
+//		straight forward to first assume all of them are disjoint, and
+//		for each edge, find it's first & second's root and connect these
+//		two roots together, reduce count by 1
+
+//	6. 	when updating root1 & root2, it should update root1 & root2's value
+//		but not child (e0, e1)
+
+//	7.	inspired from https://leetcode.com/problems/number-of-connected-components-in-an-undirected-graph/discuss/77578/Java-concise-DFS
+
+//		when traversing, it's important to mark all connect points, e.g.
+//		[1, 5] means 1 -> 5 & 5 -> 1, with this data, I can avoit order
+//		traverse dependent problem.
+
+//		time complexity is O(V+E), every node and edges are visited
+//		space complexity O(V+E)
+
+//	8.	reference https://www.cs.princeton.edu/~rs/AlgsDS07/01UnionFind.pdf
+
+//		explains find & union
