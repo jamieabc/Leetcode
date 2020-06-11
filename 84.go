7731c3515bf5986139c3953f05b592d92a2f024b
Aaron Chang <xesuschang@gmail.com>
2020-06-11 12:52:22 +0800

    Squashed commit of 84:
    commit 39fc84d810cda3b1c4f2839f6ab9e897aeb94145
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Thu Jun 11 12:47:32 2020 +0800
    
        attempt 12
    
    commit ea480f10d384846715c6ba0da644d313a5e3e380
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Thu Jun 11 11:42:00 2020 +0800
    
        add reference
    
    commit 550972f7bf1b2c885f02393b36eccd9638e9cee6
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Thu Jun 11 11:13:09 2020 +0800
    
        attempt 11
    
    commit 218b1b7333df2f6459454fecba43cf098f64a9a4
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Thu Jun 11 11:03:27 2020 +0800
    
        attempt 10

--- 84.go
+++ 84.go
@@ -1,23 +1,120 @@
 package main
 
-//Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.
+// Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.
 //
 //
 //
 //
-//Above is a histogram where width of each bar is 1, given height = [2,1,5,6,2,3].
+// Above is a histogram where width of each bar is 1, given height = [2,1,5,6,2,3].
 //
 //
 //
 //
-//The largest rectangle is shown in the shaded area, which has area = 10 unit.
+// The largest rectangle is shown in the shaded area, which has area = 10 unit.
 //
 //
 //
-//Example:
+// Example:
 //
-//Input: [2,1,5,6,2,3]
-//Output: 10
+// Input: [2,1,5,6,2,3]
+// Output: 10
+
+func largestRectangleArea(heights []int) int {
+	if len(heights) == 0 {
+		return 0
+	}
+
+	left, right := make([]int, len(heights)), make([]int, len(heights))
+	left[0] = -1
+	right[len(right)-1] = len(right)
+
+	var j int
+	for i := 1; i < len(heights); i++ {
+		for j = i - 1; j >= 0; {
+			if heights[j] <= heights[i] {
+				left[i] = j
+				break
+			}
+			j = left[j]
+		}
+
+		if j < 0 {
+			left[i] = -1
+		}
+	}
+
+	for i := len(heights) - 2; i >= 0; i-- {
+		for j = i + 1; j < len(heights); {
+			if heights[j] < heights[i] {
+				right[i] = j
+				break
+			}
+			j = right[j]
+		}
+
+		if j == len(heights) {
+			right[i] = j
+		}
+	}
+
+	var maxArea int
+
+	for i := range heights {
+		maxArea = max(maxArea, heights[i]*(right[i]-left[i]-1))
+	}
+
+	return maxArea
+}
+
+func largestRectangleArea2(heights []int) int {
+	if len(heights) == 0 {
+		return 0
+	}
+
+	// left boundary
+	stack := []int{0}
+
+	var maxArea int
+	for i := range heights {
+		maxArea = max(maxArea, heights[i])
+
+		// keep stack in height increasing order
+		for len(stack) > 1 && heights[stack[len(stack)-1]] >= heights[i] {
+			top := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			end := stack[len(stack)-1]
+			// every range is from left to right, right is the index when loop start
+			// end depends on height of left item, if left item height == current height, width should
+			// include it, otherwise, width should not include it
+			if heights[end] < heights[top] {
+				end++
+			}
+
+			maxArea = max(maxArea, heights[top]*(i-end))
+
+		}
+
+		stack = append(stack, i)
+	}
+
+	for start, i := stack[len(stack)-1], len(stack)-1; i >= 1; i-- {
+		end := stack[i-1]
+		if heights[end] < heights[stack[i]] {
+			end++
+		}
+		maxArea = max(maxArea, heights[stack[i]]*(start-end+1))
+	}
+
+	return maxArea
+}
+
+func max(i, j int) int {
+	if i >= j {
+		return i
+	}
+	return j
+}
 
 type stack struct {
 	data []int
@@ -46,7 +143,7 @@ func (s *stack) peek() int {
 	return s.data[length-1]
 }
 
-func largestRectangleArea(heights []int) int {
+func largestRectangleArea1(heights []int) int {
 	s := stack{
 		data: make([]int, 0),
 	}
@@ -131,3 +228,11 @@ func max(i, j int) int {
 //	   minimum of all range.
 //  10. zero height is the special case, it terminates area calculation
 // 	11. optimize, for the struct item, height is not necessary
+
+//	12.	use stack to keep height increasing sequence, if sequence is
+//		increasing, it's easier to calculate area
+
+//	13.	inspired form https://leetcode.com/problems/largest-rectangle-in-histogram/discuss/28902/5ms-O(n)-Java-solution-explained-(beats-96)
+
+//		author decompose problem into 2 indicators: for any height, find
+//		out its right most & left most index that's >= self
