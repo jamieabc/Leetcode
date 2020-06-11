8bc4b085196d9fa5e19beccd1d839e484ac5ddc0
Aaron Chang <xesuschang@gmail.com>
2020-06-11 12:11:04 +0800

    Squashed commit of 1007:
    commit b4c845f39fe576da73eea82266aa327fea84664b
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Thu Jun 11 12:10:48 2020 +0800
    
        add comment
    
    commit a1df1d4d7caf5251a71a002626605b58f3126c1b
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Tue Jun 9 20:54:20 2020 +0800
    
        attempt 3
    
    commit de4c64e403243cfd9c50e2036965e0d09d8488cc
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Tue Jun 9 20:45:44 2020 +0800
    
        attempt 2
    
    commit fcb70ec8dda36a8a4e983aabe06dab1381e22aea
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Tue Jun 9 20:16:50 2020 +0800
    
        attempt 1

--- 1007.go
+++ 1007.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 //In a row of dominoes, A[i] and B[i] represent the top and bottom halves of the i-th domino.  (A domino is a tile with two numbers from 1 to 6 - one on each half of the tile.)
 //
 //We may rotate the i-th domino, so that A[i] and B[i] swap values.
@@ -33,6 +35,93 @@ package main
 //    2 <= A.length == B.length <= 20000
 
 func minDominoRotations(A []int, B []int) int {
+	// if there's a solution, choose A[0] or B[0] are identical
+	a := rotate(A, B, A[0])
+	if a != -1 {
+		return a
+	}
+
+	// if only condition for B[0]
+	return rotate(A, B, B[0])
+}
+
+func rotate(A, B []int, target int) int {
+	var a, b int
+	for i := range A {
+		if A[i] != target && B[i] != target {
+			return -1
+		} else if A[i] != target && B[i] == target {
+			a++
+		} else if B[i] != target && A[i] == target {
+			b++
+		}
+	}
+
+	return min(a, b)
+}
+
+func minDominoRotations2(A []int, B []int) int {
+	a, b := A[0], B[0]
+	dp := make([]int, 4) // a at A, a at B, b at A, b at B
+
+	for i := range A {
+		if A[i] != a && B[i] != a {
+			dp[0] = -1
+			dp[1] = -1
+		} else if A[i] != a {
+			if B[i] == a && dp[0] >= 0 {
+				dp[0]++
+			} else {
+				dp[0] = -1
+			}
+		} else if B[i] != a {
+			if A[i] == a && dp[1] >= 0 {
+				dp[1]++
+			} else {
+				dp[1] = -1
+			}
+		}
+
+		if A[i] != b && B[i] != b {
+			dp[2] = -1
+			dp[3] = -1
+		} else if A[i] != b {
+			if B[i] == b && dp[2] >= 0 {
+				dp[2]++
+			} else {
+				dp[2] = -1
+			}
+		} else if B[i] != b {
+			if A[i] == b && dp[3] >= 0 {
+				dp[3]++
+			} else {
+				dp[3] = -1
+			}
+		}
+	}
+
+	m := math.MaxInt32
+	for i := range dp {
+		if dp[i] >= 0 {
+			m = min(m, dp[i])
+		}
+	}
+
+	if m == math.MaxInt32 {
+		return -1
+	}
+
+	return m
+}
+
+func min(i, j int) int {
+	if i <= j {
+		return i
+	}
+	return j
+}
+
+func minDominoRotations1(A []int, B []int) int {
 	lengthA := len(A)
 
 	if lengthA <= 1 {
@@ -107,3 +196,21 @@ func findRotation(target, backup []int, num int) int {
 // 2. when length is 1, no need to compare
 // 3. optimization, since it's either all A or B are same, possible number is either A[0] or B[0], this can reduce complexity
 // 4. length of A & B are same, because each means half side value of dominos
+
+//	5.	if every number are same, no need to to anything
+
+//	6.	inspired from solution, if 2 rows can both fits condition, then
+//		flipping numbers for 2 conditions (A[0] or B[0]) are same inverted.
+
+//		e.g. A: 2, 5, 5, 5, 2  2: 3, 5: 2
+//			 B: 5, 2, 2, 2, 5  5: 2, 2: 3
+
+//		however, there still exists condition that A cannot be same with
+//		A[0], e.g.
+
+//		A: 2, 5, 5, 2, 2
+//		B: 5, 1, 2, 5, 5
+
+//	7.	inspired from https://leetcode.com/problems/minimum-domino-rotations-for-equal-row/discuss/252242/JavaC%2B%2BPython-Different-Ideas
+
+//		lee with 3 solutions....how come so many!
