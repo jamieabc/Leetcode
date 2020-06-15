cc50f3d2abab1fd108c7bebb50e67a0320705674
Aaron Chang <xesuschang@gmail.com>
2020-06-15 10:21:27 +0800

    Squashed commit of 843:
    commit 202c58db74407736d77cd86ba8c2fd04b7dbc3cd
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon Jun 15 10:21:06 2020 +0800
    
        add reference
    
    commit 1e24f8e63a87ec931b4292adaebb6ed417b16470
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon Jun 15 09:52:08 2020 +0800
    
        attempt 9
    
    commit ed83ffc0875f11784c084aad437487598cf2eb88
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon Jun 15 09:31:36 2020 +0800
    
        attempt 8
    
    commit 9eb07046fc612aa2fa047848cc020f53f1f9f7ce
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon Jun 15 09:30:43 2020 +0800
    
        attempt 7
    
    commit a77621a1ec85e3a267b2bdf1db198671e363c02f
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon Jun 15 08:57:45 2020 +0800
    
        attempt65
    
    commit 8961b89c8f28a16438ff9433a4a1c2a8cf9a83ec
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sun Jun 14 18:33:25 2020 +0800
    
        attempt 5
    
    commit 8e0ef3414ddcd36e52a2b1210611aa438d292b4a
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sun Jun 14 18:00:07 2020 +0800
    
        attempt 4
    
    commit 9b489d632149ef51df5259e8a46dc1622bfa5126
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sun Jun 14 17:59:09 2020 +0800
    
        attempt 3
    
    commit e1043e61e5225e555160c5651be667d635c76504
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sun Jun 14 15:13:36 2020 +0800
    
        attempt 2
    
    commit fb34fdb625c3fbf65da698a4bdedbdd446e2253c
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sun Jun 14 15:03:56 2020 +0800
    
        attempt 1

---
+++ 843.go
@@ -0,0 +1,250 @@
+package main
+
+import (
+	"math"
+	"math/rand"
+)
+
+// This problem is an interactive problem new to the LeetCode platform.
+//
+// We are given a word list of unique words, each word is 6 letters long, and one word in this list is chosen as secret.
+//
+// You may call master.guess(word) to guess a word.  The guessed word should have type string and must be from the original list with 6 lowercase letters.
+//
+// This function returns an integer type, representing the number of exact matches (value and position) of your guess to the secret word.  Also, if your guess is not in the given wordlist, it will return -1 instead.
+//
+// For each test case, you have 10 guesses to guess the word. At the end of any number of calls, if you have made 10 or less calls to master.guess and at least one of these guesses was the secret, you pass the testcase.
+//
+// Besides the example test case below, there will be 5 additional test cases, each with 100 words in the word list.  The letters of each word in those testcases were chosen independently at random from 'a' to 'z', such that every word in the given word lists is unique.
+//
+// Example 1:
+// Input: secret = "acckzz", wordlist = ["acckzz","ccbazz","eiowzz","abcczz"]
+//
+// Explanation:
+//
+// master.guess("aaaaaa") returns -1, because "aaaaaa" is not in wordlist.
+// master.guess("acckzz") returns 6, because "acckzz" is secret and has all 6 matches.
+// master.guess("ccbazz") returns 3, because "ccbazz" has 3 matches.
+// master.guess("eiowzz") returns 2, because "eiowzz" has 2 matches.
+// master.guess("abcczz") returns 4, because "abcczz" has 4 matches.
+//
+// We made 5 calls to master.guess and one of them was the secret, so we pass the test case.
+//
+// Note:  Any solutions that attempt to circumvent the judge will result in disqualification.
+
+/**
+ * // This is the Master's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * type Master struct {
+ * }
+ *
+ * func (this *Master) Guess(word string) int {}
+ */
+/**
+ * // This is the Master's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * type Master struct {
+ * }
+ *
+ * func (this *Master) Guess(word string) int {}
+ */
+
+func findSecretWord(wordlist []string, master *Master) {
+	counts := make([][]int, 6)
+	for i := range counts {
+		counts[i] = make([]int, 26)
+	}
+
+	for i := range wordlist {
+		for j, c := range wordlist[i] {
+			counts[j][int(c-'a')]++
+		}
+	}
+
+	size := len(wordlist)
+	score := make([]int, size)
+	for i, w := range wordlist {
+		for j := range w {
+			score[i] += counts[j][int(w[j]-'a')]
+		}
+	}
+
+	possible := make([]int, size)
+	for i := range possible {
+		possible[i] = i
+	}
+
+	for count := 0; count < 10; count++ {
+		target := freqChoose(score, possible)
+		result := master.Guess(wordlist[possible[target]])
+
+		remain := make([]int, 0)
+		for i := range possible {
+			if i != target && matches(wordlist[possible[i]], wordlist[possible[target]]) == result {
+				remain = append(remain, possible[i])
+			}
+		}
+
+		if len(remain) == 0 {
+			return
+		}
+
+		possible = remain
+	}
+}
+
+func freqChoose(score, possible []int) int {
+	highest := math.MinInt32
+	var idx int
+
+	for i := range possible {
+		if score[possible[i]] > highest {
+			highest = score[possible[i]]
+			idx = i
+		}
+	}
+
+	return idx
+}
+
+func findSecretWord2(wordlist []string, master *Master) {
+	size := len(wordlist)
+	zMatch := make([][]int, size)
+	for i := range zMatch {
+		zMatch[i] = make([]int, size)
+	}
+
+	// tc: O(n^2)
+	for i := range wordlist {
+		for j := i + 1; j < size; j++ {
+			zMatch[i][j] = matches(wordlist[i], wordlist[j])
+			zMatch[j][i] = zMatch[i][j]
+		}
+	}
+
+	possible := make([]int, size)
+	for i := range possible {
+		possible[i] = i
+	}
+
+	for count := 10; count > 0; count-- {
+		target := minMaxChoose(zMatch, possible)
+		result := master.Guess(wordlist[possible[target]])
+
+		remain := make([]int, 0)
+		for j := range possible {
+			if j != target && zMatch[possible[target]][possible[j]] == result {
+				remain = append(remain, possible[j])
+			}
+		}
+
+		if len(remain) == 0 {
+			return
+		}
+		possible = remain
+	}
+}
+
+func minMaxChoose(zMatch [][]int, possible []int) int {
+	minMax := math.MaxInt32
+	var idx int
+
+	for i := range possible {
+		var tmp int
+		for j := 0; j < len(possible); j++ {
+			if i != j && zMatch[possible[i]][possible[j]] == 0 {
+				tmp++
+			}
+		}
+		if minMax > tmp {
+			minMax = tmp
+			idx = i
+		}
+	}
+	return idx
+}
+
+func findSecretWord1(wordlist []string, master *Master) {
+	possible := make([]int, len(wordlist))
+	for i := range possible {
+		possible[i] = i
+	}
+
+	for count := 10; count > 0; count-- {
+		target := choose(possible)
+		result := master.Guess(wordlist[possible[target]])
+		if result == 6 {
+			return
+		}
+
+		tmp := make([]int, 0)
+
+		for i := 0; i < len(possible); i++ {
+			if i != target && matches(wordlist[possible[i]], wordlist[possible[target]]) == result {
+				tmp = append(tmp, possible[i])
+			}
+		}
+
+		possible = tmp
+		if len(possible) == 0 {
+			break
+		}
+	}
+}
+
+func choose(candidates []int) int {
+	return rand.Intn(len(candidates))
+}
+
+func matches(src, dst string) int {
+	var match int
+	for i := range src {
+		if src[i] == dst[i] {
+			match++
+		}
+	}
+
+	return match
+}
+
+//	problems
+//	1.	when guessing words, use src as base, count the difference
+
+//	2.	matches is considered on ith char
+
+//	3.	inspired by https://leetcode.com/problems/guess-the-word/discuss/133862/Random-Guess-and-Minimax-Guess-with-Comparison
+
+//		lee oberves the problem and mentions a good point, if a word list
+//		is made up of aaaa, bbbb, cccc, etc., then at least 26 tries to
+//		get correct answer. 10 tries is to check for reasonable solution
+
+//		most important observation comes from the conclusion that most
+//		guesses will end-up to 0, which is crucial to whole discussion.
+
+//		minimax strategy, reduces as many words as possible. the possibility
+//		to get 0 match is (25/26)^6 ~ 80%, which means mostly guess will get
+//		0 matches. the problem becomes: how to remove most words if there's
+//		0 matches. since any words with 0 matches to guess word will be
+//		retained, the target becomes: choose a word that has minimum
+//		0 matches to all other words.
+
+//		further improvement on O(n^2) is to calculate char occurrence, to
+//		eliminates more words, choose the word with more popular chars on
+//		each position
+
+//	4.	inspired by https://leetcode.com/problems/guess-the-word/discuss/134251/Optimal-MinMax-Solution-(%2B-extra-challenging-test-cases)
+
+//		author describe guessing word as the distance to secret
+//		word. to choose better words, each iteration should re-calculate
+//		word score. basic idea is to choose a word among candidates that
+//		partitions equally in all distances (0 ~ 6)
+
+//		the idea different from lee is that global optimum is not locally
+//		optimum. e.g. a word is good to partition at global could be worst
+//		when candidates shrink.
+
+//		it's a really good insight not also describing guess as distance,
+//		but also points out globally optimum doesn't guarantee locally
+//		optimum
+
+//		due to time limit, I didn't implement this solution
