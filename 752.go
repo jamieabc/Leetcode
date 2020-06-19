87fdcac980f1c22b382b730f94b4ef528f8407f4
Aaron Chang <xesuschang@gmail.com>
2020-06-19 20:28:22 +0800

    Squashed commit of 752:
    commit 978e8c327c7ff4f0892f3322cb7a26fb392f22a1
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Jun 19 20:27:24 2020 +0800
    
        attempt 3
    
    commit 0331e16e083e6caa26a5290c57d131c472a59ddb
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Jun 19 20:25:30 2020 +0800
    
        add reference
    
    commit 5f8e896909c389bff486f08d348bed8db1c64dcf
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Jun 19 19:46:18 2020 +0800
    
        attempt 2
    
    commit 510771c6b776784561a951d44e61dab0dd1df485
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Jun 19 19:17:11 2020 +0800
    
        attempt 1

---
+++ 752.go
@@ -0,0 +1,220 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+//  You have a lock in front of you with 4 circular wheels. Each wheel has 10 slots: '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'. The wheels can rotate freely and wrap around: for example we can turn '9' to be '0', or '0' to be '9'. Each move consists of turning one wheel one slot.
+//
+// The lock initially starts at '0000', a string representing the state of the 4 wheels.
+//
+// You are given a list of deadends dead ends, meaning if the lock displays any of these codes, the wheels of the lock will stop turning and you will be unable to open it.
+//
+// Given a target representing the value of the wheels that will unlock the lock, return the minimum total number of turns required to open the lock, or -1 if it is impossible.
+//
+// Example 1:
+//
+// Input: deadends = ["0201","0101","0102","1212","2002"], target = "0202"
+// Output: 6
+// Explanation:
+// A sequence of valid moves would be "0000" -> "1000" -> "1100" -> "1200" -> "1201" -> "1202" -> "0202".
+// Note that a sequence like "0000" -> "0001" -> "0002" -> "0102" -> "0202" would be invalid,
+// because the wheels of the lock become stuck after the display becomes the dead end "0102".
+//
+// Example 2:
+//
+// Input: deadends = ["8888"], target = "0009"
+// Output: 1
+// Explanation:
+// We can turn the last wheel in reverse to move from "0000" -> "0009".
+//
+// Example 3:
+//
+// Input: deadends = ["8887","8889","8878","8898","8788","8988","7888","9888"], target = "8888"
+// Output: -1
+// Explanation:
+// We can't reach the target without getting stuck.
+//
+// Example 4:
+//
+// Input: deadends = ["0000"], target = "8888"
+// Output: -1
+//
+// Note:
+//
+//     The length of deadends will be in the range [1, 500].
+//     target will not be in the list deadends.
+//     Every string in deadends and the string target will be a string of 4 digits from the 10,000 possibilities '0000' to '9999'.
+
+func openLock(deadends []string, target string) int {
+	invalid := make(map[string]bool)
+	for _, deadend := range deadends {
+		invalid[deadend] = true
+	}
+
+	if target == "0000" {
+		return 0
+	}
+
+	if invalid["0000"] {
+		return -1
+	}
+
+	visited := make(map[string]int)
+	visited["0000"] = 0
+	minCost := math.MaxInt32
+
+	dfs([]byte{'0', '0', '0', '0'}, target, 0, &minCost, invalid, visited)
+
+	if minCost == math.MaxInt32 {
+		return -1
+	}
+
+	return minCost
+}
+
+func dfs(current []byte, target string, cost int, minCost *int, invalid map[string]bool, visited map[string]int) {
+	for i := range current {
+		possibilities := [][]byte{nextNum(current, i), prevNum(current, i)}
+
+		for _, bytes := range possibilities {
+			str := string(bytes)
+
+			if str == target {
+				*minCost = min(*minCost, cost+1)
+				return
+			}
+
+			if !invalid[str] {
+				if steps, ok := visited[str]; !ok {
+					visited[str] = cost + 1
+					dfs(bytes, target, cost+1, minCost, invalid, visited)
+				} else {
+					if steps > cost+1 {
+						visited[str] = cost + 1
+						dfs(bytes, target, cost+1, minCost, invalid, visited)
+					}
+				}
+			}
+		}
+	}
+}
+
+func openLock1(deadends []string, target string) int {
+	invalid := make(map[string]bool)
+	for _, d := range deadends {
+		invalid[d] = true
+	}
+
+	if target == "0000" {
+		return 0
+	}
+
+	// initial state in dead end
+	if invalid["0000"] {
+		return -1
+	}
+
+	queue := [][]byte{
+		{'0', '0', '0', '0'},
+	}
+
+	visited := make(map[string]bool)
+	var str string
+	steps := 1
+
+	for len(queue) > 0 {
+		end := len(queue)
+
+		for i := 0; i < end; i++ {
+			popped := queue[i]
+
+			for i := 0; i < len(target); i++ {
+				possibilities := [][]byte{nextNum(popped, i), prevNum(popped, i)}
+
+				for _, bytes := range possibilities {
+					str = string(bytes)
+
+					if str == target {
+						return steps
+					}
+
+					if !invalid[str] && !visited[str] {
+						visited[str] = true
+						queue = append(queue, bytes)
+					}
+
+				}
+			}
+		}
+		queue = queue[end:]
+		steps++
+	}
+
+	return -1
+}
+
+func nextNum(current []byte, idx int) []byte {
+	next := make([]byte, len(current))
+	copy(next, current)
+
+	if next[idx] == '9' {
+		next[idx] = '0'
+	} else {
+		next[idx]++
+	}
+
+	return next
+}
+
+func prevNum(current []byte, idx int) []byte {
+	next := make([]byte, len(current))
+	copy(next, current)
+
+	if next[idx] == '0' {
+		next[idx] = '9'
+	} else {
+		next[idx]--
+	}
+
+	return next
+}
+
+func min(i, j int) int {
+	if i <= j {
+		return i
+	}
+	return j
+}
+
+// 	problems
+//	1.	why can't I write DFS solution? I don't know how to stop recursion.
+//		because there are 2 ways of iterating: increase number or decrease
+//		number.
+
+//		I don't know how to write a program that can detect increment,
+//		and when encounter any deadend, stop it.
+
+//	2.	no need to detect, just iterate through all possibilities
+
+//	3.	when implementing dfs, I encounter a problem: dfs needs to store
+//		map[string]int, due to nature of dfs, sequence will like
+//		0000 -> 0001 -> 0002 -> 0003
+
+//		so even if a number is visited, there's still possibility other
+//		path exists smaller outcome
+
+//	4.	inspired from https://leetcode.com/problems/open-the-lock/discuss/110230/BFS-solution-C%2B%2B
+
+//		bi-directional search starts from 0000 and target, and each time
+//		choose sets with smaller size
+
+//	5.	inspired from solution, for every number, there are 2 directions(
+//		increment & decrement), there arn n digits, and when generating
+//		new number copy take O(2n^2)
+
+//		there are total 10^4 possible combinations, and first need iterate
+//		invalid words
+
+//		tc: O(n^2 * a^n + d), n: 4 digits, a: 10 digits, d: size of deadends
