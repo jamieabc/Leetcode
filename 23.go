2e2a007a3f02ba611f33f10bdf17fcd97fb24199
Aaron Chang <xesuschang@gmail.com>
2020-07-23 10:23:15 +0800

    Squashed commit of 23:
    commit efe717038b0adfd21ccf601824959b0d2c302353
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Thu Jul 23 10:21:56 2020 +0800
    
        attempt 3
    
    commit 9feb7952d4f902816312c4abb138de16ae6cd6f4
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Thu Jul 23 10:17:26 2020 +0800
    
        attempt 2
    
    commit 3106acbf9b8aaaae3f7b596363da48df4ccbb32d
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Thu Jul 23 10:16:41 2020 +0800
    
        attempt 1

---
+++ 23.go
@@ -0,0 +1,78 @@
+package main
+
+import "container/heap"
+
+// Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+//
+// Example:
+//
+// Input:
+// [
+//   1->4->5,
+//   1->3->4,
+//   2->6
+// ]
+// Output: 1->1->2->3->4->4->5->6
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type MinHeap []*ListNode
+
+func (h MinHeap) Len() int           { return len(h) }
+func (h MinHeap) Less(i, j int) bool { return h[i].Val < h[j].Val }
+func (h MinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *MinHeap) Push(x interface{}) {
+	*h = append(*h, x.(*ListNode))
+}
+
+func (h *MinHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[:n-1]
+	return x
+}
+
+func mergeKLists(lists []*ListNode) *ListNode {
+	h := &MinHeap{}
+	heap.Init(h)
+
+	for i := range lists {
+		if lists[i] != nil {
+			heap.Push(h, lists[i])
+		}
+	}
+
+	var head *ListNode
+	ptr := head
+
+	for h.Len() > 0 {
+		popped := heap.Pop(h).(*ListNode)
+
+		if ptr == nil {
+			head = popped
+			ptr = head
+		} else {
+			ptr.Next = popped
+			ptr = ptr.Next
+		}
+
+		if popped.Next != nil {
+			heap.Push(h, popped.Next)
+		}
+	}
+
+	return head
+}
+
+//	problems
+//	1.	pointer might be nil
+
+//	2.	use original linked list
