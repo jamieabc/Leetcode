4285a7897afa3aa838f98b13a16848a5e9a7e60a
Aaron Chang <xesuschang@gmail.com>
2020-07-27 12:50:36 +0800

    Squashed commit of 444:
    commit 705c9139a3c118e7f935fb5d2210d8190580a2ce
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon Jul 27 12:50:23 2020 +0800
    
        add reference
    
    commit a8edb6087f4f467f460d9b16090fd046f4d20ec7
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon Jul 27 12:12:12 2020 +0800
    
        attempt 8
    
    commit 24b9f93590ba71d549ddb8624988337b13a9aaca
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon Jul 27 12:09:01 2020 +0800
    
        attempt 7
    
    commit 4d9b51c88075200298575d71168a92e3bb8658fe
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon Jul 27 12:05:58 2020 +0800
    
        attempt 6
    
    commit b64d061599469128d9cd9c3a5a8de989ba786447
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon Jul 27 12:01:54 2020 +0800
    
        attempt 5
    
    commit 4d00c45eb72de7f1598764f2d48fbd2fffbe75cd
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon Jul 27 11:54:42 2020 +0800
    
        attempt 4
    
    commit b0385fa2db35b9e76e6c1738371e55efe50d7f6c
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon Jul 27 11:52:32 2020 +0800
    
        attempt 3
    
    commit f6c5214274700f111cfccc90bcccb32d266c1e78
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon Jul 27 11:51:12 2020 +0800
    
        attempt 2
    
    commit 22d728fb69b725e6d5eeec1da2daff4fcbf67dff
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon Jul 27 11:49:11 2020 +0800
    
        attempt 1

--- 444.go
+++ 444.go
@@ -1,42 +1,66 @@
 package main
 
-import "fmt"
+import "math"
 
+// Check whether the original sequence org can be uniquely reconstructed from the sequences in seqs. The org sequence is a permutation of the integers from 1 to n, with 1 ≤ n ≤ 104. Reconstruction means building a shortest common supersequence of the sequences in seqs (i.e., a shortest sequence so that all sequences in seqs are subsequences of it). Determine whether there is only one sequence that can be reconstructed from seqs and it is the org sequence.
+//
+//
+//
+// Example 1:
+//
+// Input: org = [1,2,3], seqs = [[1,2],[1,3]]
+// Output: false
+// Explanation: [1,2,3] is not the only one sequence that can be reconstructed, because [1,3,2] is also a valid sequence that can be reconstructed.
+//
+// Example 2:
+//
+// Input: org = [1,2,3], seqs = [[1,2]]
+// Output: false
+// Explanation: The reconstructed sequence can only be [1,2].
+//
+// Example 3:
+//
+// Input: org = [1,2,3], seqs = [[1,2],[1,3],[2,3]]
+// Output: true
+// Explanation: The sequences [1,2], [1,3], and [2,3] can uniquely reconstruct the original sequence [1,2,3].
+//
+// Example 4:
+//
+// Input: org = [4,1,5,2,6,3], seqs = [[5,2,6,3],[4,1,5,2]]
+// Output: true
+//
+//
+//
+// Constraints:
+//
+//     1 <= n <= 10^4
+//     org is a permutation of {1,2,...,n}.
+//     1 <= segs[i].length <= 10^5
+//     seqs[i][j] fits in a 32-bit signed integer.
+//
+//
+//
+// UPDATE (2017/1/8):
+// The seqs parameter had been changed to a list of list of strings (instead of a 2d array of strings). Please reload the code definition to get the latest changes.
+
+// time limit exceed
 func sequenceReconstruction(org []int, seqs [][]int) bool {
-	indexes := make([]int, len(org))
-	for i := range org {
-		indexes[org[i]-1] = i
-	}
-	orders := make(map[string]bool)
-
-	for _, seq := range seqs {
-		for i := range seq {
-			if seq[i] > len(org) {
-				return false
-			}
-
-			if len(seq) == 1 {
-				orders[fmt.Sprintf("%d", seq[0])] = true
-				continue
-			}
-
-			if i < len(seq)-1 && (seq[i+1] > len(org) || indexes[seq[i]-1] >= indexes[seq[i+1]-1]) {
-				return false
-			}
-
-			if i < len(seq)-1 {
-				orders[keyrize(seq[i], seq[i+1])] = true
-			}
-		}
-	}
-
-	if len(orders) == 0 || len(orders) < len(org)-1 {
+	// no edge, no order, no unique sequence
+	if len(seqs) == 0 && len(org) > 0 {
 		return false
 	}
 
-	// make sure consecutive relationships are constructed
-	for i := 0; i < len(org)-1; i++ {
-		if !orders[keyrize(org[i], org[i+1])] {
+	graph, inDegree := buildGraph(seqs, len(org))
+
+	sorted := topologicalSort(graph, inDegree)
+
+	if len(sorted) != len(org) {
+		return false
+	}
+
+	// compare each element
+	for i := range sorted {
+		if org[i] != sorted[i] {
 			return false
 		}
 	}
@@ -44,8 +68,69 @@ func sequenceReconstruction(org []int, seqs [][]int) bool {
 	return true
 }
 
-func keyrize(i, j int) string {
-	return fmt.Sprintf("%d-%d", i, j)
+func topologicalSort(graph map[int][]int, inDegree []int) []int {
+	todo := make([]int, 0)
+	for i := range inDegree {
+		if inDegree[i] == 0 {
+			todo = append(todo, i)
+		}
+	}
+
+	// should have only 1 start point
+	if len(todo) == 0 || len(todo) > 1 {
+		return []int{}
+	}
+
+	result := make([]int, 0)
+
+	for len(todo) > 0 {
+		end := len(todo)
+		var count int
+
+		for i := 0; i < end; i++ {
+			next := todo[0]
+			todo = todo[1:]
+			result = append(result, next+1)
+
+			// update children in-degree
+			for _, child := range graph[next] {
+				inDegree[child]--
+
+				if inDegree[child] == 0 {
+					count++
+					if count > 1 {
+						return []int{}
+					}
+
+					todo = append(todo, child)
+				}
+			}
+		}
+	}
+
+	return result
+}
+
+func buildGraph(seqs [][]int, size int) (map[int][]int, []int) {
+	graph := make(map[int][]int)
+	inDegree := make([]int, size)
+
+	for _, seq := range seqs {
+		// single char can only be the org
+		if len(seq) == 1 && seq[0] > size {
+			inDegree[0] = math.MaxInt32
+			continue
+		}
+
+		for i := range seq {
+			for j := i + 1; j < len(seq); j++ {
+				graph[seq[i]-1] = append(graph[seq[i]-1], seq[j]-1)
+				inDegree[seq[j]-1]++
+			}
+		}
+	}
+
+	return graph, inDegree
 }
 
 //	problems
@@ -76,3 +161,9 @@ func keyrize(i, j int) string {
 //	6.	so many corner cases, finally TLE, not keep doing it
 
 //	7. 	didn't write pass solution
+
+//	8. 	need to check if sequence is valid by order
+
+//	9.	inspired from https://leetcode.com/problems/sequence-reconstruction/discuss/92572/Simple-Solution-%3A-one-pass-using-only-array-(C%2B%2B-92ms-Java-16ms)
+
+//		not implement it
