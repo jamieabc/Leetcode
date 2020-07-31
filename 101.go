5ec87985e671de7ee5e97c380353f605f870a729
Aaron Chang <xesuschang@gmail.com>
2020-07-31 09:38:47 +0800

    Squashed commit of 101:
    commit cdaf88a27e56be0ba29ab4ae6e667a5e5bf00c38
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Jul 31 09:36:44 2020 +0800
    
        attempt 2
    
    commit f72d710c4362be3f746b511592b74b8d007d9cd3
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Jul 31 09:32:06 2020 +0800
    
        attempt 1

--- 101.go
+++ 101.go
@@ -1,71 +1,72 @@
 package main
 
-//Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+// Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
 //
-//For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+// For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
 //
-//    1
-//   / \
-//  2   2
-// / \ / \
-//3  4 4  3
-//
-//
-//
-//But the following [1,2,2,null,3,null,3] is not:
-//
-//    1
-//   / \
-//  2   2
-//   \   \
-//   3    3
+//     1
+//    / \
+//   2   2
+//  / \ / \
+// 3  4 4  3
 //
 //
 //
-//Note:
-//Bonus points if you could solve it both recursively and iteratively.
+// But the following [1,2,2,null,3,null,3] is not:
+//
+//     1
+//    / \
+//   2   2
+//    \   \
+//    3    3
 
-/**
- * Definition for a binary tree node.
- * type TreeNode struct {
- *     Val int
- *     Left *TreeNode
- *     Right *TreeNode
- * }
- */
 func isSymmetric(root *TreeNode) bool {
-	// empty
 	if root == nil {
 		return true
 	}
 
-	// single root
-	if root.Left == nil && root.Right == nil {
+	stack1, stack2 := []*TreeNode{root.Left}, []*TreeNode{root.Right}
+
+	for len(stack1) > 0 && len(stack2) > 0 {
+		n1, n2 := stack1[len(stack1)-1], stack2[len(stack2)-1]
+		stack1 = stack1[:len(stack1)-1]
+		stack2 = stack2[:len(stack2)-1]
+
+		if n1 == nil && n2 == nil {
+			continue
+		}
+
+		if (n1 == nil && n2 != nil) || (n1 != nil && n2 == nil) || n1.Val != n2.Val {
+			return false
+		}
+
+		stack1 = append(stack1, n1.Left, n1.Right)
+		stack2 = append(stack2, n2.Right, n2.Left)
+	}
+
+	return len(stack1) == 0 && len(stack2) == 0
+}
+
+func isSymmetric2(root *TreeNode) bool {
+	if root == nil {
 		return true
 	}
 
-	return traverse(root.Left, root.Right)
+	return recursive(root.Left, root.Right)
 }
 
-func traverse(left, right *TreeNode) bool {
-	if left == nil && right == nil {
+func recursive(n1, n2 *TreeNode) bool {
+	if n1 == nil && n2 == nil {
 		return true
 	}
 
-	// one side is empty
-	if left == nil || right == nil {
+	if (n1 == nil && n2 != nil) || (n1 != nil && n2 == nil) {
 		return false
 	}
 
-	// value different
-	if left.Val != right.Val {
+	if n1.Val != n2.Val {
 		return false
 	}
 
-	// symmetric
-	return traverse(left.Left, right.Right) && traverse(left.Right, right.Left)
+	return recursive(n1.Left, n2.Right) && recursive(n1.Right, n2.Left)
 }
-
-// problems
-// 1. symmetric, means value of left children = value of right children
-// 2. optimization, when only one node is nil, only need to check any nil, because case of both nil is eliminated
