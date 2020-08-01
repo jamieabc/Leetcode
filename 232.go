99a0385f9dad35c99c1a42c8770f10f6dd64edd2
Aaron Chang <xesuschang@gmail.com>
2020-08-01 14:50:10 +0800

    Squashed commit of 232:
    commit 8ca1fc36a8cbf0a43f324b154d38e0bf7fb9c107
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sat Aug 1 14:47:31 2020 +0800
    
        attempt 1

--- 232.go
+++ 232.go
@@ -23,87 +23,49 @@ package main
 //    Depending on your language, stack may not be supported natively. You may simulate a stack by using a list or deque (double-ended queue), as long as you use only standard operations of a stack.
 //    You may assume that all operations are valid (for example, no pop or peek operations will be called on an empty queue).
 
-type MyStack struct {
-	data []int
-}
-
-func (s *MyStack) Push(x int) {
-	s.data = append(s.data, x)
-}
-
-func (s *MyStack) Pop() int {
-	length := len(s.data)
-	popped := s.data[length-1]
-	s.data = s.data[:length-1]
-	return popped
-}
-
-func (s *MyStack) Peek() int {
-	return s.data[len(s.data)-1]
-}
-
-func (s *MyStack) Empty() bool {
-	return len(s.data) == 0
-}
-
+// lazy operation
 type MyQueue struct {
-	data     *MyStack
-	reversed *MyStack
+	data, reversed []int
 }
 
 /** Initialize your data structure here. */
 func Constructor() MyQueue {
 	return MyQueue{
-		data: &MyStack{
-			data: make([]int, 0),
-		},
-		reversed: &MyStack{
-			data: make([]int, 0),
-		},
+		data:     make([]int, 0),
+		reversed: make([]int, 0),
 	}
 }
 
 /** Push element x to the back of queue. */
 func (this *MyQueue) Push(x int) {
-	this.data.Push(x)
+	this.data = append(this.data, x)
 }
 
-/** Removes the element from in front of queue and returns that element. */
 func (this *MyQueue) Pop() int {
-	var popped int
-	for {
-		popped = this.data.Pop()
-		if this.data.Empty() {
-			break
+	this.reverse()
+	popped := this.reversed[len(this.reversed)-1]
+	this.reversed = this.reversed[:len(this.reversed)-1]
+
+	return popped
+}
+
+func (this *MyQueue) reverse() {
+	if len(this.reversed) == 0 {
+		// change stack order (data) to queue order (reversed)
+		for len(this.data) > 0 {
+			this.reversed = append(this.reversed, this.data[len(this.data)-1])
+			this.data = this.data[:len(this.data)-1]
 		}
-		this.reversed.Push(popped)
 	}
-
-	for !this.reversed.Empty() {
-		this.data.Push(this.reversed.Pop())
-	}
-
-	return popped
 }
 
-/** Get the front element. */
 func (this *MyQueue) Peek() int {
-	var popped int
-	for !this.data.Empty() {
-		popped = this.data.Pop()
-		this.reversed.Push(popped)
-	}
-
-	for !this.reversed.Empty() {
-		this.data.Push(this.reversed.Pop())
-	}
-
-	return popped
+	this.reverse()
+	return this.reversed[len(this.reversed)-1]
 }
 
-/** Returns whether the queue is empty. */
 func (this *MyQueue) Empty() bool {
-	return this.data.Empty()
+	return len(this.data)+len(this.reversed) == 0
 }
 
 /**
