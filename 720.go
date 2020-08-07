c4b016dfad9014c2944e61415cca4e0c46d5df06
Aaron Chang <xesuschang@gmail.com>
2020-08-07 15:50:20 +0800

    Squashed commit of 720:
    commit 4d20d820e356d77e5d8deedb1942c66a4e8e7177
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Aug 7 15:49:36 2020 +0800
    
        attempt 7
    
    commit edc040e92175ae87d908369711336f90712c195c
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Aug 7 15:48:53 2020 +0800
    
        attempt 6
    
    commit 6c23e30a9a34b93bb08c40129ffd53ed14303258
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Aug 7 15:44:09 2020 +0800
    
        attempt 5
    
    commit f849f7db51a5b4a6336e831eedb9d2a15b0e29d7
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Aug 7 15:35:48 2020 +0800
    
        attempt 4
    
    commit d26db93fb2cf1e6474ff89f70cbf1668f738e669
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Aug 7 14:21:34 2020 +0800
    
        attempt 3
    
    commit c2c8f918636c462bc8e63adc045ef59e497d1d27
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Aug 7 13:54:19 2020 +0800
    
        attempt 2
    
    commit be9f5b3f031874c9991d0cadc92aeb7a89d3d6b5
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Aug 7 13:42:10 2020 +0800
    
        attempt 1

---
+++ 720.go
@@ -0,0 +1,173 @@
+package main
+
+import "sort"
+
+// Given a list of strings words representing an English Dictionary, find the longest word in words that can be built one character at a time by other words in words. If there is more than one possible answer, return the longest word with the smallest lexicographical order.
+//
+// If there is no answer, return the empty string.
+// Example 1:
+//
+// Input:
+// words = ["w","wo","wor","worl", "world"]
+// Output: "world"
+// Explanation:
+// The word "world" can be built one character at a time by "w", "wo", "wor", and "worl".
+// Example 2:
+//
+// Input:
+// words = ["a", "banana", "app", "appl", "ap", "apply", "apple"]
+// Output: "apple"
+// Explanation:
+// Both "apply" and "apple" can be built from other words in the dictionary. However, "apple" is lexicographically smaller than "apply".
+// Note:
+//
+// All the strings in the input will only contain lowercase letters.
+// The length of words will be in the range [1, 1000].
+// The length of words[i] will be in the range [1, 30].
+
+// tc: O(n log n)
+func longestWord(words []string) string {
+	sort.Strings(words)
+
+	table := make(map[string]bool)
+	table[""] = true
+	maxSize, result := 0, ""
+
+	for _, word := range words {
+		if _, ok := table[word[:len(word)-1]]; ok {
+			table[word] = true
+
+			if len(word) > maxSize || (len(word) == maxSize && word < result) {
+				result = word
+				maxSize = len(word)
+			}
+		}
+	}
+
+	return result
+}
+
+type Trie struct {
+	Val      byte
+	Children map[byte]*Trie
+	IsWord   bool
+}
+
+func (t *Trie) Insert(str string) {
+	node := t
+	for i := range str {
+		if _, ok := node.Children[str[i]]; !ok {
+			node.Children[str[i]] = &Trie{
+				Val:      str[i],
+				Children: make(map[byte]*Trie),
+			}
+		}
+		node = node.Children[str[i]]
+	}
+	node.IsWord = true
+}
+
+func (t *Trie) Dfs(prev string) string {
+	var current string
+	if t.Val != 0 {
+		current = prev + string(t.Val)
+	}
+
+	str := current
+
+	for _, n := range t.Children {
+		if !n.IsWord {
+			continue
+		}
+
+		tmp := n.Dfs(current)
+		if len(tmp) > len(str) || (len(tmp) == len(str) && tmp < str) {
+			str = tmp
+		}
+	}
+
+	return str
+}
+
+func longestWord2(words []string) string {
+	t := &Trie{
+		Children: make(map[byte]*Trie),
+	}
+
+	// create trie
+	for _, word := range words {
+		t.Insert(word)
+	}
+
+	// find longest
+	return t.Dfs("")
+}
+
+func longestWord1(words []string) string {
+	var maxSize int
+	counter := make(map[int]map[string]bool)
+
+	for _, word := range words {
+		if _, ok := counter[len(word)]; !ok {
+			counter[len(word)] = make(map[string]bool)
+		}
+		counter[len(word)][word] = true
+
+		if len(word) > maxSize {
+			maxSize = len(word)
+		}
+	}
+
+	var result string
+	var j int
+
+	for i := maxSize; i >= 1; i-- {
+		for str := range counter[i] {
+			// check if this word can be come up from shorter words
+			for j = len(str) - 1; j > 0; j-- {
+				if !counter[j][str[:j]] {
+					break
+				}
+			}
+
+			// valid word
+			if j == 0 {
+				if len(result) == 0 {
+					result = str
+				} else {
+					// choose smaller lexical
+					for k := 0; k < len(str); k++ {
+						if str[k] > result[k] {
+							break
+						} else if str[k] < result[k] {
+							result = str
+						}
+					}
+				}
+			}
+		}
+
+		if len(result) > 0 {
+			return string(result)
+		}
+	}
+
+	return ""
+}
+
+//	problems
+//	1.	wrong logic about choosing smaller lexical, because it should top when
+//		char is larger than original, or update when char is smaller
+
+//	2.	inspired from sample code
+
+//		I was trying to build a hashmap for size to words, then start from
+//		longest word, traverse down to check if it can be build from scratch
+
+//		but solution provides a way to sort words, then build from smallest
+//		I think this is similar to do searching, choose one after search all
+//		afterwards; if not found, jump to next one and only search from that one
+//		afterwards
+
+//	3.	when building trie, this problem is different from previous one, it
+//		needs to check if all previous words exist
