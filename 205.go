4fa46b4c9a5932b339430a4dc8af5c03e68da259
Aaron Chang <xesuschang@gmail.com>
2020-08-11 12:23:00 +0800

    Squashed commit of 205:
    commit 956723ae3c19d6e125676cd6d13afde0ffe35d2f
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Tue Aug 11 12:20:46 2020 +0800
    
        attempt 2
    
    commit 050788c87311386d9e33e4bc3c57cc43953b11e9
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Tue Aug 11 12:18:54 2020 +0800
    
        attempt 1

--- 205.go
+++ 205.go
@@ -23,8 +23,46 @@ package main
 //
 //Note:
 //You may assume both s and t have the same length.
-
 func isIsomorphic(s string, t string) bool {
+	// make sure every mapping from s -> t is unique
+	mapping := make([]int, 256)
+	for i := range mapping {
+		mapping[i] = -1
+	}
+
+	var idx1, idx2 int
+	for i := range s {
+		idx1, idx2 = int(s[i]), int(t[i])
+
+		if mapping[idx1] > -1 && mapping[idx1] != idx2 {
+			return false
+		}
+
+		mapping[idx1] = idx2
+	}
+
+	// make sure every mapping occurs only once
+	for i := 0; i < len(mapping); i++ {
+		if mapping[i] == -1 {
+			continue
+		}
+
+		if i != mapping[i] && mapping[i] != mapping[mapping[i]] {
+			mapping[i], mapping[mapping[i]] = mapping[mapping[i]], mapping[i]
+			i--
+		}
+	}
+
+	for i := range mapping {
+		if mapping[i] > -1 && i != mapping[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func isIsomorphic1(s string, t string) bool {
 	mapping1 := make(map[rune]rune)
 
 	for i, c := range s {
@@ -51,3 +89,6 @@ func isIsomorphic(s string, t string) bool {
 
 	return true
 }
+
+//	problems
+//	1.	characters mapping, not only alphabet
