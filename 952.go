46acbe25c2fa2a2ad0b61e8738640ab8f0264a5b
Aaron Chang <xesuschang@gmail.com>
2020-09-03 23:15:07 +0800

    Squashed commit of 952:
    commit fea8a75122ef8068d1dc2790b2ea3cb08f750994
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Thu Sep 3 23:08:55 2020 +0800
    
        add comment
    
    commit 1fe6061cca52f338fd8d8d4c859378abaa3be9b3
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Thu Sep 3 22:51:01 2020 +0800
    
        attempt 22
    
    commit a5eee774b53f6fa8e35e4d3d0dcc73a16aac4630
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Thu Sep 3 16:04:03 2020 +0800
    
        attempt 21
    
    commit bafbab22d344f914854f4e60d758a8c45d5bfc44
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Thu Sep 3 10:04:00 2020 +0800
    
        attempt 20
    
    commit 16bb1ba816b7a42568bb66120cee55db7bf51ffb
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Thu Sep 3 10:02:17 2020 +0800
    
        attempt 19
    
    commit 5b261d9acc56114f5275b102663a704c2e2be455
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Wed Sep 2 19:34:43 2020 +0800
    
        attempt 18
    
    commit 5b80c76ba9ae8971431d6437d916a6a372743095
    Author: jamieabc <xesuschang@gmail.com>
    Date:   Wed Sep 2 13:47:13 2020 +0800
    
        add comment
    
    commit a41c66b11f0d879c35b95827bbb4c459e57392c5
    Author: jamieabc <xesuschang@gmail.com>
    Date:   Wed Sep 2 13:06:51 2020 +0800
    
        attempt 17
    
    commit bbf7b7689809ff5727c8b6c2c330a35ae6d1f8d3
    Author: jamieabc <xesuschang@gmail.com>
    Date:   Wed Sep 2 12:42:57 2020 +0800
    
        attempt 16
    
    commit f5e3812a238f46398f417e5ef6316727fb5a01e6
    Author: jamieabc <xesuschang@gmail.com>
    Date:   Tue Sep 1 18:35:18 2020 +0800
    
        attempt 15
    
    commit aa6a478f1e294e659cd0cacc82ffb37087cd6d44
    Author: jamieabc <xesuschang@gmail.com>
    Date:   Tue Sep 1 18:17:26 2020 +0800
    
        attempt 14
    
    commit 1cebc878eed7565ee2314d0e2f04f89b25271dbd
    Author: jamieabc <xesuschang@gmail.com>
    Date:   Tue Sep 1 17:47:15 2020 +0800
    
        attempt 13
    
    commit 9600708ea179f99d1abeb4e15a8598fc4293fdaa
    Author: jamieabc <xesuschang@gmail.com>
    Date:   Tue Sep 1 13:52:04 2020 +0800
    
        attempt 12
    
    commit 7de384c2643215183cb7eb552ef39ce41dc69848
    Author: jamieabc <xesuschang@gmail.com>
    Date:   Mon Aug 31 19:36:08 2020 +0800
    
        attempt 11
    
    commit 37964606e6c020ed9ea267b0753eb0d948c86d9d
    Author: jamieabc <xesuschang@gmail.com>
    Date:   Mon Aug 31 19:31:28 2020 +0800
    
        attempt 10
    
    commit 08acd3afc1c033cc78cc368b4b7ee9055cd2acd9
    Author: jamieabc <xesuschang@gmail.com>
    Date:   Mon Aug 31 19:29:28 2020 +0800
    
        attempt 9
    
    commit e459f8cc6f9ad2c3badc675818d71229e76c7ed4
    Author: jamieabc <xesuschang@gmail.com>
    Date:   Mon Aug 31 18:53:00 2020 +0800
    
        attempt 8
    
    commit f30e5f244b00d99148f0f90fbdb0dec2561c3bbc
    Author: jamieabc <xesuschang@gmail.com>
    Date:   Mon Aug 31 18:47:08 2020 +0800
    
        attempt 7
    
    commit 31da8547fc5e3197abc820e1f9b004a181fba0f2
    Author: jamieabc <xesuschang@gmail.com>
    Date:   Mon Aug 31 18:26:53 2020 +0800
    
        attempt 6
    
    commit 1a0c2ed0d49110dc9e90c0657ec024304ea06a28
    Author: jamieabc <xesuschang@gmail.com>
    Date:   Mon Aug 31 18:10:33 2020 +0800
    
        attempt 5
    
    commit 72b390d2900d94a8ef726ac9500aebca539b1ae2
    Author: jamieabc <xesuschang@gmail.com>
    Date:   Mon Aug 31 14:18:56 2020 +0800
    
        attempt 4
    
    commit d6b2b8a92ebad62fcdc5a8fe8c99dd83e8dcbd69
    Author: jamieabc <xesuschang@gmail.com>
    Date:   Mon Aug 31 13:52:35 2020 +0800
    
        attempt 3
    
    commit 1f0856d01d2c46b0d5685190a768dc591b2f2f49
    Author: jamieabc <xesuschang@gmail.com>
    Date:   Mon Aug 31 13:46:52 2020 +0800
    
        attempt 2
    
    commit 5288e26c0736a514a2cc020f314ca2782704df98
    Author: jamieabc <xesuschang@gmail.com>
    Date:   Mon Aug 31 13:30:47 2020 +0800
    
        attempt 1

---
+++ 952.go
@@ -0,0 +1,234 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math"
+	"strconv"
+	"strings"
+	"time"
+)
+
+//Given a non-empty array of unique positive integers A, consider the following graph:
+//
+//There are A.length nodes, labelled A[0] to A[A.length - 1];
+//There is an edge between A[i] and A[j] if and only if A[i] and A[j] share a common factor greater than 1.
+//
+//Return the size of the largest connected component in the graph.
+//
+//
+//
+//Example 1:
+//
+//Input: [4,6,15,35]
+//Output: 4
+//
+//Example 2:
+//
+//Input: [20,50,9,63]
+//Output: 2
+//
+//Example 3:
+//
+//Input: [2,3,6,7,4,12,21,39]
+//Output: 8
+//
+//Note:
+//
+//1 <= A.length <= 20000
+//1 <= A[i] <= 100000
+
+func main() {
+	start := time.Now()
+
+	data, err := ioutil.ReadFile("txt.txt")
+	if err != nil {
+		panic(err)
+	}
+	input := string(data)
+
+	if len(input) == 0 {
+		return
+	}
+
+	if input[0] == '[' {
+		input = input[1 : len(input)-2]
+	}
+
+	strNums := strings.Split(string(input), ",")
+	nums := make([]int, 0)
+	for i := range strNums {
+		num, _ := strconv.Atoi(strNums[i])
+		nums = append(nums, num)
+	}
+
+	fmt.Println(largestComponentSize(nums))
+
+	elapsed := time.Since(start)
+	fmt.Println("elapsed: ", elapsed)
+}
+
+func largestComponentSize(A []int) int {
+	primeGraph := make(map[int][]int) // 2: [0, 1], primes number 2 can fully divide A[0] & A[1]
+
+	// group factors for every number
+	for i := range A {
+		groupPrimeFactors(i, A, primeGraph)
+	}
+
+	groups := make([]int, len(A))
+	for i := range groups {
+		groups[i] = i
+	}
+
+	// union
+	for _, indexes := range primeGraph {
+		for i := 1; i < len(indexes); i++ {
+			union(groups, indexes[i-1], indexes[i])
+		}
+	}
+
+	// flatten groups, avoid order dependency
+	for i := range groups {
+		parent(groups, i)
+	}
+
+	// count groups
+	counter := make(map[int]int)
+	maxCount := math.MinInt32
+	for _, f := range groups {
+		counter[groups[f]]++
+		maxCount = max(maxCount, counter[groups[f]])
+	}
+
+	return maxCount
+}
+
+func max(i, j int) int {
+	if i >= j {
+		return i
+	}
+	return j
+}
+
+// in order to have all groups follow same order, choose to align to smaller number
+func union(groups []int, to, from int) {
+	groups[parent(groups, from)] = groups[parent(groups, to)]
+}
+
+func parent(groups []int, idx int) int {
+	if groups[idx] != idx {
+		groups[idx] = parent(groups, groups[idx])
+	}
+	return groups[idx]
+}
+
+// O(nk), k: average count of number's primes factors
+func groupPrimeFactors(idx int, nums []int, primeGroups map[int][]int) {
+	num := nums[idx]
+
+	for i := 2; i*i <= num; i++ {
+		if num%i != 0 {
+			continue
+		}
+
+		primeGroups[i] = append(primeGroups[i], idx)
+		for num /= i; num%i == 0; num /= i {
+		}
+	}
+
+	if num > 1 {
+		primeGroups[num] = append(primeGroups[num], idx)
+	}
+}
+
+//	Notes
+//	1.	be careful about common factor limit, not only satisfies n/2, but also
+//		need to satisfies >= 2
+
+//	2.	groups need to be merged when any number has root smaller than number
+//		with smaller index
+
+//	3.	union happens whenever common factor is found, because it also needs to
+//		update root belong to which group
+
+//	4.	to find common factors, every number program starts from 2, so first
+//		number will iterate n times to find common factors, which is a huge
+//		waste of time.
+
+//	5.	as long as a number's group is same as another number's group, no need
+//		to check common factors, avoid un-necessary operations
+
+//	6.	search prime factor up to square root of the number, but need to check
+//		every number even if it's not prime, e.g. sqrt(68) = 9, 2*34 = 4*17
+
+//	7.	when comparing group, need to find group parent instead of it's current
+//		parent, without checking parent, it might cause a loop, e.g. 2's parent
+//		is 1, and 1's parent is 2
+
+//	8.	numbers increase very fast, but primes increase more slower, I would
+//		like to use prime numbers as group index, such that operations could be
+//		reduced dramatically
+
+//	9.	after merge primes into group by numbers, eventually, need to map number
+//		to primes so that count is possible
+
+//	10.	1 is not prime, but since I assume prime starts from 2 with index 0, it
+//		will merge 1 into 2, so need to specially process it
+
+//	11.	for every union, it needs to update at root, otherwise, it might be
+//		changed in the next find
+
+//	12.	inspired from sample code, it's sufficient to check primes up to sqrt(n),
+//		but in a more brilliant way. n = sqrt(n) * sqrt(n), if some prime number
+//		is smaller than sqrt(n) means it "might" exist another prime number
+//		larger thatn sqrt(n)
+
+//		e.g. sqrt(35) = 5.xxxx, check up to 5, 35 / 5 = 7
+
+//		so, how do I know if 7 is another prime, because prime numbers is pre-
+//		calculated only up to 5, which is sqrt(35)
+
+//		the way is to divide n for every prime factors, and it it turns out that
+//		this n after all prime number division still larger than 1, which means
+//		remaining n is prime number
+
+//		e.g. n = 100000, sqrt(100000) = 316, primes : 2, 3, 5, 7, 11, ..., 313
+//		for a given number 94238,, 94238 / 2 = 47119, 47119 cannot be fully
+//		divided by rest of primes => 47119 is another prime number
+
+//		so, with this observation, calculations can be dramatically reduced
+
+//		though it's a brilliant solution to find prime number, I can't think of
+//		a way to use this technique, because when union numbers, each number
+//		should have a group. And in this technique, not all prime numbers are
+//		found in the first time, which means some numbers are missing and is
+//		found during processing numbers.
+
+//		this technique can be used by author is because he uses a graph to
+//		demonstrate edges among numbers, and after graph is build, he traverse
+//		this graph to find maximum groups
+
+//	13.	after thinking, even with union, technique to find primes still can be
+//		used. just add new find prime into all data structures, and also put into
+//		hash to more quickly found
+
+//	14.	inspired from sample code, author provides a quick way to union: not to
+//		to union for each number, instead, create a map to store what prime
+// 		numbers has edges, and do union when this graph is done.
+
+//		it's fast because it can find minimum parent when do the union one time,
+//		unlike my algorithm, it takes several loops for intermediate (un-necessary)
+//		computations
+
+//		since union is done last, so it's okay to use technique to check prime
+//		factors up to sqrt(n), and add new prime number when processing
+
+//		because I think finding all prime number at first is actually not that
+//		efficient, especially when numbers in the array are not that big
+
+//	15.	inspired from solution, union-find tc: O(M * log N), M: union or find
+//		operation count, N: number of elements, but if union-find is called upon
+//		building up, average tc would be O(1)
+
+//		overall tc: O(N * sqrt(K)), K: max number in the list
