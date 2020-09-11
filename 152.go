36762cf5f6311d6ab06fa37ee8937e8474651312
jamieabc <xesuschang@gmail.com>
2020-09-12 00:06:10 +0800

    Squashed commit of 152:
    commit 0ab75781861f85aabeee5ecd9d362d15af922ca8
    Author: jamieabc <xesuschang@gmail.com>
    Date:   Sat Sep 12 00:05:01 2020 +0800
    
        add comment
    
    commit a5fb386c2ab62b4fd5560df90cdd2fef4c43e3f1
    Author: jamieabc <xesuschang@gmail.com>
    Date:   Fri Sep 11 23:12:47 2020 +0800
    
        attempt 4
    
    commit ae49f018cff87bf3174ac33263cdc3c2e0d7a1f4
    Author: jamieabc <xesuschang@gmail.com>
    Date:   Fri Sep 11 23:08:20 2020 +0800
    
        attempt 3
    
    commit 0d8a476bba15d26ae02323215425c0fa649a5cbc
    Author: jamieabc <xesuschang@gmail.com>
    Date:   Fri Sep 11 23:07:20 2020 +0800
    
        attempt 2
    
    commit d04fc3ed475e71205178a932af6e7c4770ccf1a7
    Author: jamieabc <xesuschang@gmail.com>
    Date:   Fri Sep 11 23:05:12 2020 +0800
    
        attempt 1

--- 152.go
+++ 152.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 //Given an integer array nums, find the contiguous subarray within an array (containing at least one number) which has the largest product.
 //
 //Example 1:
@@ -15,6 +17,29 @@ package main
 //Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
 
 func maxProduct(nums []int) int {
+	maxProduct := math.MinInt32
+	maxSoFar, minSoFar := 1, 1
+
+	for _, n := range nums {
+		if n == 0 {
+			maxSoFar, minSoFar = 1, 1
+			maxProduct = max(maxProduct, 0)
+		} else if n > 0 {
+			maxSoFar *= n
+			maxProduct = max(maxProduct, maxSoFar)
+			minSoFar = min(1, minSoFar*n)
+		} else {
+			tmp := minSoFar * n
+			maxProduct = max(maxProduct, tmp)
+			minSoFar = min(n, maxSoFar*n)
+			maxSoFar = max(1, tmp)
+		}
+	}
+
+	return maxProduct
+}
+
+func maxProduct1(nums []int) int {
 	length := len(nums)
 	if length == 0 {
 		return 0
@@ -58,3 +83,52 @@ func min(i, j int) int {
 	}
 	return j
 }
+
+//	Notes
+//	1.	when multiply, need 2 information, if current number is included, max
+//		product could come from following current number conditions:
+//		- positive: current number * all previous positive maximum product
+//		- negative: current number * all previous negative minimum product
+
+//		basic thinking: include this number or not:
+//		if only use current number, max or min could only be previous with current
+//		if current number not included, then product default to 1
+
+//	2.	in order to have multiply number, set default positive & negative to 1,
+//		but this number should not influence max product, in other words, if all
+//		numbers are negative, max product should not be 1
+
+//	3.	inspired from solution, variable namings are better: maxSoFar & minSoFar
+
+//	4.	inspired from https://leetcode.com/problems/maximum-product-subarray/discuss/183483/JavaC%2B%2BPython-it-can-be-more-simple
+
+//		lee provides a very brilliant solution, which comes from the fact that
+//		max product must be continuous.
+
+//		if there's no zero, odd negative numbers
+//		_ _ _ _ _ -1 _ _ _ _ _ _ _ _ _
+//      <-------->   <---------------->
+//         A                B
+//		max product comes from either A or B
+
+//		if there's no zero, even negative numbers
+//		_ _ _ _ _ -1 _ _ _ _ -1 _ _ _ _ _ _
+//		<-------->  <------->  <----------->
+//           A          B            C
+//      <----------------------------------->
+//					    D
+//		max product comes from A, B, C, D
+
+//		if there more than 1 odd negative numbers
+//		_ _ _ _ _ -1 _ _ _ _ -1 _ _ _ _ _ _ -1 _ _ _ _ _ _
+//		<-------->  <------->  <----------->  <----------->
+//           A          B            C				D
+//		<-------------------->
+//				E
+//					<-------------------------------------->
+//										F
+//		max product comes from A, B, C, D, E, F
+
+//		overall, scan forward & backward, max product will always with in range
+
+//		if encounter any zero, reset product to 1 and keep following rules
