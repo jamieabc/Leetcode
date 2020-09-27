05aac6adaee3035d318cd7adec1b16d8748fd797
Aaron Chang <xesuschang@gmail.com>
2020-09-27 14:30:53 +0800

    Squashed commit of 1600:
    commit 07bbfcc28ef6316de7a7eec0b28e91fb4881ab2a
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sun Sep 27 14:22:26 2020 +0800
    
        attempt 4

---
+++ 1600.go
@@ -0,0 +1,140 @@
+package main
+
+//A kingdom consists of a king, his children, his grandchildren, and so on. Every once in a while, someone in the family dies or a child is born.
+//
+//The kingdom has a well-defined order of inheritance that consists of the king as the first member. Let's define the recursive function Successor(x, curOrder), which given a person x and the inheritance order so far, returns who should be the next person after x in the order of inheritance.
+//
+//Successor(x, curOrder):
+//    if x has no children or all of x's children are in curOrder:
+//        if x is the king return null
+//        else return Successor(x's parent, curOrder)
+//    else return x's oldest child who's not in curOrder
+//
+//For example, assume we have a kingdom that consists of the king, his children Alice and Bob (Alice is older than Bob), and finally Alice's son Jack.
+//
+//    In the beginning, curOrder will be ["king"].
+//    Calling Successor(king, curOrder) will return Alice, so we append to curOrder to get ["king", "Alice"].
+//    Calling Successor(Alice, curOrder) will return Jack, so we append to curOrder to get ["king", "Alice", "Jack"].
+//    Calling Successor(Jack, curOrder) will return Bob, so we append to curOrder to get ["king", "Alice", "Jack", "Bob"].
+//    Calling Successor(Bob, curOrder) will return null. Thus the order of inheritance will be ["king", "Alice", "Jack", "Bob"].
+//
+//Using the above function, we can always obtain a unique order of inheritance.
+//
+//Implement the ThroneInheritance class:
+//
+//    ThroneInheritance(string kingName) Initializes an object of the ThroneInheritance class. The name of the king is given as part of the constructor.
+//    void birth(string parentName, string childName) Indicates that parentName gave birth to childName.
+//    void death(string name) Indicates the death of name. The death of the person doesn't affect the Successor function nor the current inheritance order. You can treat it as just marking the person as dead.
+//    string[] getInheritanceOrder() Returns a list representing the current order of inheritance excluding dead people.
+//
+//
+//
+//Example 1:
+//
+//Input
+//["ThroneInheritance", "birth", "birth", "birth", "birth", "birth", "birth", "getInheritanceOrder", "death", "getInheritanceOrder"]
+//[["king"], ["king", "andy"], ["king", "bob"], ["king", "catherine"], ["andy", "matthew"], ["bob", "alex"], ["bob", "asha"], [null], ["bob"], [null]]
+//Output
+//[null, null, null, null, null, null, null, ["king", "andy", "matthew", "bob", "alex", "asha", "catherine"], null, ["king", "andy", "matthew", "alex", "asha", "catherine"]]
+//
+//Explanation
+//ThroneInheritance t= new ThroneInheritance("king"); // order: king
+//t.birth("king", "andy"); // order: king > andy
+//t.birth("king", "bob"); // order: king > andy > bob
+//t.birth("king", "catherine"); // order: king > andy > bob > catherine
+//t.birth("andy", "matthew"); // order: king > andy > matthew > bob > catherine
+//t.birth("bob", "alex"); // order: king > andy > matthew > bob > alex > catherine
+//t.birth("bob", "asha"); // order: king > andy > matthew > bob > alex > asha > catherine
+//t.getInheritanceOrder(); // return ["king", "andy", "matthew", "bob", "alex", "asha", "catherine"]
+//t.death("bob"); // order: king > andy > matthew > bob > alex > asha > catherine
+//t.getInheritanceOrder(); // return ["king", "andy", "matthew", "alex", "asha", "catherine"]
+//
+//
+//
+//Constraints:
+//
+//    1 <= kingName.length, parentName.length, childName.length, name.length <= 15
+//    kingName, parentName, childName, and name consist of lowercase English letters only.
+//    All arguments childName and kingName are distinct.
+//    All name arguments of death will be passed to either the constructor or as childName to birth first.
+//    For each call to birth(parentName, childName), it is guaranteed that parentName is alive.
+//    At most 105 calls will be made to birth and death.
+//    At most 10 calls will be made to getInheritanceOrder.
+
+type Node struct {
+	Name     string
+	Dead     bool
+	Children []*Node
+}
+
+type ThroneInheritance struct {
+	King   *Node
+	Family map[string]*Node
+}
+
+func Constructor(kingName string) ThroneInheritance {
+	n := &Node{
+		Name:     kingName,
+		Children: make([]*Node, 0),
+	}
+
+	family := make(map[string]*Node)
+	family[kingName] = n
+
+	return ThroneInheritance{
+		King:   n,
+		Family: family,
+	}
+}
+
+func (this *ThroneInheritance) Birth(parentName string, childName string) {
+	n := &Node{
+		Name:     childName,
+		Children: make([]*Node, 0),
+	}
+	this.Family[childName] = n
+
+	this.Family[parentName].Children = append(this.Family[parentName].Children, n)
+}
+
+func (this *ThroneInheritance) Death(name string) {
+	this.Family[name].Dead = true
+}
+
+func (this *ThroneInheritance) GetInheritanceOrder() []string {
+	stack := []*Node{this.King}
+	result := make([]string, 0)
+
+	// dfs
+	for len(stack) > 0 {
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		if !node.Dead {
+			result = append(result, node.Name)
+		}
+
+		for i := len(node.Children) - 1; i >= 0; i-- {
+			stack = append(stack, node.Children[i])
+		}
+	}
+
+	return result
+}
+
+/**
+ * Your ThroneInheritance object will be instantiated and called as such:
+ * obj := Constructor(kingName);
+ * obj.Birth(parentName,childName);
+ * obj.Death(name);
+ * param_3 := obj.GetInheritanceOrder();
+ */
+
+//	Notes
+
+//	1.	TLE for first 3 submissions, for tree traversal, each time searching
+//		takes O(n) which could be costly, one way to accelerate is using a map
+//		to store every node position by name
+
+//		also, this could be a hint that every name is distinct which states a
+//		map could be used
