8015df629f7d7636c44187742c471aff0dd7b8df
Aaron Chang <xesuschang@gmail.com>
2020-10-02 09:36:07 +0800

    Squashed commit of 933:
    commit 252fa17bec941883eca9abcf3a9e048090715100
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Oct 2 09:35:39 2020 +0800
    
        add comment
    
    commit 63db53187ed07a63b1bd2a4d4788b9122a4f48dc
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Oct 2 09:31:27 2020 +0800
    
        attempt 2

---
+++ 933.go
@@ -0,0 +1,102 @@
+package main
+
+// You have a RecentCounter class which counts the number of recent requests within a certain time frame.
+//
+// Implement the RecentCounter class:
+//
+//     RecentCounter() Initializes the counter with zero recent requests.
+//     int ping(int t) Adds a new request at time t, where t represents some time in milliseconds, and returns the number of requests that has happened in the past 3000 milliseconds (including the new request). Specifically, return the number of requests that have happened in the inclusive range [t - 3000, t].
+//
+// It is guaranteed that every call to ping uses a strictly larger value of t than the previous call.
+//
+//
+//
+// Example 1:
+//
+// Input
+// ["RecentCounter", "ping", "ping", "ping", "ping"]
+// [[], [1], [100], [3001], [3002]]
+// Output
+// [null, 1, 2, 3, 3]
+//
+// Explanation
+// RecentCounter recentCounter = new RecentCounter();
+// recentCounter.ping(1);     // requests = [1], range is [-2999,1], return 1
+// recentCounter.ping(100);   // requests = [1, 100], range is [-2900,100], return 2
+// recentCounter.ping(3001);  // requests = [1, 100, 3001], range is [1,3001], return 3
+// recentCounter.ping(3002);  // requests = [1, 100, 3001, 3002], range is [2,3002], return 3
+//
+//
+//
+// Constraints:
+//
+//     1 <= t <= 109
+//     Each test case will call ping with strictly increasing values of t.
+//     At most 104 calls will be made to ping.
+
+type RecentCounter struct {
+	Data []int
+	Idx  int
+}
+
+func Constructor() RecentCounter {
+	return RecentCounter{
+		Data: make([]int, 3000),
+	}
+}
+
+func (this *RecentCounter) Ping(t int) int {
+	start := this.Idx % len(this.Data)
+	smallest := max(1, t-len(this.Data))
+	var count, i int
+
+	// it could happen that the value to replace in range
+	if this.Data[start] >= smallest {
+		count++
+	}
+
+	this.Data[start] = t
+	this.Idx++
+
+	// former closest call
+	for i = start; i >= 0; i-- {
+		if this.Data[i] >= smallest {
+			count++
+		} else {
+			break
+		}
+	}
+
+	// if all former closest calls are valid, keep going to check former calls
+	if i < 0 {
+		for i = len(this.Data) - 1; i > start; i-- {
+			if this.Data[i] >= smallest {
+				count++
+			} else {
+				break
+			}
+		}
+	}
+
+	return count
+}
+
+func max(i, j int) int {
+	if i >= j {
+		return i
+	}
+	return j
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Ping(t);
+ */
+
+//	Notes
+//	1.	time starts from 1, but index start 0 from, need to reduce t by 1 when
+//		finding circular array index
+
+//	2.	since t is included by time range [t-3000, t] with inclusion, it should
+//		at most 3001 valid calls, so need to check before updating
