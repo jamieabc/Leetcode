553bf6708cc87a793c01d5a55ff747110fbc0915
Aaron Chang <xesuschang@gmail.com>
2020-10-08 16:20:03 +0800

    Squashed commit of 311:
    commit dc3823166316aa287ffc851ae8cb1f6187e74e12
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Thu Oct 8 16:19:50 2020 +0800
    
        add comment

--- 311.go
+++ 311.go
@@ -52,7 +52,7 @@ func multiply(A [][]int, B [][]int) [][]int {
 	return result
 }
 
-//	problems
+//	Notes
 //	1.	Wrong index of product matrix
 
 //	2.	Optimization, store A's row & B's column that contains non-zero
@@ -63,15 +63,28 @@ func multiply(A [][]int, B [][]int) [][]int {
 //	3. reference from https://leetcode.com/problems/sparse-matrix-multiplication/discuss/76151/54ms-Detailed-Summary-of-Easiest-JAVA-solutions-Beating-99.9
 
 //		check for first array non-zero value
-//		convert second array into sequence of non-zero data
-//		B = | 1 0 0 1 |
-//		    | 0 0 0 0 |
-//		    | 1 0 0 0 |
 
-//		=> [ [ [0, 1], [3, 1] ], [[]], [ [ [0, 1] ] ] ]
+//		space compression
+//		convert second array into sequence of non-zero data (index, value)
+//		B = | 1 0 0 1 |     => [ [ (0, 1), (3, 1)]  value 1 at index 0, 3 at index 1
+//		    | 0 0 0 0 |			 []
+//		    | 1 0 0 0 |			 [ (0, 1) ] ]       value 1 at index 0
+
 //		CMU sparse matrix http://www.cs.cmu.edu/~scandal/cacm/node9.html
 
 //	4.	another reference https://github.com/SCIN/Facebook-Interview-Coding-1/blob/master/Sparce%20Matrix%20Multiplication.java
 
 //		this one is more details in the interview process, use list to save
 //		space, sorted index, different length of matrix, etc. Helpful
+
+//	5.	inspired from https://leetcode.com/problems/sparse-matrix-multiplication/discuss/419538/What-the-interviewer-is-expecting-when-this-problem-is-asked-in-an-interview...
+
+//		use 2 pointers to compute, because cik = aij * bjk,
+//		data structure: idx1: [0, 1, 3]
+//					 	val1: [4, 10, 20]
+
+//		index 0 w/ val 4, index 1 w/ val 10, index 3 w/ val 20
+
+//		the other follow up might be one array is much longer than the other one
+//		can use shorter one as base, binary search longer one, use use searched
+//		index as next start point
