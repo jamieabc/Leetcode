8d3e95b84bfb91f0448956f6b355e3700ed6d574
Aaron Chang <xesuschang@gmail.com>
2021-01-23 14:48:36 +0800

    Squashed commit of 739:
    commit 0edc0331efc8e5c48410b8a0a2184da9d76751a4
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sat Jan 23 14:48:20 2021 +0800
    
        add comment
    
    commit 74bc6d0b6c4c3388fd11204c7b531d0ee34a4501
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sat Jan 23 14:47:36 2021 +0800
    
        attempt 2

--- 739.go
+++ 739.go
@@ -7,6 +7,19 @@ package main
 // Note: The length of temperatures will be in the range [1, 30000]. Each temperature will be an integer in the range [30, 100].
 
 func dailyTemperatures(T []int) []int {
+	size := len(T)
+	ans := make([]int, size)
+
+	for i := size - 1; i >= 0; i-- {
+		for j := i - 1; j >= 0 && T[j] < T[i]; j-- {
+			ans[j] = i - j
+		}
+	}
+
+	return ans
+}
+
+func dailyTemperatures1(T []int) []int {
 	stack := make([]int, 0)
 	result := make([]int, len(T))
 
@@ -22,7 +35,7 @@ func dailyTemperatures(T []int) []int {
 	return result
 }
 
-//	problems
+//	Notes
 //	1.	inspired by https://leetcode.com/problems/daily-temperatures/discuss/109832/Java-Easy-AC-Solution-with-Stack
 
 //		no need to have if checking, because stack size checking is also done
@@ -30,3 +43,31 @@ func dailyTemperatures(T []int) []int {
 //		the reason to have j is to know what's the distance from current
 //		temperature, so author saves index into stack to reduce additional
 //		j checking
+
+//	2.	inspired from https://leetcode.com/problems/daily-temperatures/discuss/121787/C%2B%2B-Clean-code-with-explanation%3A-O(n)-time-and-O(1)-space-(beats-99.13)
+
+//		tc remains O(n) and sc can be further reduced down to O(1), the trick is that finding
+//		next larger number is a mono-increasing. viewing backward if mono-decreasing, start
+//		backward, once larger number is found, all number after that will be determined by
+//		that number
+
+//		e.g. 5, 8, 9, 4, 3, 2, 10, 6
+//		ans	 0, 0, 0, 0, 0, 0,  0, 0
+
+//								v  ^		start from 6 backward, 10 is larger
+//			 0, 0, 0, 0, 0, 0,  0, 1
+
+//			 v				    ^			start from 10 backward, no any larger, update
+//			 6, 5, 4, 3, 2, 1,  0, 1
+
+//			 			 v	^    			start from 2 backward, 3 is larger
+//			 6, 5, 4, 3, 2, 1,  0, 1
+
+//			 		  v	 ^	     			start from 3 backward, 4 is larger
+//			 6, 5, 4, 3, 2, 1,  0, 1
+
+//			 	   v  ^	  	     			start from 4 backward, 9 is larger
+//			 6, 5, 4, 3, 2, 1,  0, 1
+
+//			 v	   ^   	  	     			start from 9 backward, no any larger, update
+//			 2, 1, 4, 3, 2, 1,  0, 1
