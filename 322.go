7ba64f9d02c93207ae5e4a55d2ea496a928ebda7
Aaron Chang <xesuschang@gmail.com>
2021-01-23 13:23:35 +0800

    Squashed commit of 322:
    commit e4ddf2be76b06bffccce87581a5ce7c81d9040c9
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sat Jan 23 13:18:51 2021 +0800
    
        attempt 3
    
    commit 6eaf23cc8d1e51a412a74bd01b10977dbd6ae704
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sat Jan 23 13:17:44 2021 +0800
    
        attempt 2
    
    commit b9c704b6fb327a1772adf79d7528e3a71f9d81fa
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sat Jan 23 13:15:32 2021 +0800
    
        attempt 1

---
+++ 322.go
@@ -0,0 +1,94 @@
+package main
+
+import "math"
+
+// You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+//
+// You may assume that you have an infinite number of each kind of coin.
+//
+//
+//
+// Example 1:
+//
+// Input: coins = [1,2,5], amount = 11
+// Output: 3
+// Explanation: 11 = 5 + 5 + 1
+//
+// Example 2:
+//
+// Input: coins = [2], amount = 3
+// Output: -1
+//
+// Example 3:
+//
+// Input: coins = [1], amount = 0
+// Output: 0
+//
+// Example 4:
+//
+// Input: coins = [1], amount = 1
+// Output: 1
+//
+// Example 5:
+//
+// Input: coins = [1], amount = 2
+// Output: 2
+//
+//
+//
+// Constraints:
+//
+//     1 <= coins.length <= 12
+//     1 <= coins[i] <= 231 - 1
+//     0 <= amount <= 104
+
+func coinChange(coins []int, amount int) int {
+	// dp default to 0, and final checking condition is also 0, need to check additionally
+	if amount == 0 {
+		return 0
+	}
+
+	// dp[i]: min # of coins to reach that value
+	dp := make([]int, amount+1)
+
+	for i := range coins {
+		if coins[i] <= amount {
+			dp[coins[i]] = 1
+		}
+	}
+
+	for i := 1; i <= amount; i++ {
+		if dp[i] == 0 {
+			continue
+		}
+
+		for j := range coins {
+			target := coins[j] + i
+
+			if target <= amount {
+				if dp[target] == 0 {
+					dp[target] = dp[i] + 1
+				} else {
+					dp[target] = min(dp[target], dp[i]+1)
+				}
+			}
+		}
+	}
+
+	if dp[amount] == 0 {
+		return -1
+	}
+
+	return dp[amount]
+}
+
+func min(i, j int) int {
+	if i <= j {
+		return i
+	}
+	return j
+}
+
+//	Notes
+//	1.	fill dp default math.Maxint32 is just as a mark ot make min work, can
+//		also be done by check if dp[i] == 0
