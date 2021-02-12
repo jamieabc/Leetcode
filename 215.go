8d596c896941c3a603e9397acc5f7f8d763b901d
Aaron Chang <xesuschang@gmail.com>
2021-02-12 09:06:08 +0800

    Squashed commit of 215:
    commit 30b73607a9566f8199ccd7b40c8571a35674a619
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Feb 12 09:05:37 2021 +0800
    
        add comment
    
    commit 86fc4da1779e6253fc64f4f48f1188f1a8c9152b
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Feb 12 09:04:40 2021 +0800
    
        attempt 3
    
    commit 819b7474c74e3147763ee55c1dbd38bf726732ec
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Feb 12 08:58:12 2021 +0800
    
        attempt 2

--- 215.go
+++ 215.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"container/heap"
+	"math/rand"
+)
+
 // Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
 //
 // Example 1:
@@ -14,182 +19,81 @@ package main
 //
 // Note:
 // You may assume k is always valid, 1 ≤ k ≤ array's length.
+
+// tc: average O(n), worst O(n^2)
 func findKthLargest(nums []int, k int) int {
-	quickSelect(nums, 0, len(nums)-1, k)
+	rand.Shuffle(len(nums), func(i, j int) {
+		nums[i], nums[j] = nums[j], nums[i]
+	})
+
+	// kth from 1 ~ n, so convert it to index base
+	quickSelect(nums, 0, len(nums)-1, k-1)
 
 	return nums[k-1]
 }
 
-func quickSelect(nums []int, start, end, target int) {
-	if start >= end {
-		return
-	}
-
-	pivot := partition(nums, start, end)
-	if pivot == target-1 {
-		return
-	} else if pivot < target {
-		quickSelect(nums, pivot+1, end, target)
-	} else {
-		quickSelect(nums, start, pivot-1, target)
-	}
-}
-
-func partition(nums []int, start, end int) int {
+func quickSelect(nums []int, target, start, end int) int {
+	store := start
 	nums[start], nums[end] = nums[end], nums[start]
-	storeAt := start
 
+	// partition
 	for i := start; i < end; i++ {
-		if nums[i] >= nums[end] {
-			nums[i], nums[storeAt] = nums[storeAt], nums[i]
-			storeAt++
+		// becareful, it's kth largest, order is desc
+		if nums[i] > nums[end] {
+			nums[store], nums[i] = nums[i], nums[store]
+			store++
 		}
 	}
 
-	nums[storeAt], nums[end] = nums[end], nums[storeAt]
+	nums[store], nums[end] = nums[end], nums[store]
 
-	return storeAt
-}
-
-func findKthLargest1(nums []int, k int) int {
-	length := len(nums)
-	start, end, idx := 0, length-1, length-k
-	for start < end {
-		tmp := partition(nums, start, end)
-		if tmp > idx {
-			end = tmp - 1
-		} else if tmp < idx {
-			start = tmp + 1
-		} else {
-			return nums[tmp]
-		}
+	if store == target {
+		return nums[store]
+	} else if store > target {
+		return quickSelect(nums, target, start, store-1)
 	}
-	return nums[idx]
-}
-
-func partition(nums []int, start, end int) int {
-	pivot := start
-	for start <= end {
-		for start <= end && nums[start] <= nums[pivot] {
-			start++
-		}
-		for start <= end && nums[end] > nums[pivot] {
-			end--
-		}
-
-		if start <= end {
-			nums[start], nums[end] = nums[end], nums[start]
-		}
-	}
-	nums[pivot], nums[end] = nums[end], nums[pivot]
-	return end
+	return quickSelect(nums, target, store+1, end)
 }
 
 // min heap
-type heap struct {
-	data []int
-	size int
+type MinHeap []int
+
+func (h MinHeap) Len() int           { return len(h) }
+func (h MinHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h MinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h MinHeap) Peek() int          { return h[0] }
+
+func (h *MinHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
 }
 
-func (h *heap) parent(i int) int {
-	return (i - 1) / 2
+func (h *MinHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
 }
 
-func (h *heap) left(i int) int {
-	l := (i * 2) + 1
-	if l >= len(h.data) {
-		return -1
-	}
-	return l
-}
-
-func (h *heap) right(i int) int {
-	r := (i * 2) + 2
-	if r >= len(h.data) {
-		return -1
-	}
-	return r
-}
-
-func (h *heap) bubbleUp(i int) {
-	if i == 0 {
-		h.bubbleDown(0)
-		return
-	}
-	p := h.parent(i)
-
-	if h.data[p] > h.data[i] {
-		h.swap(p, i)
-		h.bubbleUp(p)
-	} else {
-		h.bubbleDown(i)
-	}
-}
-
-func (h *heap) bubbleDown(i int) {
-	l, r := h.left(i), h.right(i)
-
-	if l == -1 && r == -1 {
-		return
-	} else if l != -1 && r != -1 {
-		if h.data[i] <= h.data[l] && h.data[i] <= h.data[r] {
-			return
-		}
-
-		if h.data[l] <= h.data[r] {
-			h.swap(l, i)
-			h.bubbleDown(l)
-		} else {
-			h.swap(r, i)
-			h.bubbleDown(r)
-		}
-	} else if r == -1 {
-		if h.data[l] <= h.data[i] {
-			h.swap(l, i)
-		}
-	}
-}
-
-func (h *heap) swap(dst, src int) {
-	h.data[src], h.data[dst] = h.data[dst], h.data[src]
-}
-
-func (h *heap) insert(n int) {
-	if len(h.data) < h.size {
-		// not full
-		h.data = append(h.data, n)
-		h.bubbleUp(len(h.data) - 1)
-	} else {
-		if n > h.data[0] {
-			h.swap(0, len(h.data)-1)
-			h.data[len(h.data)-1] = n
-			h.bubbleDown(0)
-			h.bubbleUp(len(h.data) - 1)
-		}
-	}
-}
-
+// tc: O(n log(k))
 func findKthLargest1(nums []int, k int) int {
-	h := &heap{
-		data: make([]int, 0),
-		size: k,
-	}
+	h := &MinHeap{}
+	heap.Init(h)
 
 	for _, n := range nums {
-		h.insert(n)
+		if h.Len() == k && h.Peek() < n {
+			heap.Pop(h)
+		}
+
+		if h.Len() < k {
+			heap.Push(h, n)
+		}
 	}
 
-	return h.data[0]
+	return h.Peek()
 }
 
-func min(i, j int) int {
-	if i <= j {
-		return i
-	}
-	return j
-}
-
-//	problems
+//	Notes
 //	1.	when finding kth largest number, I should use min heap to make sure
 //		all other number in the heap are larger, means root number is kth
 //		largest
