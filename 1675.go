08e2e3fe7bf0a9a850459a1f9f48313d8fe367e8
Aaron Chang <xesuschang@gmail.com>
2021-02-20 21:04:30 +0800

    Squashed commit of 1675:
    commit ecd2b6f35f251bcbd2f3da3bee0eb338d2bce454
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sat Feb 20 21:02:33 2021 +0800
    
        add comment
    
    commit f8e245d7bcee28479c5184b96d4f6c6d702e5401
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sat Feb 20 19:51:34 2021 +0800
    
        attempt 5
    
    commit 19cea45c1986bb6700df42e3c98cbb40e359263f
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sat Feb 20 19:48:32 2021 +0800
    
        attempt 4
    
    commit 06c2b8e4add1acf5daf0bb7ca2622a6f2a167021
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Feb 12 11:28:41 2021 +0800
    
        attempt 3
    
    commit 0db0c4e51c46e24b0c297d65b9599a872d7c25af
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Feb 12 11:17:27 2021 +0800
    
        attempt 2
    
    commit fd9c15f673cc237da1c309c6b7b3830099567f10
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Feb 12 11:16:43 2021 +0800
    
        attempt 1
    
    commit 54b3b98f00729746d23af5fef678adc30b008107
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Feb 5 18:42:42 2021 +0800
    
        add comment

---
+++ 1675.go
@@ -0,0 +1,167 @@
+package main
+
+import (
+	"math"
+	"sort"
+)
+
+// You are given an array nums of n positive integers.
+//
+// You can perform two types of operations on any element of the array any number of times:
+//
+//     If the element is even, divide it by 2.
+//         For example, if the array is [1,2,3,4], then you can do this operation on the last element, and the array will be [1,2,3,2].
+//     If the element is odd, multiply it by 2.
+//         For example, if the array is [1,2,3,4], then you can do this operation on the first element, and the array will be [2,2,3,4].
+//
+// The deviation of the array is the maximum difference between any two elements in the array.
+//
+// Return the minimum deviation the array can have after performing some number of operations.
+//
+//
+//
+// Example 1:
+//
+// Input: nums = [1,2,3,4]
+// Output: 1
+// Explanation: You can transform the array to [1,2,3,2], then to [2,2,3,2], then the deviation will be 3 - 2 = 1.
+//
+// Example 2:
+//
+// Input: nums = [4,1,5,20,3]
+// Output: 3
+// Explanation: You can transform the array after two operations to [4,2,5,5,3], then the deviation will be 5 - 2 = 3.
+//
+// Example 3:
+//
+// Input: nums = [2,10,8]
+// Output: 3
+//
+//
+//
+// Constraints:
+//
+//     n == nums.length
+//     2 <= n <= 105
+//     1 <= nums[i] <= 109
+
+type MaxHeap []int
+
+func (h MaxHeap) Len() int           { return len(h) }
+func (h MaxHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h MaxHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h MaxHeap) Peek() int          { return h[0] }
+
+func (h *MaxHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *MaxHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[:n-1]
+	return x
+}
+
+func minimumDeviation(nums []int) int {
+	for i := range nums {
+		if nums[i]&1 > 0 {
+			nums[i] = nums[i] << 1
+		}
+	}
+
+	low := math.MaxInt32
+	for _, n := range nums {
+		low = min(low, n)
+	}
+
+	maxHeap := &MaxHeap{}
+	heap.Init(maxHeap)
+
+	for _, n := range nums {
+		heap.Push(maxHeap, n)
+	}
+
+	ans := maxHeap.Peek() - low
+
+	// try to reduce deviation by half largest number, if number
+	// cannot be half (already even), then stop
+	for true {
+		n := heap.Pop(maxHeap).(int)
+		if n&1 > 0 {
+			break
+		}
+
+		low = min(low, n>>1)
+		heap.Push(maxHeap, n>>1)
+		ans = min(ans, maxHeap.Peek()-low)
+	}
+
+	return ans
+}
+
+func min(i, j int) int {
+	if i <= j {
+		return i
+	}
+	return j
+}
+
+//	Notes
+//	1.	odd -> become larger, odd -> become smaller
+//		this problem cares only about largest & smallest, to make it simpler, make all
+//		numbers to odd
+
+//	2.	sorted by value ascending
+//		smallest ... ... ... ... largest
+
+//		to make deviation smaller, at least one number in list should remain same, if
+//		all numbers are double, then deviation also double
+
+//		case 1: current_smallest * 2 < next smaller
+//
+//				deviation shrinks to largest - current_smallest * 2, stop
+
+//		case 2: next number < current_smallest * 2 < largest
+
+//				all numbers are sorted, and it's a non-decreasing sequence
+//				new smallest could be current_smallest * 2 (if next number * 2 < largest)
+//				or if next number  * 2 > largest, deviation is changed to
+//				min(next number * 2 - min(largest, current_smallest * 2,
+//				    largest - current_smallest)
+
+//		case 3: current_smallest * 2 > largest
+
+//	3.	it could happen that original array has smallest deviation
+
+//	4.	after operation, largest & smallest may change, needs to sort again
+
+//	5.	find original largest & smallest, even with this, still not be correct
+
+//		original [10, 4, 3] => deviation = 7
+//		change   [2, 3, 5]  => deviation = 3
+
+// 		logic is still wrong
+
+//	6.	inspired from https://youtu.be/OTlusbuZX94?t=1371
+
+//		odd => larger, even => smaller, the strategy is to double all odd numbers
+//		then use heap to track max numbers, if it's even, half it, need to also check
+//		if new half number will be smaller than current minimum
+
+//		I have come up with similar idea to make all even numbers to odd, but I guess
+//		this is in wrong direction, because there could have multiple division, where
+//		to stop?
+
+//		e.g. 24 -> 12 -> 6 -> 3
+
+//		and start from odd will be a pretty good strategy, because every number
+//		doubled is even
+
+//		I guess the most tricky part is how to guarantee answer within this way of operation
+
+//	7.	inspired from https://leetcode.com/problems/minimize-deviation-in-array/discuss/955262/C%2B%2B-intuitions-and-flip
+
+//		voturbac a faster way of solving this: make array as heap, can operate directly
+//		on array
