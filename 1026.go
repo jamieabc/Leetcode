3f96f214cebc9aac39e735cdc553ea056356a0ee
Aaron Chang <xesuschang@gmail.com>
2021-03-03 18:36:02 +0800

    Squashed commit of 1026:
    commit 80f46c3e82a60da483534a90a3c68c888f95023b
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Wed Mar 3 18:35:21 2021 +0800
    
        attempt 2

--- 1026.go
+++ 1026.go
@@ -37,6 +37,68 @@ import "math"
  */
 
 func maxAncestorDiff(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	return dfs(root, root.Val, root.Val)
+}
+
+func dfs(node *TreeNode, largest, smallest int) int {
+	if node == nil {
+		return largest - smallest
+	}
+
+	largest = max(largest, node.Val)
+	smallest = min(smallest, node.Val)
+
+	return max(dfs(node.Left, largest, smallest), dfs(node.Right, largest, smallest))
+}
+
+func maxAncestorDiff3(root *TreeNode) int {
+	var maxDiff int
+
+	dfs(root, &maxDiff)
+
+	return maxDiff
+}
+
+func dfs3(node *TreeNode, maxDiff *int) (int, int) {
+	if node == nil {
+		return -1, -1
+	}
+
+	lMin, lMax := dfs3(node.Left, maxDiff)
+	rMin, rMax := dfs3(node.Right, maxDiff)
+
+	smallest, largest := node.Val, node.Val
+
+	if lMin != -1 {
+		*maxDiff = max(
+			*maxDiff,
+			max(
+				abs(node.Val-lMin),
+				abs(node.Val-lMax),
+			),
+		)
+		smallest, largest = min(smallest, lMin), max(largest, lMax)
+	}
+
+	if rMin != -1 {
+		*maxDiff = max(
+			*maxDiff,
+			max(
+				abs(node.Val-rMin),
+				abs(node.Val-rMax),
+			),
+		)
+		smallest, largest = min(smallest, rMin), max(largest, rMax)
+	}
+
+	return smallest, largest
+}
+
+func maxAncestorDiff2(root *TreeNode) int {
 	return dfs(root, root.Val, root.Val)
 }
 
@@ -103,7 +165,7 @@ func abs(i, j int) int {
 	return j - i
 }
 
-//	problems
+//	Notes
 //	1.	forget about conditions that maximum value might come from max-min or
 //		min-max
 
@@ -111,3 +173,11 @@ func abs(i, j int) int {
 
 //		lee uses dfs, passes max & min to leaf nodes to decide what's largest
 //		difference
+
+//	3.	use a complex way to solve this, I know problem wants (max, min) on
+//		every path, so I try bottom-up approach. But it's really a waste,
+//		because if only thing to know is (max, min), then can pass it down to
+//		leaf node. If has less condition checking
+
+//		also, it reduces check for abs, since max & min makes difference
+//		positive
