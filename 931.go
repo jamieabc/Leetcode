1ff1a47bd9008a3ec97d48dfdd4dba7854269a52
Aaron Chang <xesuschang@gmail.com>
2021-03-05 20:31:31 +0800

    Squashed commit of 931:
    commit cc609b835ef889623f04772b4008113f31dc62ef
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Mar 5 20:31:15 2021 +0800
    
        add comment
    
    commit fa373b6130a86c6e61e33aaefa631b43f9619953
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Mar 5 20:29:08 2021 +0800
    
        attempt 2

--- 931.go
+++ 931.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 // Given a square array of integers A, we want the minimum sum of a falling path through A.
 //
 // A falling path starts at any element in the first row, and chooses one element from each row.  The next row's choice must be in a column that is different from the previous row's column by at most one.
@@ -26,7 +28,46 @@ package main
 //     1 <= A.length == A[0].length <= 100
 //     -100 <= A[i][j] <= 100
 
-func minFallingPathSum(A [][]int) int {
+func minFallingPathSum(matrix [][]int) int {
+	w, h := len(matrix[0]), len(matrix)
+
+	// dp[i][j]: minimum sum from [i, j] downward
+	dp := make([][]int, h)
+	for i := range dp {
+		dp[i] = make([]int, w)
+
+		for j := range dp[0] {
+			dp[i][j] = math.MaxInt32
+		}
+	}
+
+	for i := range matrix[0] {
+		dp[h-1][i] = matrix[h-1][i]
+	}
+
+	for i := h - 2; i >= 0; i-- {
+		for j := range dp[0] {
+			if j > 0 {
+				dp[i][j] = min(dp[i][j], dp[i+1][j-1]+matrix[i][j])
+			}
+
+			dp[i][j] = min(dp[i][j], dp[i+1][j]+matrix[i][j])
+
+			if j < w-1 {
+				dp[i][j] = min(dp[i][j], dp[i+1][j+1]+matrix[i][j])
+			}
+		}
+	}
+
+	ans := math.MaxInt32
+	for i := range dp[0] {
+		ans = min(ans, dp[0][i])
+	}
+
+	return ans
+}
+
+func minFallingPathSum1(A [][]int) int {
 	length := len(A)
 
 	if length == 1 {
@@ -60,7 +101,7 @@ func min(i, j int) int {
 	return j
 }
 
-//	problems
+//	Notes
 //	1.	this is wrong, if a number is chosen, left choices are only one
 //		direction, e.g. if choose right number, then choices are right
 //		right-1, right+1
