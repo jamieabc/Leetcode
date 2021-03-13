351d426fec0534faf347e3a2217d737079fbb2d9
Aaron Chang <xesuschang@gmail.com>
2021-03-13 14:11:03 +0800

    Squashed commit of 1286:
    commit f2c66473644e9f9b02a98fe24ff08960e6e3f69b
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sat Mar 13 14:10:49 2021 +0800
    
        attempt 3
    
    commit bf8a708c634c1146a117cafb3d4b1aca750238c3
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sat Mar 13 13:37:18 2021 +0800
    
        attempt 2

--- 1286.go
+++ 1286.go
@@ -26,20 +26,135 @@ package main
 // It's guaranteed that all calls of the function next are valid.
 
 type CombinationIterator struct {
+	Stack          []int
+	Str            string
+	Cur            []byte
+	Remain, Target int
+}
+
+func Constructor(characters string, combinationLength int) CombinationIterator {
+	remain := 1
+	size := len(characters)
+
+	for i := 0; i < combinationLength; i++ {
+		remain *= size - i
+	}
+
+	for i := 1; i <= combinationLength; i++ {
+		remain /= i
+	}
+
+	return CombinationIterator{
+		Stack:  make([]int, 0),
+		Str:    characters,
+		Cur:    make([]byte, 0),
+		Remain: remain,
+		Target: combinationLength,
+	}
+}
+
+func (this *CombinationIterator) Next() string {
+	if len(this.Cur) == 0 {
+		for i := 0; i < this.Target; i++ {
+			this.Stack = append(this.Stack, i)
+			this.Cur = append(this.Cur, this.Str[i])
+		}
+	} else {
+		// remove previous chars not able to meet target length
+		for len(this.Stack) > 0 {
+			this.Cur = this.Cur[:len(this.Cur)-1]
+			last := this.Stack[len(this.Stack)-1]
+			this.Stack = this.Stack[:len(this.Stack)-1]
+
+			if last < len(this.Str)-(this.Target-len(this.Cur)) {
+				this.Cur = append(this.Cur, this.Str[last+1])
+				this.Stack = append(this.Stack, last+1)
+				break
+			}
+		}
+
+		// appending chars, each index increase by 1 from previous
+		for len(this.Cur) < this.Target {
+			i := this.Stack[len(this.Stack)-1]
+			this.Cur = append(this.Cur, this.Str[i+1])
+			this.Stack = append(this.Stack, i+1)
+		}
+	}
+
+	this.Remain--
+	return string(this.Cur)
+}
+
+func (this *CombinationIterator) HasNext() bool {
+	return this.Remain > 0
+}
+
+type CombinationIterator2 struct {
+	Data []string
+}
+
+func Constructor(characters string, combinationLength int) CombinationIterator2 {
+	ans := make([]string, 0)
+	size := len(characters)
+
+	for i := (1 << size) - 1; i >= 0; i-- {
+		if oneBits(i) == combinationLength {
+			tmp := make([]byte, 0)
+
+			for j := size - 1; j >= 0; j-- {
+				if i&(1<<j) != 0 {
+					// becareful, left shift is reverse order of
+					// original string
+					tmp = append(tmp, characters[size-1-j])
+				}
+			}
+
+			ans = append(ans, string(tmp))
+		}
+	}
+
+	return CombinationIterator2{
+		Data: ans,
+	}
+}
+
+func oneBits(i int) int {
+	var count int
+
+	for i > 0 {
+		count++
+		i = i & (i - 1)
+	}
+
+	return count
+}
+
+func (this *CombinationIterator2) Next() string {
+	str := this.Data[0]
+	this.Data = this.Data[1:]
+
+	return str
+}
+
+func (this *CombinationIterator2) HasNext() bool {
+	return len(this.Data) > 0
+}
+
+type CombinationIterator1 struct {
 	Str   string
 	Stack []int
 	Size  int
 }
 
-func Constructor(characters string, combinationLength int) CombinationIterator {
-	return CombinationIterator{
+func Constructor(characters string, combinationLength int) CombinationIterator1 {
+	return CombinationIterator1{
 		Str:   characters,
 		Size:  combinationLength,
 		Stack: make([]int, 0),
 	}
 }
 
-func (this *CombinationIterator) Next() string {
+func (this *CombinationIterator1) Next() string {
 	result := make([]byte, this.Size)
 
 	if len(this.Stack) == 0 {
@@ -74,7 +189,7 @@ func (this *CombinationIterator) Next() string {
 	return string(result)
 }
 
-func (this *CombinationIterator) HasNext() bool {
+func (this *CombinationIterator1) HasNext() bool {
 	if len(this.Stack) == 0 {
 		return true
 	}
@@ -123,3 +238,14 @@ func (this *CombinationIterator) HasNext() bool {
 //		L2: j = 0 (0 indexed)
 //		L3: l = 3 (0 indexed), switch aj & al => 2 4 3 1
 //		L4: 2 1 3 4
+
+//	3.	inspired from solution, don't forget the scenario to precompute all
+//		combinations, which results in tc O(1) & sc O(2^n)
+
+//	4.	inspired from solution, instead of using backtracking, can use bitmask
+//		bitmask start from 2^n-1, if 1's count == target, push it, very smart
+
+//		count 1 in an integer can use a & (a-1) to do so
+
+//		convert bitmask precompute into bitmark itegration is easier, the key is
+//		to have bitmarks always the next valid number, so that hasNext can check
