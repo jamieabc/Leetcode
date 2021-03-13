a8e1d24c00c5c1525b4847749d0513e0504d0fe2
Aaron Chang <xesuschang@gmail.com>
2021-03-13 11:38:55 +0800

    Squashed commit of 114:
    commit 4e914357634fb14fe459f5c7648381b1e08fac24
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sat Mar 13 11:38:28 2021 +0800
    
        attempt 5
    
    commit 2b7f617401e4c64edb29cccf5501e0943e0b9d05
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sat Mar 13 11:34:37 2021 +0800
    
        attempt 4
    
    commit 15408b21e3dfe908cab41cd9b50c98a88dc8b5f7
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sat Mar 13 11:23:06 2021 +0800
    
        attempt 3
    
    commit d9ccd165cb360b61cdce864b75caaeec2396b35b
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sat Mar 13 10:37:17 2021 +0800
    
        attempt 2
    
    commit 4bc59993279ed76fde03bf84d282c82e20996001
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sat Mar 13 10:36:45 2021 +0800
    
        attempt 1

--- 114.go
+++ 114.go
@@ -32,7 +32,99 @@ package main
  *     Right *TreeNode
  * }
  */
+
 func flatten(root *TreeNode) {
+	for node := root; node != nil; {
+		if node.Left != nil {
+			nodeWithoutRight := node.Left
+
+			// find first node on left subtree without right child, so that this node
+			// will be flattened in the furture
+			for ; nodeWithoutRight.Right != nil; nodeWithoutRight = nodeWithoutRight.Right {
+			}
+
+			nodeWithoutRight.Right = node.Right
+			node.Right = node.Left
+			node.Left = nil
+		}
+
+		node = node.Right
+	}
+}
+
+func flatten4(root *TreeNode) {
+	stack := []*TreeNode{root}
+
+	// the point is to keep previous node, so that original tree can be transformed
+	var prev *TreeNode
+
+	for len(stack) > 0 {
+		cur := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		// put in reverse order (right, left) such that get from stack with correct order
+		if cur.Right != nil {
+			stack = append(stack, cur.Right)
+		}
+
+		if cur.Left != nil {
+			stack = append(stack, cur.Left)
+			cur.Left = nil
+		}
+
+		if prev != nil {
+			prev.Right = cur
+		}
+		prev = cur
+	}
+}
+
+func flatten3(node *TreeNode) {
+	var prev *TreeNode
+	postOrder(node, &prev)
+}
+
+func postOrder(node *TreeNode, prev **TreeNode) {
+	if node == nil {
+		return
+	}
+
+	postOrder(node.Right, prev)
+	postOrder(node.Left, prev)
+
+	// very beautiful
+	node.Right = *prev
+	node.Left = nil
+	*prev = node
+}
+
+func flatten2(root *TreeNode) {
+	inOrder(root)
+}
+
+func inOrder(node *TreeNode) *TreeNode {
+	if node == nil {
+		return nil
+	}
+
+	right := node.Right
+	node.Right = nil
+
+	if node.Left != nil {
+		node.Right = inOrder(node.Left)
+		node.Left = nil
+	}
+
+	cur := node
+	for ; cur.Right != nil; cur = cur.Right {
+	}
+
+	cur.Right = inOrder(right)
+
+	return node
+}
+
+func flatten1(root *TreeNode) {
 	if root == nil {
 		return
 	}
@@ -66,3 +158,29 @@ func convert(node *TreeNode) (*TreeNode, *TreeNode) {
 
 	return node, rightEnd
 }
+
+//	Notes
+//	1.	becareful of boundary condition...cannot assume root is non-nil
+
+//	2.	for recursion, better to set current node right tree to nil, such that
+//		no infinite loop occurs
+
+//	3.	inspired from solution, there's a very beautiful description of recursion:
+//		Recursion is all about postponing decisions until something else is completed.
+
+//		what a precise and exact description
+
+//	4.	inspired from solution, morris traversal can reach tc O(n) & sp O(1)
+
+//		very beautiful solution, especially when merging left node to right node,
+//		it searches first node of left-subtree w/o right child
+
+//	5.	inspired from https://leetcode.com/problems/flatten-binary-tree-to-linked-list/discuss/36977/My-short-post-order-traversal-Java-solution-for-share
+
+//		author provides a very elegant recursion solution to this
+//		the key point here is to solve right side first, then left side, and eventually
+//		build a flatten list
+
+//		the best part of it is no need to traverse when a new sub-tree going to right,
+//		from #inOrder, the loop is about finding where to concat right node, so this way
+//		reduces this part
