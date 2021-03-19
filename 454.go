a995dca3035ef0636871b0cf5e0d93774e8b4ec7
Aaron Chang <xesuschang@gmail.com>
2021-03-19 19:35:45 +0800

    Squashed commit of 454:
    commit 6ab14b3e79cad4a1f747b804a5feb7813d0e109b
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Mar 19 19:34:58 2021 +0800
    
        add comment

--- 454.go
+++ 454.go
@@ -20,7 +20,7 @@ package main
 // 1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
 // 2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
 
-// tc: O(n^2)
+// tc: O(n^2), sc: O(n^2)
 func fourSumCount(A []int, B []int, C []int, D []int) int {
 	table := make(map[int]int)
 	for i := range A {
@@ -39,7 +39,38 @@ func fourSumCount(A []int, B []int, C []int, D []int) int {
 	return count
 }
 
+// tc: O(n^2), sc: O(n^2)
+func fourSumCount1(A []int, B []int, C []int, D []int) int {
+	table1, table2 := make(map[int]int), make(map[int]int)
+
+	for i := range A {
+		for j := range B {
+			table1[A[i]+B[j]]++
+		}
+	}
+
+	for i := range C {
+		for j := range D {
+			table2[C[i]+D[j]]++
+		}
+	}
+
+	var ans int
+
+	for sum1, occurrence := range table1 {
+		if val, ok := table2[-sum1]; ok {
+			ans += occurrence * val
+		}
+	}
+
+	return ans
+}
+
 //	Notes
 //	1.	inspired from https://leetcode.com/problems/4sum-ii/discuss/93917/Easy-2-lines-O(N2)-Python
 
 //		no need to use another table to store combination sums
+
+//	2.	if there are duplicates, then cannot sort C & D to find matched sum, because when
+//		iterating through array, if C[i] = 2 && D[j] = 2, don't know which array to move
+//		forward, i or j?
