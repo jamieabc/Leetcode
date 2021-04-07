4a632b94f5097a9e4e6f5d04eb599e16615530c1
Aaron Chang <xesuschang@gmail.com>
2021-04-07 18:20:48 +0800

    Squashed commit of 105:
    commit d1705576b5823d57c7796e7903662068051ee00e
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Wed Apr 7 18:20:31 2021 +0800
    
        add comment
    
    commit 50a1dd84461b34a5efdecf7b555767c618a0d6a2
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Wed Apr 7 18:14:52 2021 +0800
    
        attempt 1

---
+++ 105.go
@@ -0,0 +1,76 @@
+package main
+
+// Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+//
+//
+//
+// Example 1:
+//
+// Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+// Output: [3,9,20,null,null,15,7]
+//
+// Example 2:
+//
+// Input: preorder = [-1], inorder = [-1]
+// Output: [-1]
+//
+//
+//
+// Constraints:
+//
+// 1 <= preorder.length <= 3000
+// inorder.length == preorder.length
+// -3000 <= preorder[i], inorder[i] <= 3000
+// preorder and inorder consist of unique values.
+// Each value of inorder also appears in preorder.
+// preorder is guaranteed to be the preorder traversal of the tree.
+// inorder is guaranteed to be the inorder traversal of the tree.
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	if len(inorder) == 0 {
+		return nil
+	}
+
+	return dfs(preorder, inorder)
+}
+
+func dfs(preorder, inorder []int) *TreeNode {
+	if len(preorder) == 0 {
+		return nil
+	}
+
+	cur := &TreeNode{
+		Val: preorder[0],
+	}
+
+	if len(preorder) == 1 {
+		return cur
+	}
+
+	var in int
+	for i := range inorder {
+		if inorder[i] == preorder[0] {
+			in = i
+			break
+		}
+	}
+
+	cur.Left = dfs(preorder[1:1+in], inorder[:in])
+	cur.Right = dfs(preorder[1+in:], inorder[in+1:])
+
+	return cur
+}
+
+//	Notes
+//	1.	inspired from solution, use hashmap to find index, reduce tc from
+//		O(n^2) to O(n)
+
+//		but to use hashmap, every value should be unique
