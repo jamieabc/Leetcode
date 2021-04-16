1ee43fe46fc3778084c771293aa07fb188be7d5e
Aaron Chang <xesuschang@gmail.com>
2021-04-16 20:21:50 +0800

    Squashed commit of 85:
    commit ebf7f07933ad308158c70c9b601a785573825a6a
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Apr 16 20:21:36 2021 +0800
    
        add comment
    
    commit 36b65b564dd7f1b93e96030dee6525f8e038dfdd
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri Apr 16 19:51:48 2021 +0800
    
        attempt 1

--- 85.go
+++ 85.go
@@ -13,6 +13,8 @@ package main
 //]
 //Output: 6
 
+// tc: O(mn), scan row with O(n), calculate max area also takes O(n)
+// overall O(m*2n) = O(mn)
 func maximalRectangle(matrix [][]byte) int {
 	if len(matrix) == 0 {
 		return 0
@@ -56,6 +58,62 @@ func maximalRectangle(matrix [][]byte) int {
 	}
 
 	return rect
+}
+
+// tc: O(mn)
+func maximalRectangle2(matrix [][]byte) int {
+	h := len(matrix)
+
+	if h == 0 {
+		return 0
+	}
+
+	w := len(matrix[0])
+
+	// dp[i]: vertical consecutive 1s
+	dp := make([]int, w)
+	var largest int
+
+	for i := range matrix {
+		for j := range matrix[i] {
+			if matrix[i][j] == '0' {
+				dp[j] = 0
+			} else {
+				dp[j]++
+			}
+		}
+
+		// count maximum rectangle so far
+		largest = max(largest, maxArea(dp))
+	}
+
+	return largest
+}
+
+func maxArea(ones []int) int {
+	var area int
+	stack := []int{-1}
+
+	for i := range ones {
+		for len(stack) > 1 && ones[stack[len(stack)-1]] > ones[i] {
+			prev := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			area = max(area, ones[prev]*(i-stack[len(stack)-1]-1))
+		}
+
+		stack = append(stack, i)
+	}
+
+	// process remaining data
+	for len(stack) > 1 {
+		cur := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		area = max(area, ones[cur]*(len(ones)-stack[len(stack)-1]-1))
+	}
+
+	return area
 }
 
 func maximalRectangle(matrix [][]byte) int {
@@ -120,42 +178,42 @@ func min(i, j int) int {
 	return j
 }
 
-// problems
-// 1. k should be controller by i not xMax
-// 2. when calculating xMax, it is absolute index, which means it's x+i
-// 3. optimize, use dp, O(m n^2)
-// 4. for the first row or column, I just pass it, the problem is that
-// 	  if only one row/column, then max is updated which returns error value
-// 5. when encounter 0 and then another positive width, the start needs
-//    to be updated to the row that has positive number
-// 6. optimize, no need to initialize dp first, it can be calculated the
-// 	  same time when traversing dp
-//	  if matrix[y][x] == '1' then dp[y][x] = dp[y][x-1] + 1
-// 7. optimize, when width is 0, no need to calculate any upper elements
-// 8. optimize, since width = 0 is no longer considered, start can be
-// 	  removed
-// 9. optimize, complexity of O(m^2 n) is still too large, the problem
-//    comes form the fact that re-calculating rectangle width.
-//    From solution article, a clever method has been proposed.
-// 	  every rectangle is composed of height, left, right.
+//	Notes
+//	1.	k should be controller by i not xMax
+//	2.	when calculating xMax, it is absolute index, which means it's x+i
+//	3.	optimize, use dp, O(m n^2)
+//	4.	for the first row or column, I just pass it, the problem is that
+//		if only one row/column, then max is updated which returns error value
+//	5.	when encounter 0 and then another positive width, the start needs
+//		to be updated to the row that has positive number
+//	6.	optimize, no need to initialize dp first, it can be calculated the
+//		same time when traversing dp
+//		if matrix[y][x] == '1' then dp[y][x] = dp[y][x-1] + 1
+//	7.	optimize, when width is 0, no need to calculate any upper elements
+//	8.	optimize, since width = 0 is no longer considered, start can be
+//		removed
+//	9.	optimize, complexity of O(m^2 n) is still too large, the problem
+//		comes form the fact that re-calculating rectangle width.
+//		From solution article, a clever method has been proposed.
+//		every rectangle is composed of height, left, right.
 //
-//	  height[j] = height[j] + 1 if matrix[y][x] == 1
+//		height[j] = height[j] + 1 if matrix[y][x] == 1
 //
-//	  left boundary of a rectangle can be considered as maximum index of
-// 	  right most zero from current point left part.
+//		left boundary of a rectangle can be considered as maximum index of
+//		right most zero from current point left part.
 //
-//	  left[j] = max(left[j], current_left)
-//	  current_left starts from 9 and is updated whenever encounter 0
+//		left[j] = max(left[j], current_left)
+//		current_left starts from 9 and is updated whenever encounter 0
 //
-//	  right boundary of a rectangle can be considered as minimum index of
-//	  right most zero from current point right part.
+//		right boundary of a rectangle can be considered as minimum index of
+//		right most zero from current point right part.
 //
-//    be aware that right boundary must start from right most element
-//	  (length-1), so that right most zero is considered.
+//		be aware that right boundary must start from right most element
+//		(length-1), so that right most zero is considered.
 //
-//	  right[j] = min(right[j], current_right)
-//	  current_right starts from length and is updated whenever
-//	  encounter 0
+//		right[j] = min(right[j], current_right)
+//		current_right starts from length and is updated whenever
+//		encounter 0
 
 //	10.	this problem relates to lc 84, calculate maximum rectangle
 //		tc: O(mn), because worst case in stack-based traverse is (2n), so for
