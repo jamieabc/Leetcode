6e979686e6cae652ef2f3a783aa4a8f4d4465e7c
Aaron Chang <xesuschang@gmail.com>
2021-04-19 19:23:43 +0800

    Squashed commit of 1593:
    commit 22e6447340f2b1488b4f7735d86adaf902df7bc9
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon Apr 19 19:23:31 2021 +0800
    
        attempt 3
    
    commit 5363706cb6983fdaa2efbb14c51a19371f01ab8f
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon Apr 19 19:16:30 2021 +0800
    
        attempt 2

--- 1593.go
+++ 1593.go
@@ -37,12 +37,46 @@ import "math"
 //    s contains only lower case English letters.
 
 func maxUniqueSplit(s string) int {
-	set := make(map[string]bool)
+	table := make(map[string]bool)
 
-	return recursive(s, 0, set)
+	var count int
+
+	backtracking(s, 0, 0, table, &count)
+
+	return count
 }
 
-func recursive(str string, start int, set map[string]bool) int {
+func backtracking(s string, idx, cur int, table map[string]bool, count *int) {
+	if idx == len(s) {
+		*count = max(*count, cur)
+		return
+	}
+
+	// prune branches, discrad those cannot outperform best
+	if *count > cur+len(s)-idx {
+		return
+	}
+
+	for i := idx; i < len(s); i++ {
+		for j := i; j < len(s); j++ {
+			if exist, ok := table[s[i:j+1]]; !ok || !exist {
+				table[s[i:j+1]] = true
+
+				backtracking(s, j+1, cur+1, table, count)
+
+				table[s[i:j+1]] = false
+			}
+		}
+	}
+}
+
+func maxUniqueSplit1(s string) int {
+	set := make(map[string]bool)
+
+	return recursive1(s, 0, set)
+}
+
+func recursive1(str string, start int, set map[string]bool) int {
 	// terminate condition
 	if start == len(str) {
 		return 0
@@ -54,7 +88,7 @@ func recursive(str string, start int, set map[string]bool) int {
 		if !set[str[start:i+1]] {
 			set[str[start:i+1]] = true
 
-			after := recursive(str, i+1, set)
+			after := recursive1(str, i+1, set)
 			set[str[start:i+1]] = false
 
 			if after == -1 {
@@ -78,3 +112,13 @@ func max(i, j int) int {
 	}
 	return j
 }
+
+//	Notes
+//	1.	this is not dp problem, because even though recurring problem occurrs,
+//		previous split affects later combination count, so it cannot be solved
+//		by dp
+
+//	2.	inspired from https://leetcode.com/problems/split-a-string-into-the-max-number-of-unique-substrings/discuss/855690/C%2B%2B-iterative-100ms-beats-100-with-bit-twiddling
+//
+//		to faster computation, prune those length not enough to outperform
+//		current best
