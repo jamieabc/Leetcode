9b98c9aa103f7ee62d8fe470793764b64e71e05c
Aaron Chang <xesuschang@gmail.com>
2021-04-20 19:07:54 +0800

    Squashed commit of 590:
    commit 9152baf40ebc9da240a2b467c2a4a6f6c87de2d2
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Tue Apr 20 19:07:43 2021 +0800
    
        attempt 1

---
+++ 590.go
@@ -0,0 +1,64 @@
+package main
+
+// Given the root of an n-ary tree, return the postorder traversal of its nodes' values.
+//
+// Nary-Tree input serialization is represented in their level order traversal. Each group of children is separated by the null value (See examples)
+//
+//
+//
+// Example 1:
+//
+// Input: root = [1,null,3,2,4,null,5,6]
+// Output: [5,6,3,2,4,1]
+//
+// Example 2:
+//
+// Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+// Output: [2,6,14,11,7,3,12,8,4,13,9,10,5,1]
+//
+//
+//
+// Constraints:
+//
+// The number of nodes in the tree is in the range [0, 104].
+// 0 <= Node.val <= 104
+// The height of the n-ary tree is less than or equal to 1000.
+//
+//
+//
+// Follow up: Recursive solution is trivial, could you do it iteratively?
+
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Children []*Node
+ * }
+ */
+
+func postorder(root *Node) []int {
+	nums := make([]int, 0)
+	if root == nil {
+		return nums
+	}
+
+	stack := []*Node{root}
+
+	for len(stack) > 0 {
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		nums = append(nums, node.Val)
+
+		for i := range node.Children {
+			stack = append(stack, node.Children[i])
+		}
+	}
+
+	// reverse order
+	for i, j := 0, len(nums)-1; i < j; i, j = i+1, j-1 {
+		nums[i], nums[j] = nums[j], nums[i]
+	}
+
+	return nums
+}
