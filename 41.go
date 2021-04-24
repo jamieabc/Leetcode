13c51621927954f5c789e094d68842fe3f9d9258
Aaron Chang <xesuschang@gmail.com>
2021-04-24 14:55:42 +0800

    Squashed commit of 41:
    commit 63e4b06bd666ff49fff8d033f58db2a04391e0bc
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sat Apr 24 14:54:48 2021 +0800
    
        attmpt 4
    
    commit d4c860018e59d6b7288c293d090c87882eea04aa
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Sat Apr 24 14:27:34 2021 +0800
    
        attmpt 3

--- 41.go
+++ 41.go
@@ -20,6 +20,10 @@ package main
 
 func firstMissingPositive(nums []int) int {
 	for i := range nums {
+		// 1 at index 0
+		// 2 at index 1
+		// skip infinite loop condition [1, 1, 1, 1]
+		// do not change if target number is already correct
 		for i != nums[i]-1 && nums[i]-1 >= 0 && nums[i]-1 < len(nums) && nums[i] != nums[nums[i]-1] {
 			nums[i], nums[nums[i]-1] = nums[nums[i]-1], nums[i]
 		}
@@ -32,6 +36,59 @@ func firstMissingPositive(nums []int) int {
 	}
 
 	return len(nums) + 1
+}
+
+func firstMissingPositive1(nums []int) int {
+	var one bool
+
+	for i := range nums {
+		if nums[i] == 1 {
+			one = true
+		} else if nums[i] <= 0 {
+			nums[i] = 1
+		}
+	}
+
+	if !one {
+		return 1
+	}
+
+	size := len(nums)
+	for i := range nums {
+		idx := abs(nums[i])
+
+		if idx < size && nums[idx] > 0 {
+			nums[idx] *= -1
+		} else if idx == size && nums[0] > 0 {
+			nums[0] *= -1
+		}
+	}
+
+	for i := 2; i < size; i++ {
+		if nums[i] > 0 {
+			return i
+		}
+	}
+
+	if nums[0] < 0 {
+		return size + 1
+	}
+
+	return size
+}
+
+func max(i, j int) int {
+	if i >= j {
+		return i
+	}
+	return j
+}
+
+func abs(i int) int {
+	if i >= 0 {
+		return i
+	}
+	return -i
 }
 
 //	Notes
@@ -48,3 +105,9 @@ func firstMissingPositive(nums []int) int {
 
 //	3.	inspired from solution, cyclic sort is described by "index as hash key"
 //		which is a very precise explanation.
+
+//	4.	after half year, cannot understand why cyclic sort is okay
+
+//	5.	the method of cyclic sort, is to put number that is not at its position
+//		to proper location, that's why target swap index is nums[i]-1, because
+//		that's where it should be
