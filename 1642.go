774a4f6fd775cf0354cbdd0d29440252d55276ce
Aaron Chang <xesuschang@gmail.com>
2021-04-26 19:53:48 +0800

    Squashed commit of 1642:
    commit 6ba07083dad27ddcf3055a5efbc1582a3adee695
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon Apr 26 19:28:38 2021 +0800
    
        attempt 6
    
    commit 3fed4599570d9b562cd06c994161220918987dc6
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon Apr 26 19:26:12 2021 +0800
    
        attempt 5
    
    commit afb5e8c302b1aa48b08fac2372dc2d9d19b653e0
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon Apr 26 18:05:47 2021 +0800
    
        attempt 4
    
    commit 30ce2dd04e12cd0112cb6608c8d1f869c279d08a
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Mon Apr 26 17:34:33 2021 +0800
    
        attempt 3

--- 1642.go
+++ 1642.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"container/heap"
+	"sort"
+)
+
 // You are given an integer array heights representing the heights of buildings, some bricks, and some ladders.
 //
 // You start your journey from building 0 and move to the next building by possibly using bricks or ladders.
@@ -55,14 +60,143 @@ func (h *MaxHeap) Push(x interface{}) {
 }
 
 func (h *MaxHeap) Pop() interface{} {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-	return x
+	popped := (*h)[len(*h)-1]
+	*h = (*h)[:len(*h)-1]
+
+	return popped
 }
 
 func furthestBuilding(heights []int, bricks int, ladders int) int {
+	maxHeap := &MaxHeap{}
+	heap.Init(maxHeap)
+
+	size := len(heights)
+	var i int
+
+	for ; i < size-1; i++ {
+		if diff := heights[i+1] - heights[i]; diff > 0 {
+			// push heap
+			heap.Push(maxHeap, diff)
+
+			// adjust heap to meet some condition
+			for diff > bricks && maxHeap.Len() > 0 && ladders > 0 {
+				ladders--
+				bricks += heap.Pop(maxHeap).(int)
+			}
+
+			// not enough bricks
+			if diff > bricks {
+				if ladders == 0 {
+					// no ladder, cannot go any further
+					break
+				} else {
+					// use ladder
+					ladders--
+				}
+			} else {
+				bricks -= diff
+			}
+		}
+	}
+
+	return i
+}
+
+func furthestBuilding3(heights []int, bricks int, ladders int) int {
+	jumps := make([][2]int, 0)
+	size := len(heights)
+
+	for i := 0; i < size-1; i++ {
+		if heights[i+1] > heights[i] {
+			jumps = append(jumps, [2]int{heights[i+1] - heights[i], i + 1})
+		}
+	}
+
+	sort.Slice(jumps, func(i, j int) bool {
+		if jumps[i][0] != jumps[j][0] {
+			return jumps[i][0] < jumps[j][0]
+		}
+
+		return jumps[i][1] < jumps[j][1]
+	})
+
+	// binary search
+	farthest := size - 1 // in case all successful
+	for low, high := 0, size-1; low <= high; {
+		mid := low + (high-low)/2
+
+		if check(jumps, mid, bricks, ladders) {
+			low = mid + 1
+		} else {
+			high = mid - 1
+			farthest = high
+		}
+	}
+
+	return farthest
+}
+
+// tc: O(n)
+func check(jumps [][2]int, idx, bricks, ladders int) bool {
+	for i := len(jumps) - 1; i >= 0; i-- {
+		if jumps[i][1] <= idx {
+			if ladders > 0 {
+				ladders--
+			} else {
+				bricks -= jumps[i][0]
+			}
+		}
+
+		if bricks < 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+type MinHeap []int
+
+func (h MinHeap) Len() int           { return len(h) }
+func (h MinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h MinHeap) Less(i, j int) bool { return h[i] < h[j] }
+
+func (h *MinHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *MinHeap) Pop() interface{} {
+	popped := (*h)[len(*h)-1]
+	*h = (*h)[:len(*h)-1]
+
+	return popped
+}
+
+func furthestBuilding2(heights []int, bricks int, ladders int) int {
+	minHeap := &MinHeap{}
+	heap.Init(minHeap)
+
+	var i int
+
+	for ; i < len(heights)-1; i++ {
+		if diff := heights[i+1] - heights[i]; diff > 0 {
+			heap.Push(minHeap, diff)
+
+			for minHeap.Len() > ladders {
+				bricks -= heap.Pop(minHeap).(int)
+			}
+
+			// because compute next building, so return current
+			if bricks < 0 {
+				return i
+			}
+		}
+	}
+
+	return i
+}
+
+func furthestBuilding1(heights []int, bricks int, ladders int) int {
 	h := &MaxHeap{}
 	heap.Init(h)
 
@@ -111,3 +245,32 @@ func furthestBuilding(heights []int, bricks int, ladders int) int {
 //		heights = [2, 7, 9, 12]
 //		bricks = 5
 //		ladders = 1
+
+//	2.	though this problem is greedy and got WA, think over again and recall
+//		that this problem is actually max-heap problem
+
+//	3.	for heap problem, it could be a template that push something into
+//		heap, adjust heap to make some condition valid, and do after process
+
+//	4.	inspired from solution, it uses min-heap to solve the problem
+
+//		the way is to use ladders first, and when there's not enough ladders,
+//		pop from min-heap and use bricks instead
+
+//		very interesting view to see the problem
+
+//	5.	inspired from solution, it's also possible to use binary search to
+//		solve the problem
+
+//		given some index, it's possible to sort all jumps, avoid largest number
+//		by ladder, compute remaining sums < bricks
+
+//		very smart..., tc is slightly slower, each time sort takes O(n log(n)),
+//		there are log(n) times to guess, overall tc O(n log^2(n))
+
+//	6.	to improve tc for binary search, just sort all jumps, attach with
+//		index, so that it's possible to identify the jump not in current range
+
+//		tc: O(n log(n))
+//		n: check time for each guess
+//		log(n): guess count
