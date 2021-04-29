5fb5d1f1f7417f625352e89c0e6733200ebd6114
Aaron Chang <xesuschang@gmail.com>
2021-04-29 18:06:20 +0800

    Squashed commit of 1616:
    commit b9e428f83dfd16346188cd3dd01c018162c56a86
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Thu Apr 29 18:05:41 2021 +0800
    
        attempt 5
    
    commit b1c43e3757db8d68117ed7ee574d72a718bffc05
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Thu Apr 29 17:33:28 2021 +0800
    
        attempt 4

--- 1616.go
+++ 1616.go
@@ -70,6 +70,84 @@ func isPalindrome(str string) bool {
 	return true
 }
 
+func checkPalindromeFormation2(a string, b string) bool {
+	return compare2(a, b) || compare2(b, a)
+}
+
+func compare2(a, b string) bool {
+	size := len(a)
+	var i, j int
+
+	if size&1 > 0 {
+		i, j = size/2, size/2
+	} else {
+		i, j = size/2-1, size/2
+	}
+
+	for ; i >= 0; i, j = i-1, j+1 {
+		if a[i] != a[j] {
+			return isPalindrome2(a[:i+1], b[j:]) || isPalindrome2(b[:i+1], a[j:])
+		}
+	}
+
+	return true
+}
+
+func isPalindrome2(s1, s2 string) bool {
+	for i := 0; i < len(s1); i++ {
+		if s1[i] != s2[len(s2)-1-i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func checkPalindromeFormation1(a string, b string) bool {
+	size := len(a)
+
+	if size <= 1 {
+		return true
+	}
+
+	// odd length
+	if size&1 > 0 {
+		return compare(a, b, size/2, size/2) || compare(b, a, size/2, size/2)
+	}
+
+	return compare(a, b, size/2-1, size/2) || compare(b, a, size/2-1, size/2)
+}
+
+func compare(src, dst string, idx1, idx2 int) bool {
+	var i int
+	size := len(src)
+
+	for ; i+idx2 < size; i++ {
+		if src[idx1-i] != src[idx2+i] {
+			break
+		}
+	}
+
+	pos := i
+	for ; i+idx2 < size; i++ {
+		if dst[idx1-i] != src[idx2+i] {
+			break
+		}
+	}
+
+	if i+idx2 == size {
+		return true
+	}
+
+	for i = pos; i+idx2 < size; i++ {
+		if src[idx1-i] != dst[idx2+i] {
+			break
+		}
+	}
+
+	return i+idx2 == size
+}
+
 //	Notes
 //	1.	most critical thinking: for a given comparison, there might exist 2
 //		conditions to check if remaining forms a palindrome string
@@ -83,3 +161,32 @@ func isPalindrome(str string) bool {
 // 		_: already compared palindrome
 //		x: don't care
 //		o: check if this range is palindrome
+
+//	2.	for palindrome, use progressive way to check, start from middle point
+//		and compares one by one till boundary
+
+//		the point for this problem is that size is always the same, which makes
+//		middle of palindrome either at a or b
+
+//		start from a, try to match as many as same character as possible; if
+//		there's any mismatch, try b to see if can reach to end
+
+//	3.	the smart way (alex) to think is from pattern
+
+//		there are 4 conditions to check, can be separated into 2 categories
+
+//		- middle at a
+//			a -----					   -----
+//
+//			b      ---				---
+
+//		- middle at b
+//			a ---					     ---
+//
+//			b   -----				-----
+
+//		the smarter way is to check only for middle at a or middle at b
+//		start from that middle, try to expand until left != right, then compare
+//		rest of string at a & b separately
+
+//		what i think of yesterday is also O(n), but this one is more clever
