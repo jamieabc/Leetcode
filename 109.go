f91502a87ab0e6260f0a71adfca2bb6462a62f99
Aaron Chang <xesuschang@gmail.com>
2021-05-07 18:43:28 +0800

    Squashed commit of 109:
    commit 5a04f7062c2d81f6f98ec53e23f0653c7245f73a
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri May 7 18:42:46 2021 +0800
    
        add comment
    
    commit f19cdf7b09260e1cec50a353c09bd078595a44fc
    Author: Aaron Chang <xesuschang@gmail.com>
    Date:   Fri May 7 18:38:49 2021 +0800
    
        attempt 2

--- 109.go
+++ 109.go
@@ -1,26 +1,28 @@
 package main
 
-//Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+// Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
 //
-//For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+// For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
 //
-//Example:
+// Example:
 //
-//Given the sorted linked list: [-10,-3,0,5,9],
+// Given the sorted linked list: [-10,-3,0,5,9],
 //
-//One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+// One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
 //
-//0
-/// \
-//-3   9
-///   /
-//-10  5
+// 0
+// / \
+// -3   9
+// /   /
+// -10  5
 
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
-
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
 /**
  * Definition for a binary tree node.
  * type TreeNode struct {
@@ -29,7 +31,46 @@ type ListNode struct {
  *     Right *TreeNode
  * }
  */
+
+// tc: O(n), sc: O(log(n))
 func sortedListToBST(head *ListNode) *TreeNode {
+	size := findSize(head)
+	node := head
+
+	return inOrder(0, size-1, &node)
+}
+
+func inOrder(start, end int, node **ListNode) *TreeNode {
+	if start > end {
+		return nil
+	}
+
+	mid := (start + end) / 2
+	left := inOrder(start, mid-1, node)
+
+	current := &TreeNode{
+		Val:  (*node).Val,
+		Left: left,
+	}
+
+	*node = (*node).Next
+
+	current.Right = inOrder(mid+1, end, node)
+
+	return current
+}
+
+func findSize(node *ListNode) int {
+	var size int
+	for ; node != nil; node = node.Next {
+		size++
+	}
+
+	return size
+}
+
+// tc: O(n log(n)), sc: O(log(n))
+func sortedListToBST1(head *ListNode) *TreeNode {
 	if head == nil {
 		return nil
 	}
@@ -68,3 +109,37 @@ func mid(node *ListNode) *TreeNode {
 		Right: mid(slow.Next),
 	}
 }
+
+//	Notes
+//	1.	inspired from solution, at first i thought finding middle take O(n) to
+//		traverse, but then realize that it's actually n/2 operations
+
+//		tc looks like O(n^2), but take close look:
+//		n/2 + 2 * (n/4) + 4 * (n/8) + ... = n/2 + n/2 + n/2 + ...
+//		since it takes log(n) to cut, overall tc becomes (n/2) * log(n) which
+//		is O(n log(n))
+
+//		sc: O(log(n)), because each recursion takes memory
+
+//	2.	inspired from solution, stores linked list into array, which saves
+//		time for finding middle node, tc: O(n)
+
+//	3.	inspired from https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/discuss/596899/JavaPython-Clean-code-O(N)-time-O(logN)-space
+
+//		very beautiful solution, w/o knowing actual mid value, just keep cutting
+//		linked list to half until left-most node is touched
+
+//		w/ this way, not need to find middle and convert to BST, it converts
+//		when boundary meets...
+
+//		by the merge-sort like way, use middle point to determine the left
+//		and right side; but why does middle is not needed?
+
+//		as long as in-order traversal is guaranteed, the order just like
+//		linked-list. So, how to make sure sequence in pre-order traversal?
+
+//		in-order: LNR, with proper notation (index), in-order can be guaranteed
+//		this is a very brilliant solution
+
+//	4.	inspired from solution, can also put all values into array, then
+//		convert to BST, tc: O(n), sc: O(n)
